d165d947dca29b5153e1670a60b2612c2a87d701
Toshiaki Katayama <k@bioruby.org>
2001-06-21 05:50:20 +0000

    * 2001/02/17 initial version created (separated from sequence.rb) * 2001/06/21 minor change (now CT became Hash not Array)

--- lib/bio/data/codontable.rb
+++ lib/bio/data/codontable.rb
@@ -13,86 +13,78 @@
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 #  Library General Public License for more details.
 #
+#  $Id: codontable.rb,v 0.2 2001/06/21 05:50:20 katayama Exp $
+#
 
 module CodonTable
 
-  CT = [
-    # codon table 0
-    {},
+  CT = {
 
-    # codon table 1 - Eukaryote (e.g. : S. cerevisiae)
-    {
-      "ttt"=>"F", "tct"=>"S", "tat"=>"Y", "tgt"=>"C",
-      "ttc"=>"F", "tcc"=>"S", "tac"=>"Y", "tgc"=>"C",
-      "tta"=>"L", "tca"=>"S", "taa"=>"*", "tga"=>"*",
-      "ttg"=>"L", "tcg"=>"S", "tag"=>"*", "tgg"=>"W",
-
-      "ctt"=>"L", "cct"=>"P", "cat"=>"H", "cgt"=>"R",
-      "ctc"=>"L", "ccc"=>"P", "cac"=>"H", "cgc"=>"R",
-      "cta"=>"L", "cca"=>"P", "caa"=>"Q", "cga"=>"R",
-      "ctg"=>"L", "ccg"=>"P", "cag"=>"Q", "cgg"=>"R",
-
-      "att"=>"I", "act"=>"T", "aat"=>"N", "agt"=>"S",
-      "atc"=>"I", "acc"=>"T", "aac"=>"N", "agc"=>"S",
-      "ata"=>"I", "aca"=>"T", "aaa"=>"K", "aga"=>"R",
-      "atg"=>"M", "acg"=>"T", "aag"=>"K", "agg"=>"R",
-
-      "gtt"=>"V", "gct"=>"A", "gat"=>"D", "ggt"=>"G",
-      "gtc"=>"V", "gcc"=>"A", "gac"=>"D", "ggc"=>"G",
-      "gta"=>"V", "gca"=>"A", "gaa"=>"E", "gga"=>"G",
-      "gtg"=>"V", "gcg"=>"A", "gag"=>"E", "ggg"=>"G",
+    # codon table 1  - Standard (Eukaryote)
+    1 => {
+      'ttt' => 'F', 'tct' => 'S', 'tat'	=> 'Y', 'tgt' => 'C',
+      'ttc' => 'F', 'tcc' => 'S', 'tac'	=> 'Y', 'tgc' => 'C',
+      'tta' => 'L', 'tca' => 'S', 'taa'	=> '*', 'tga' => '*',
+      'ttg' => 'L', 'tcg' => 'S', 'tag'	=> '*', 'tgg' => 'W',
+       	
+      'ctt' => 'L', 'cct' => 'P', 'cat'	=> 'H', 'cgt' => 'R',
+      'ctc' => 'L', 'ccc' => 'P', 'cac'	=> 'H', 'cgc' => 'R',
+      'cta' => 'L', 'cca' => 'P', 'caa'	=> 'Q', 'cga' => 'R',
+      'ctg' => 'L', 'ccg' => 'P', 'cag'	=> 'Q', 'cgg' => 'R',
+       	
+      'att' => 'I', 'act' => 'T', 'aat'	=> 'N', 'agt' => 'S',
+      'atc' => 'I', 'acc' => 'T', 'aac'	=> 'N', 'agc' => 'S',
+      'ata' => 'I', 'aca' => 'T', 'aaa'	=> 'K', 'aga' => 'R',
+      'atg' => 'M', 'acg' => 'T', 'aag'	=> 'K', 'agg' => 'R',
+       	
+      'gtt' => 'V', 'gct' => 'A', 'gat'	=> 'D', 'ggt' => 'G',
+      'gtc' => 'V', 'gcc' => 'A', 'gac'	=> 'D', 'ggc' => 'G',
+      'gta' => 'V', 'gca' => 'A', 'gaa'	=> 'E', 'gga' => 'G',
+      'gtg' => 'V', 'gcg' => 'A', 'gag'	=> 'E', 'ggg' => 'G',
     },
 
-    # codon table 2
-    {},
+    # codon table 2  - Vertebrate Mitochondrial
+    # codon table 3  - Yeast mitochondorial
+    # codon table 4  - Mold, Protozoan, Coelenterate Mitochondrial
+    #                  and Mycoplasma/Spiroplasma
+    # codon table 5  - Invertebrate Mitochondrial
+    # codon table 6  - Ciliate Macronuclear and Dasycladacean
+    # codon table 9  - Echinoderm Mitochondrial
+    # codon table 10 - Euplotid Nuclear
 
-    # codon table 3 - Eukaryote mitochondoria (e.g. : S. cerevisiae)
-    {},
-
-    # codon table 4
-    {},
-
-    # codon table 5
-    {},
-
-    # codon table 6
-    {},
-
-    # codon table 7
-    {},
-
-    # codon table 8
-    {},
-
-    # codon table 9
-    {},
-
-    # codon table 10
-    {},
-
-    # codon table 11 - Bacteria (e.g. : E. coli)
-    {
-      "ttt"=>"F", "tct"=>"S", "tat"=>"Y", "tgt"=>"C",
-      "ttc"=>"F", "tcc"=>"S", "tac"=>"Y", "tgc"=>"C",
-      "tta"=>"L", "tca"=>"S", "taa"=>"*", "tga"=>"*",
-      "ttg"=>"L", "tcg"=>"S", "tag"=>"*", "tgg"=>"W",
-
-      "ctt"=>"L", "cct"=>"P", "cat"=>"H", "cgt"=>"R",
-      "ctc"=>"L", "ccc"=>"P", "cac"=>"H", "cgc"=>"R",
-      "cta"=>"L", "cca"=>"P", "caa"=>"Q", "cga"=>"R",
-      "ctg"=>"L", "ccg"=>"P", "cag"=>"Q", "cgg"=>"R",
-
-      "att"=>"I", "act"=>"T", "aat"=>"N", "agt"=>"S",
-      "atc"=>"I", "acc"=>"T", "aac"=>"N", "agc"=>"S",
-      "ata"=>"I", "aca"=>"T", "aaa"=>"K", "aga"=>"R",
-      "atg"=>"M", "acg"=>"T", "aag"=>"K", "agg"=>"R",
-
-      "gtt"=>"V", "gct"=>"A", "gat"=>"D", "ggt"=>"G",
-      "gtc"=>"V", "gcc"=>"A", "gac"=>"D", "ggc"=>"G",
-      "gta"=>"V", "gca"=>"A", "gaa"=>"E", "gga"=>"G",
-      "gtg"=>"V", "gcg"=>"A", "gag"=>"E", "ggg"=>"G",
+    # codon table 11 - Bacteria
+    11 => {
+      'ttt' => 'F', 'tct' => 'S', 'tat'	=> 'Y', 'tgt' => 'C',
+      'ttc' => 'F', 'tcc' => 'S', 'tac'	=> 'Y', 'tgc' => 'C',
+      'tta' => 'L', 'tca' => 'S', 'taa'	=> '*', 'tga' => '*',
+      'ttg' => 'L', 'tcg' => 'S', 'tag'	=> '*', 'tgg' => 'W',
+       	
+      'ctt' => 'L', 'cct' => 'P', 'cat'	=> 'H', 'cgt' => 'R',
+      'ctc' => 'L', 'ccc' => 'P', 'cac'	=> 'H', 'cgc' => 'R',
+      'cta' => 'L', 'cca' => 'P', 'caa'	=> 'Q', 'cga' => 'R',
+      'ctg' => 'L', 'ccg' => 'P', 'cag'	=> 'Q', 'cgg' => 'R',
+       	
+      'att' => 'I', 'act' => 'T', 'aat'	=> 'N', 'agt' => 'S',
+      'atc' => 'I', 'acc' => 'T', 'aac'	=> 'N', 'agc' => 'S',
+      'ata' => 'I', 'aca' => 'T', 'aaa'	=> 'K', 'aga' => 'R',
+      'atg' => 'M', 'acg' => 'T', 'aag'	=> 'K', 'agg' => 'R',
+       	
+      'gtt' => 'V', 'gct' => 'A', 'gat'	=> 'D', 'ggt' => 'G',
+      'gtc' => 'V', 'gcc' => 'A', 'gac'	=> 'D', 'ggc' => 'G',
+      'gta' => 'V', 'gca' => 'A', 'gaa'	=> 'E', 'gga' => 'G',
+      'gtg' => 'V', 'gcg' => 'A', 'gag'	=> 'E', 'ggg' => 'G',
     },
-  ]
+
+    # codon table 12 - Alternative Yeast Nuclear
+    # codon table 13 - Ascidian Mitochondrial
+    # codon table 14 - Flatworm Mitochondrial
+    # codon table 15 - Blepharisma Macronuclear 
+    # codon table 16 - Chlorophycean Mitochondrial  
+    # codon table 21 - Trematode Mitochondrial  
+    # codon table 22 - Scenedesmus obliquus mitochondrial  
+    # codon table 23 - Thraustochytrium Mitochondrial Code  
+
+  }
 
   def ct(codon, table = 1)
     CT[table][codon]
