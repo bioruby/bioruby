a3fa49364fd84428080bfa5d4eb3f179794e65d7
Toshiaki Katayama <k@bioruby.org>
2001-06-21 06:02:00 +0000

    * 2001/01/29 change class to module

--- lib/bio/io/dbget.rb
+++ lib/bio/io/dbget.rb
@@ -1,47 +1,118 @@
 #
-# dbget: Interface to GenomeNet DBGET system (http://www.genome.ad.jp/)
+# dbget: Interface to GenomeNet DBGET system - http://www.genome.ad.jp/dbget/
 #
-#   Copyright (C) 2000, 2001 Mitsuteru Nakao, KATAYAMA Toshiaki
+#   Copyright (C) 2000 Mitsuteru S. Nakao <n@bioruby.org>
+#   Copyright (C) 2000, 2001 KATAYAMA Toshiaki <k@bioruby.org>
 #
 #   ChangeLog:
-#     2000/11/20 Mitsuteru S. Nakao, <n@bioruby.org>
-#     2000/11/24 KATAYAMA Toshiaki, <k@bioruby.org>
-#     2001/01/26 KATAYAMA Toshiaki, <k@bioruby.org>
-#     2001/01/29 KATAYAMA Toshiaki, <k@bioruby.org>
+#     2000/11/20 v0.1 (n) initial version
+#     2000/11/24 v0.2 (k) clean up rewrite
+#     2001/01/29 v1.0 (k) change class to module
 #
 
 require "socket"
 
-class DBGET
+module DBGET
 
-  def initialize(addr="dbgetserv.genome.ad.jp", port="3266")
-    if ENV["DBGET"] =~ /:/
+  def dbget(com, arg)
+
+    addr="dbgetserv.genome.ad.jp"	# default DBGET server address
+    port="3266"				# default DBGET port number
+
+    if ENV["DBGET"] =~ /:/		# overwrite DBGET serv/port
       addr, port = ENV["DBGET"].split(':')
     end
-    @addr = addr			# DBGET server address
-    @port = port			# DBGET port number
-  end
 
-  def dbget(arg) 
-    query = arg + "\n"			# Query string
-    result = []				# Result
+    query = "#{com} #{arg}\n"		# DBGET query string
+    result = ''				# DBGET result
 
-    sock = TCPSocket.open("#{@addr}", "#{@port}")
+    sock = TCPSocket.open("#{addr}", "#{port}")
 
-    sock.write(query)			# Submit query
-    sock.flush				# Buffer flush
+    sock.write(query)			# submit query
+    sock.flush				# buffer flush
 
     while sock.gets
       next if /^\+/
       next if /^\#/
       next if /^\*Request-IDent/
-      result.push($_)
+      result << $_
     end
 
-    sock.close				# Close TCP connection
+    sock.close
 
     return result
   end
+  module_function :dbget
+
+
+  ### optional module functions (in alphabetical order)
+
+  # alink("db entry")	- print relations
+  def alink(arg) 
+    dbget("alink", arg)
+  end
+  module_function :alink
+
+  # bacc("db entry")	- get accession(s)
+
+  # bent("db entry")	- get entry name
+
+  # bfind("db keyword")	- search entris by keyword
+  def bfind(arg)
+    dbget("bfind", arg)
+  end
+  module_function :bfind
+
+  # bget("<options> [<dbname>:][<id> ..]") - get entry by ID 
+  #
+  # options:
+  #   -f      in FASTA format
+  #     -n 1  return Amino Acid sequence
+  #     -n 2  return Nucleic Acid sequence
+  #   -h      print help message
+  #   -V      print version
+  #
+  def bget(arg)
+    dbget("bget", arg)
+  end
+  module_function :bget
+
+  # binfo("db")		- get database information
+  def binfo(arg)
+    dbget("binfo", arg)
+  end
+  module_function :binfo
+
+  # blink("db entry")	- print link informations
+  def blink(arg)
+    dbget("blink", arg)
+  end
+  module_function :blink
+
+  # bman ("db entry")	- print manual page
+  def bman(arg)
+    dbget("bman", arg)
+  end
+  module_function :bman
+
+  # bref("db entry")	- get references and authors
+  def bref(arg) 
+    dbget("bref", arg)
+  end
+  module_function :bref
+
+  # btab ("db entry")	- get/generate database alias table
+  def btab(arg) 
+    dbget("btab", arg)
+  end
+  module_function :btab
+
+  # btit("db entry ..")	- get entry definition
+  def btit(arg) 
+    dbget("btit", arg)
+  end
+  module_function :btit
+
+  # lmarge ("db entry")
 
 end
-
