840d6c74083aa06f8fc630c2c62827ce3330c3ee
Toshiaki Katayama <k@bioruby.org>
2001-06-25 07:28:07 +0000

    *** empty log message ***

---
+++ lib/bio/db/litdb.rb
@@ -0,0 +1,68 @@
+#
+# bio/db/litdb.rb - LITDB database class
+#
+#   Copyright (C) 2001 KATAYAMA Toshiaki <k@bioruby.org>
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Library General Public
+#  License as published by the Free Software Foundation; either
+#  version 2 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Library General Public License for more details.
+#
+#  $Id: litdb.rb,v 0.1 2001/06/25 07:28:07 katayama Exp $
+#
+
+require 'bio/db'
+
+class LITDB < NCBIDB
+
+  DELIMITER	= RS = "\nEND\n"
+  TAGSIZE	= 12
+
+  def initialize(entry)
+    super(entry, TAGSIZE)
+  end
+
+  # CODE
+  def id
+    field_fetch('CODE')
+  end
+
+  # TITLE
+  def title
+    field_fetch('TITLE')
+  end
+
+  # FIELD
+  def field
+    field_fetch('FIELD')
+  end
+
+  # JOURNAL
+  def journal
+    field_fetch('JOURNAL')
+  end
+
+  # VOLUME
+  def journal
+    field_fetch('VOLUME')
+  end
+
+  # KEYWORD ';;'
+  def journal
+    unless @data['KEYWORD']
+      @data['KEYWORD'] = fetch('KEYWORD').split(/;;\s*/)
+    end
+    @data['KEYWORD']
+  end
+
+  # AUTHOR
+  def journal
+    field_fetch('AUTHOR')
+  end
+
+end
