0f8fabbfee2f49b77a0d164b851a82a017826e9a
Toshiaki Katayama <k@bioruby.org>
2001-08-21 11:08:08 +0000

    * changed header comment * move include Math to class BioVector * clean up BioMatrix (remove needless comments, if clause, and variables) * changed method name BioMatrix.get_weight to BioMatrix.get_weight_na * clean up BioVector (cange class variables to local variables)

--- lib/bio/matrix.rb
+++ lib/bio/matrix.rb
@@ -1,5 +1,6 @@
-#!/usr/bin/ruby 
-# bio/biomatrix.rb - biological matrix class
+#
+# bio/matrix.rb - biological matrix class
+#
 #   Copyright (C) 2001 KAWASHIMA Shuichi <s@bioruby.org>
 #
 #  This library is free software; you can redistribute it and/or
@@ -12,56 +13,47 @@
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 #  Library General Public License for more details.
 #
+#  $Id: matrix.rb,v 1.2 2001/08/21 11:08:08 katayama Exp $
 #
-include Math
-require 'bio/sequence'
+
 require 'matrix'
 
 class BioMatrix < Matrix
 
   def win_search(seq, threshold)
+    result = []
     row_size = self.row_size
-#    if seq.size < row_size
-#      raise BioMatrixException, "#{seq} window size is larger than the query sequence"
-#    end
-    if seq.size > row_size
-      @result = []
+    seq_size = seq.size
+    if seq_size > row_size
       i = 0
       max_score = get_max
-      for x in (0 .. seq.size - row_size)
+      for x in (0 .. seq_size - row_size)
         sum = 0
-        score = 0
         for y in (0 .. row_size - 1)
-          sum += get_weight(seq[x+y, 1], y)
+          sum += get_weight_na(seq[x+y], y)
         end
         score = sum.to_f/max_score.to_f
         if score > threshold
-          tmp_ary = []
-          tmp_ary << score
-          s_score = "%4.3f" % tmp_ary
-          @result[i] = [x, s_score]
-          i += 1
+          result[i] = [x, "%4.3f" % score]
+	  i += 1
         end
       end
-      if @result.size == 0
-        @result = nil
-      end
-      @result
-    else
-      @result = nil
-      @result
     end
+    if result.size == 0
+      result = nil
+    end
+    result
   end
 
-  def get_weight(nt, col)
-    case nt
-    when 'a', 'A'
+  def get_weight_na(na, col)
+    case na.downcase
+    when 'a'
       return self[col, 0]
-    when 'c', 'C'
+    when 'c'
       return self[col, 1]
-    when 'g', 'G'
+    when 'g'
       return self[col, 2]
-    when 't', 'T'
+    when 't'
       return self[col, 3]
     else
       return 0
@@ -69,57 +61,55 @@ class BioMatrix < Matrix
   end
 
   def get_max
-    max = 0
-    tmp = 0
     sum = 0
     for i in (0 .. self.row_size - 1)
-      for j in (0 .. 3)
+      max = 0
+      for j in (0 .. self.column_size - 1)
         tmp = self[i, j]
         if max < tmp
           max = tmp
         end
       end
       sum += max
-      max = 0
     end
     sum
   end
 
 end
 
-class BioMatrixException<StandardError
-
-end
 
 class BioVector < Vector
+
+  include Math
 
   def cc(v)
     Vector.Raise ErrDimensionMismatch if size != v.size
 
-    @x_mean = self.mean
-    @y_mean = v.mean
-    @x_total = 0.0
-    @y_total = 0.0
-    @s_total = 0.0
-    each2(v) {
-      |x, y|
-      @x_total += (x - @x_mean)*(x - @x_mean)
-      @y_total += (y - @y_mean)*(y - @y_mean)
-      @s_total += (x - @x_mean)*(y - @y_mean)
-    }
-    if sqrt( @x_total * @y_total ) != 0
-      @s_total/sqrt( @x_total * @y_total )
+    x_mean = self.mean
+    y_mean = v.mean
+    x_total = 0.0
+    y_total = 0.0
+    s_total = 0.0
+
+    each2(v) do |x, y|
+      x_total += (x - x_mean)*(x - x_mean)
+      y_total += (y - y_mean)*(y - y_mean)
+      s_total += (x - x_mean)*(y - y_mean)
+    end
+
+    if sqrt(x_total * y_total) != 0.0
+      s_total/sqrt(x_total * y_total)
     else
       0.0
     end
   end
 
   def mean
-    @sum = 0.0
+    sum = 0.0
     0.upto(size - 1) do |i|
-      @sum += self[i]
+      sum += self[i]
     end
-    @sum/size
+    sum/size
   end
 
 end
