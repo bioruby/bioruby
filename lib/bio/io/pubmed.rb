f0c5fca89fa6736b5db3489819b5a4e0486e6b06
Toshiaki Katayama <k@bioruby.org>
2001-09-12 07:27:58 +0000

    * initial version retrive and parse a PubMed entry

---
+++ lib/bio/io/pubmed.rb
@@ -0,0 +1,276 @@
+#
+# bio/io/pubmed.rb - NCBI PubMed client class
+#
+#   Copyright (C) 2001 KATAYAMA Toshiaki <k@bioruby.org>
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Library General Public
+#  License as published by the Free Software Foundation; either
+#  version 2 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Library General Public License for more details.
+#
+#  $Id: pubmed.rb,v 1.1 2001/09/12 07:27:58 katayama Exp $
+#
+#  For more informations :
+#    http://www.ncbi.nlm.nih.gov/entrez/query/static/overview.html
+#    http://www.ncbi.nlm.nih.gov/entrez/query/static/linking.html
+#    http://www.ncbi.nlm.nih.gov/entrez/query/static/help/pmhelp.html#MEDLINEDisplayFormat
+#    http://www.ncbi.nlm.nih.gov/entrez/query/static/help/pmhelp.html#SearchFieldDescriptionsandTags
+#    http://www.ncbi.nlm.nih.gov/entrez/utils/utils_index.html
+#    http://www.ncbi.nlm.nih.gov/entrez/utils/pmfetch_help.html
+#
+
+require 'net/http'
+
+
+class PubMed
+
+  def initialize(id)
+    host = "www.ncbi.nlm.nih.gov"
+#   path = "/entrez/utils/pmfetch.fcgi?db=PubMed&mode=text&report=medline&id="
+#   path = "/entrez/query.fcgi?cmd=Text&db=PubMed&dopt=MEDLINE&uid="
+    path = "/entrez/query.fcgi?cmd=Search&db=PubMed&doptcmdl=MEDLINE&term="
+    http = Net::HTTP.new(host)
+
+    @pubmed	= Hash.new('')
+    result	= http.get(path + id).pop
+
+    if result =~ /#{id}\s+Error/
+      raise( result )
+    else
+#     result.gsub!(/<\/?pre>/, '')
+      result.sub!(/.*<pre>/, '')
+      result.sub!(/<\/pre>.*/, '')
+      tag = ''
+      result.each_line do |line|
+	line.chomp!		# some entry have \r or \r\n
+        if line =~ /^\w/
+          tag = line[0,4].strip
+        end
+        @pubmed[tag] += line[6..-1] if line.length > 6
+      end
+    end
+  end
+  attr_reader :pubmed
+
+
+  ### Common MEDLINE tags
+
+  # PMID - PubMed Unique Identifier
+  #   Unique number assigned to each PubMed citation.
+  def pmid
+    @pubmed['PMID'].strip
+  end
+  alias pubmedid pmid
+
+  # UI   - MEDLINE Unique Identifier
+  #   Unique number assigned to each MEDLINE citation.
+  def ui
+    @pubmed['UI'].strip
+  end
+  alias medlineid ui
+
+  # TA   - Journal Title Abbreviation
+  #   Standard journal title abbreviation.
+  def ta
+    @pubmed['TA'].gsub(/\s+/, ' ').strip
+  end
+  alias journal ta
+
+  # VI   - Volume
+  #   Journal volume.
+  def vi
+    @pubmed['VI'].strip
+  end
+  alias volume vi
+
+  # IP   - Issue
+  #   The number of the issue, part, or supplement of the journal in which
+  #   the article was published.
+  def ip
+    @pubmed['IP'].strip
+  end
+  alias issue ip
+
+  # PG   - Page Number
+  #   The full pagination of the article.
+  def pg
+    @pubmed['PG'].strip
+  end
+  alias pages pg
+
+  # DP   - Publication Date
+  #   The date the article was published.
+  def dp
+    @pubmed['DP'].strip
+  end
+  alias date dp
+
+  def year
+    dp[0,4]
+  end
+
+  # TI   - Title Words
+  #   The title of the article.
+  def ti
+    @pubmed['TI'].gsub(/\s+/, ' ').strip
+  end
+  alias title ti
+
+  # AB   - Abstract
+  #   Abstract.
+  def ab
+    @pubmed['AB'].gsub(/\s+/, ' ').strip
+  end
+  alias abstract ab
+
+  # AU   - Author Name
+  #   Authors' names.
+  def au
+    @pubmed['AU'].strip.split(/\n/)
+  end
+  alias authors au
+
+  # SO   - Source
+  #   Composite field containing bibliographic information.
+  def so
+    @pubmed['SO'].strip
+  end
+  alias source so
+
+  # MH   - MeSH Terms
+  #   NLM's controlled vocabulary.
+  def mh
+    @pubmed['MH'].strip.split(/\n/)
+  end
+  alias mesh mh
+
+
+  ### Other MEDLINE tags
+
+  # AD   - Affiliation
+  #   Institutional affiliation and address of the first author, and grant
+  #   numbers.
+
+  # AID  - Article Identifier
+  #   Article ID values may include the pii (controlled publisher identifier)
+  #   or doi (Digital Object Identifier).
+
+  # CI   - Copyright Information
+  #   Copyright statement.
+
+  # CIN  - Comment In
+  #   Reference containing a comment about the article.
+
+  # CN   - Collective Name
+  #   Corporate author or group names with authorship responsibility.
+
+  # CON  - Comment On
+  #   Reference upon which the article comments.
+
+  # CY   - Country
+  #   The place of publication of the journal.
+
+  # DA   - Date Created
+  #   Used for internal processing at NLM.
+
+  # DCOM - Date Completed
+  #   Used for internal processing at NLM.
+
+  # DEP  - Date of Electronic Publication
+  #   Electronic publication date.
+
+  # EDAT - Entrez Date
+  #   The date the citation was added to PubMed.
+
+  # EIN  - Erratum In
+  #   Reference containing a published erratum to the article.
+
+  # GS   - Gene Symbol
+  #   Abbreviated gene names (used 1991 through 1996).
+
+  # ID   - Identification Number 
+  #   Research grant numbers, contract numbers, or both that designate
+  #   financial support by any agency of the US PHS (Public Health Service).
+
+  # IS   - ISSN
+  #   International Standard Serial Number of the journal.
+
+  # JC   - Journal Title Code
+  #   MEDLINE unique three-character code for the journal.
+
+  # JID  - NLM Unique ID
+  #   Unique journal ID in NLM's catalog of books, journals, and audiovisuals.
+
+  # LA   - Language
+  #   The language in which the article was published.
+
+  # LR   - Last Revision Date
+  #   The date a change was made to the record during a maintenance procedure.
+
+  # MHDA - MeSH Date
+  #   The date MeSH terms were added to the citation. The MeSH date is the
+  #   same as the Entrez date until MeSH are added.
+
+  # PHST - Publication History Status Date
+  #   History status date.
+
+  # PS   - Personal Name as Subject
+  #   Individual is the subject of the article.
+
+  # PST  - Publication Status
+  #   Publication status.
+
+  # PT   - Publication Type
+  #   The type of material the article represents.
+
+  # RF   - Number of References
+  #   Number of bibliographic references for Review articles.
+
+  # RIN  - Retraction In
+  #   Retraction of the article
+
+  # RN   - EC/RN Number
+  #   Number assigned by the Enzyme Commission to designate a particular
+  #   enzyme or by the Chemical Abstracts Service for Registry Numbers.
+
+  # ROF  - Retraction Of
+  #   Article being retracted.
+
+  # RPF  - Republished From
+  #   Original article.
+
+  # SB   - Journal Subset
+  #   Code for a specific set of journals.
+
+  # SI   - Secondary Source Identifier
+  #   Identifies a secondary source that supplies information, e.g., other
+  #   data sources, databanks and accession numbers of molecular sequences
+  #   discussed in articles.
+
+  # TT   - Transliterated / Vernacular Title 
+  #   Non-Roman alphabet language titles are transliterated.
+
+  # UIN  - Update In
+  #   Update to the article.
+
+  # UOF  - Update Of
+  #   The article being updated.
+
+  # URLF - URL Full-Text
+  #   Link to the full-text of article at provider's website. Links are
+  #   incomplete. Use PmLink for the complete set of available links.
+  #   [PmLink] http://www.ncbi.nlm.nih.gov/entrez/utils/pmlink_help.html
+
+  # URLS - URL Summary
+  #   Link to the article summary at provider's website. Links are
+  #   incomplete. Use PmLink for the complete set of available links.
+  #   [PmLink] http://www.ncbi.nlm.nih.gov/entrez/utils/pmlink_help.html
+
+end
+
+
