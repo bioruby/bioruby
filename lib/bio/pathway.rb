bf7d9e1d4ce32dfbd2a4a8f3a25c527c74eb4f57
Toshiaki Katayama <k@bioruby.org>
2001-11-12 21:24:05 +0000

    * reformatted

--- lib/bio/pathway.rb
+++ lib/bio/pathway.rb
@@ -17,8 +17,212 @@
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
-#  $Id: pathway.rb,v 1.5 2001/11/06 16:58:52 okuji Exp $
+#  $Id: pathway.rb,v 1.6 2001/11/12 21:24:05 katayama Exp $
 #
+
+require 'bio/matrix'
+
+module Bio
+
+  class Pathway
+
+    # Graph (adjacency list) generation from the list of Relation
+    def initialize(list, undirected = nil)
+      @undirected = undirected
+      @index = {}
+      @label = {}
+      @graph = {}
+      list.each do |rel|
+	append(rel)
+      end
+    end
+    attr_accessor :label
+    attr_reader :graph, :index
+
+    def append(rel)
+      if @graph[rel.from].nil?
+	@graph[rel.from] = {}
+      end
+      if @graph[rel.to].nil?
+	@graph[rel.to] = {}
+      end
+      @graph[rel.from][rel.to] = rel.relation
+      @graph[rel.to][rel.from] = rel.relation if @undirected
+    end
+
+    def nodes
+      @graph.keys.length
+    end
+
+    def edges
+      edges = 0
+      @graph.each_value do |v|
+	edges += v.size
+      end
+      edges
+    end
+
+    # Convert adjacency list to adjacency matrix
+    def to_matrix
+      @graph.keys.each_with_index do |k, i|
+	@index[k] = i
+      end
+
+      # note : following code only makes reference to the same []
+      #
+      #   matrix = Array.new(nodes, Array.new(nodes))
+      #
+      # so create each Array object as follows:
+
+      matrix = Array.new
+      nodes.times do |i|
+	matrix.push(Array.new(nodes))
+      end
+
+      @graph.each do |from, hash|
+	hash.each do |to, relation|
+	  x = @index[from]
+	  y = @index[to]
+	  matrix[x][y] = relation
+	end
+      end
+      Matrix[*matrix]
+    end
+
+    # Select labeled nodes and generate subgraph
+    def subgraph
+      sub_graph = Pathway.new([], @undirected)
+      @graph.each do |from, hash|
+	next unless @label[from]
+	hash.each do |to, relation|
+	  next unless @label[to]
+	  sub_graph.append(Relation.new(from, to, relation))
+	end
+      end
+      return sub_graph
+    end
+
+    def common_subgraph(graph)
+      raise NotImplementedError
+    end
+
+    def clique
+      raise NotImplementedError
+    end
+
+    # returns frequency of the nodes having same number of edges as hash
+    def small_world
+      freq = Hash.new(0)
+      @graph.each_value do |v|
+	freq[v.size] += 1
+      end
+      return freq
+    end
+
+    # breadth first search implementation from the textbook
+    def breadth_first_search(root)
+      white = -1; gray = 0; black = 1
+
+      color = Hash.new(white)
+      distance = {}
+      predecessor = {}
+
+      color[root] = gray
+      distance[root] = 0
+      predecessor[root] = nil
+
+      queue = [ root ]
+
+      while from = queue.shift
+	@graph[from].keys.each do |to|
+	  if color[to] == white
+	    color[to] = gray
+	    distance[to] = distance[from] + 1
+	    predecessor[to] = from
+	    queue.push(to)
+	  end
+	end
+	color[from] = black
+      end
+
+      return distance, predecessor
+    end
+    alias bfs breadth_first_search
+
+    # simplified version of bfs (not use colors, not return predecessor)
+    def bfs_distance(root)
+      seen = {}
+      distance = {}
+
+      seen[root] = true
+      distance[root] = 0
+
+      queue = [ root ]
+
+      while from = queue.shift
+	@graph[from].keys.each do |to|
+	  unless seen[to]
+	    seen[to] = true
+	    distance[to] = distance[from] + 1
+	    queue.push(to)
+	  end
+	end
+      end
+      return distance
+    end
+
+    # simple application of bfs
+    def shortest_path(node1, node2)
+      distance, route = breadth_first_search(node1)
+      step = distance[node2]
+      node = node2
+      path = [ node2 ]
+      while node != node1 and route[node]
+	node = route[node]
+	path.unshift(node)
+      end
+      return step, path
+    end
+
+    def dijkstra
+      raise NotImplementedError
+    end
+
+    def floyd
+      raise NotImplementedError
+    end
+
+  end
+
+
+  class Relation
+
+    def initialize(node1, node2, edge)
+      @node = [node1, node2]
+      @edge = edge
+    end
+    attr_accessor :node, :edge
+
+    def from
+      @node[0]
+    end
+
+    def to
+      @node[1]
+    end
+
+    def relation
+      @edge
+    end
+
+  end
+
+end
+
+
+if __FILE__ == $0
+  # test code here
+end
 
 =begin
 
@@ -58,197 +262,4 @@
 --- Bio::Relation#relation
 
 =end
-
-
-module Bio
-
-require 'bio/matrix'
-
-class Pathway
-
-  # Graph (adjacency list) generation from the list of Relation
-  def initialize(list, undirected = nil)
-    @undirected = undirected
-    @index = {}
-    @label = {}
-    @graph = {}
-    list.each do |rel|
-      append(rel)
-    end
-  end
-  attr_accessor :label
-  attr_reader :graph, :index
-
-  def append(rel)
-    if @graph[rel.from].nil?
-      @graph[rel.from] = {}
-    end
-    if @graph[rel.to].nil?
-      @graph[rel.to] = {}
-    end
-    @graph[rel.from][rel.to] = rel.relation
-    @graph[rel.to][rel.from] = rel.relation if @undirected
-  end
-
-  def nodes
-    @graph.keys.length
-  end
-
-  def edges
-    edges = 0
-    @graph.each_value do |v|
-      edges += v.size
-    end
-    edges
-  end
-
-  # Convert adjacency list to adjacency matrix
-  def to_matrix
-    @graph.keys.each_with_index do |k, i|
-      @index[k] = i
-    end
-
-    # note : following code only makes reference to the same []
-    #matrix = Array.new(nodes, Array.new(nodes))
-
-    matrix = Array.new
-    nodes.times do |i|
-      matrix.push(Array.new(nodes))
-    end
-
-    @graph.each do |from, hash|
-      hash.each do |to, relation|
-	x = @index[from]
-	y = @index[to]
-	matrix[x][y] = relation
-      end
-    end
-    Matrix[*matrix]
-  end
-
-  # Select labeled nodes and generate subgraph
-  def subgraph
-    sub_graph = Pathway.new([], @undirected)
-    @graph.each do |from, hash|
-      next unless @label[from]
-      hash.each do |to, relation|
-	next unless @label[to]
-	sub_graph.append(Relation.new(from, to, relation))
-      end
-    end
-    return sub_graph
-  end
-
-  def common_subgraph(graph)
-    raise NotImplementedError
-  end
-
-  def clique
-    raise NotImplementedError
-  end
-
-  def small_world
-    freq = Hash.new(0)
-    @graph.each_value do |v|
-      freq[v.size] += 1
-    end
-    return freq
-  end
-
-  def breadth_first_search(root)
-    white = -1; gray = 0; black = 1
-
-    color = Hash.new(white)
-    distance = {}
-    predecessor = {}
-
-    color[root] = gray
-    distance[root] = 0
-    predecessor[root] = nil
-
-    queue = [ root ]
-
-    while from = queue.shift
-      @graph[from].keys.each do |to|
-	if color[to] == white
-	  color[to] = gray
-	  distance[to] = distance[from] + 1
-	  predecessor[to] = from
-	  queue.push(to)
-	end
-      end
-      color[from] = black
-    end
-
-    return distance, predecessor
-  end
-  alias bfs breadth_first_search
-
-  def bfs_distance(root)
-    seen = {}
-    distance = {}
-
-    seen[root] = true
-    distance[root] = 0
-
-    queue = [ root ]
-
-    while from = queue.shift
-      @graph[from].keys.each do |to|
-	unless seen[to]
-	  seen[to] = true
-	  distance[to] = distance[from] + 1
-	  queue.push(to)
-	end
-      end
-    end
-    return distance
-  end
-
-  def shortest_path(node1, node2)
-    distance, route = breadth_first_search(node1)
-    step = distance[node2]
-    node = node2
-    path = [ node2 ]
-    while node != node1 and route[node]
-      node = route[node]
-      path.unshift(node)
-    end
-    return step, path
-  end
-
-  def dijkstra
-    raise NotImplementedError
-  end
-
-  def floyd
-    raise NotImplementedError
-  end
-
-end
-
-
-class Relation
-
-  def initialize(node1, node2, edge)
-    @node = [node1, node2]
-    @edge = edge
-  end
-  attr_accessor :node, :edge
-
-  def from
-    @node[0]
-  end
-
-  def to
-    @node[1]
-  end
-
-  def relation
-    @edge
-  end
-
-end
-
-end				# module Bio
 
--- lib/bio/reference.rb
+++ lib/bio/reference.rb
@@ -17,8 +17,180 @@
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
-#  $Id: reference.rb,v 1.6 2001/11/06 16:58:52 okuji Exp $
+#  $Id: reference.rb,v 1.7 2001/11/12 21:27:24 katayama Exp $
 #
+
+module Bio
+
+  class Reference
+
+    def initialize(hash)
+      @authors	= hash['authors']	# [ "Hoge, J.P.", "Fuga, F.B." ]
+      @title	= hash['title']		# "Title of the study."
+      @journal	= hash['journal']	# "Theor. J. Hoge"
+      @volume	= hash['volume']	# 12
+      @issue	= hash['issue']		# 3
+      @pages	= hash['pages']		# 123-145
+      @year	= hash['year']		# 2001
+      @pubmed	= hash['pubmed']	# 12345678
+      @medline	= hash['medline']	# 98765432
+    end
+    attr_reader :authors, :title, :journal, :volume, :issue, :pages, :year,
+      :pubmed, :medline
+
+    def format(style = nil, option = nil)
+      case style
+      when /^nature$/i
+	return nature(option)
+      when /^science$/i
+	return science
+      when /^genome\s*biology$/i
+	return genomebiology
+      when /^genome\s*res/i
+	return genomeres
+      when /^nar$/i
+	return nar
+      when /^current/i
+	return current
+      when /^trends/i
+	return trends
+      when /^cell$/i
+	return cell
+      when 'bibitem'
+	return bibitem(option)
+      when 'bibtex'
+	return bibtex(option)
+      else
+	return general
+      end
+    end
+
+    def bibitem(item = nil)
+      item  = "PMID@pubmed" unless item
+      pages = @pages.sub('-', '--')
+      return <<-"END".collect {|line| line.strip}.join("\n")
+	\\bibitem{#{item}}
+	#{@authors.join(', ')}
+	#{@title},
+	{\\em #{@journal}}, #{@volume}(#{@issue}):#{pages}, #{@year}.
+      END
+    end
+
+    def bibtex(section = nil)
+      section = "article" unless section
+      authors = authors_join(' and ')
+      pages   = @pages.sub('-', '--')
+      return <<-END
+	@#{section}{PMID#{@pubmed},
+	  author  = {#{authors}},
+	  title   = {#{@title}},
+	  journal = {#{@journal}},
+	  year    = {#{@year}},
+	  volume  = {#{@volume}},
+	  number  = {#{@issue}},
+	  pages   = {#{pages}},
+	}
+      END
+    end
+
+    def nature(short = nil)
+      if short
+	if @authors.size > 4
+	  authors = "#{@authors[0]} et al."
+	elsif @authors.size == 1
+	  authors = "#{@authors[0]}"
+	else
+	  authors = authors_join(' & ')
+	end
+	"#{authors} #{@journal} #{@volume}, #{@pages} (#{@year})."
+      else
+	authors = authors_join(' & ')
+	"#{authors} #{@title} #{@journal} #{@volume}, #{@pages} (#{@year})."
+      end
+    end
+
+    def science
+      if @authors.size > 4
+	authors = rev_name(@authors[0]) + " et al."
+      else
+	authors = @authors.collect {|name| rev_name(name)}.join(', ')
+      end
+      page_from, = @pages.split('-')
+      "#{authors}, #{@journal} #{@volume} #{page_from} (#{@year})."
+    end
+
+    def genomebiology
+      authors = @authors.collect {|name| strip_dots(name)}.join(', ')
+      journal = strip_dots(@journal)
+      "#{authors}: #{@title} #{journal} #{@year}, #{@volume}:#{@pages}."
+    end
+    alias current genomebiology
+
+    def genomeres
+      authors = authors_join(' and ')
+      "#{authors} #{@year}.\n  #{@title} #{@journal} #{@volume}: #{@pages}."
+    end
+
+    def nar
+      authors = authors_join(' and ')
+      "#{authors} (#{@year}) #{@title} #{@journal}, #{@volume}, #{@pages}."
+    end
+
+    def cell
+      authors = authors_join(' and ')
+      "#{authors} (#{@year}). #{@title} #{@journal} #{@volume}, #{pages}."
+    end
+
+    def trends
+      if @authors.size > 2
+	authors = "#{@authors[0]} et al."
+      elsif @authors.size == 1
+	authors = "#{@authors[0]}"
+      else
+	authors = authors_join(' and ')
+      end
+      "#{authors} (#{@year}) #{@title} #{@journal} #{@volume}, #{@pages}"
+    end
+
+    def general
+      authors = @authors.collect {|name| strip_dots(name)}.join(', ')
+      journal = strip_dots(@journal)
+      "#{authors} \"#{@title}\", #{journal} #{@volume}:#{@pages} (#{@year})"
+    end
+
+
+    private
+
+    def strip_dots(data)
+      data.tr(',.', '')
+    end
+
+    def authors_join(amp, sep = ', ')
+      authors = @authors.clone
+      if authors.length > 1
+	last = authors.pop
+	authors = authors.join(sep) + "#{amp}" + last
+      else
+	authors = authors.pop
+      end
+    end
+
+    def rev_name(name)
+      if name =~ /,/
+	name, initial = name.split(/,\s+/)
+	name = "#{initial} #{name}"
+      end
+      return name
+    end
+
+  end
+
+end
+
+
+if __FILE__ == $0
+end
+
 
 =begin
 
@@ -51,170 +223,4 @@
 
 =end
 
-
-module Bio
-
-class Reference
-
-  def initialize(hash)
-    @authors	= hash['authors']	# [ "Hoge, J.P.", "Fuga, F.B." ]
-    @title	= hash['title']		# "Title of the study."
-    @journal	= hash['journal']	# "Theor. J. Hoge"
-    @volume	= hash['volume']	# 12
-    @issue	= hash['issue']		# 3
-    @pages	= hash['pages']		# 123-145
-    @year	= hash['year']		# 2001
-    @pubmed	= hash['pubmed']	# 12345678
-    @medline	= hash['medline']	# 98765432
-  end
-  attr_reader :authors, :title, :journal, :volume, :issue, :pages, :year, :pubmed, :medline
-
-  def format(style = nil, option = nil)
-    case style
-    when /^nature$/i
-      return nature(option)
-    when /^science$/i
-      return science
-    when /^genome\s*biology$/i
-      return genomebiology
-    when /^genome\s*res/i
-      return genomeres
-    when /^nar$/i
-      return nar
-    when /^current/i
-      return current
-    when /^trends/i
-      return trends
-    when /^cell$/i
-      return cell
-    when 'bibitem'
-      return bibitem(option)
-    when 'bibtex'
-      return bibtex(option)
-    else
-      return general
-    end
-  end
-
-  def bibitem(item = nil)
-    item  = "PMID@pubmed" unless item
-    pages = @pages.sub('-', '--')
-    return <<-"END".collect {|line| line.strip}.join("\n")
-	\\bibitem{#{item}}
-	#{@authors.join(', ')}
-	#{@title},
-	{\\em #{@journal}}, #{@volume}(#{@issue}):#{pages}, #{@year}.
-    END
-  end
-
-  def bibtex(section = nil)
-    section = "article" unless section
-    authors = authors_join(' and ')
-    pages   = @pages.sub('-', '--')
-    return <<-END
-	@#{section}{PMID#{@pubmed},
-	  author  = {#{authors}},
-	  title   = {#{@title}},
-	  journal = {#{@journal}},
-	  year    = {#{@year}},
-	  volume  = {#{@volume}},
-	  number  = {#{@issue}},
-	  pages   = {#{pages}},
-	}
-    END
-  end
-
-  def nature(short = nil)
-    if short
-      if @authors.size > 4
-	authors = "#{@authors[0]} et al."
-      elsif @authors.size == 1
-	authors = "#{@authors[0]}"
-      else
-	authors = authors_join(' & ')
-      end
-      "#{authors} #{@journal} #{@volume}, #{@pages} (#{@year})."
-    else
-      authors = authors_join(' & ')
-      "#{authors} #{@title} #{@journal} #{@volume}, #{@pages} (#{@year})."
-    end
-  end
-
-  def science
-    if @authors.size > 4
-      authors = rev_name(@authors[0]) + " et al."
-    else
-      authors = @authors.collect {|name| rev_name(name)}.join(', ')
-    end
-    page_from, = @pages.split('-')
-    "#{authors}, #{@journal} #{@volume} #{page_from} (#{@year})."
-  end
-
-  def genomebiology
-    authors = @authors.collect {|name| strip_dots(name)}.join(', ')
-    journal = strip_dots(@journal)
-    "#{authors}: #{@title} #{journal} #{@year}, #{@volume}:#{@pages}."
-  end
-  alias current genomebiology
-
-  def genomeres
-    authors = authors_join(' and ')
-    "#{authors} #{@year}.\n  #{@title} #{@journal} #{@volume}: #{@pages}."
-  end
-
-  def nar
-    authors = authors_join(' and ')
-    "#{authors} (#{@year}) #{@title} #{@journal}, #{@volume}, #{@pages}."
-  end
-
-  def cell
-    authors = authors_join(' and ')
-    "#{authors} (#{@year}). #{@title} #{@journal} #{@volume}, #{pages}."
-  end
-
-  def trends
-    if @authors.size > 2
-      authors = "#{@authors[0]} et al."
-    elsif @authors.size == 1
-      authors = "#{@authors[0]}"
-    else
-      authors = authors_join(' and ')
-    end
-    "#{authors} (#{@year}) #{@title} #{@journal} #{@volume}, #{@pages}"
-  end
-
-  def general
-    authors = @authors.collect {|name| strip_dots(name)}.join(', ')
-    journal = strip_dots(@journal)
-    "#{authors} \"#{@title}\", #{journal} #{@volume}:#{@pages} (#{@year})"
-  end
-
-
-  private
-
-  def strip_dots(data)
-    data.tr(',.', '')
-  end
-
-  def authors_join(amp, sep = ', ')
-    authors = @authors.clone
-    if authors.length > 1
-      last = authors.pop
-      authors = authors.join(sep) + "#{amp}" + last
-    else
-      authors = authors.pop
-    end
-  end
-
-  def rev_name(name)
-    if name =~ /,/
-      name, initial = name.split(/,\s+/)
-      name = "#{initial} #{name}"
-    end
-    return name
-  end
-
-end
-
-end				# module Bio
 
