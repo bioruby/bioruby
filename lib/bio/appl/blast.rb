7735501598ab35223835526ae1672eda57f3a1e6
Mitsuteru Nakao <n@bioruby.org>
2001-11-12 22:07:14 +0000

    Add Bio::Blast class for blasting on the local machine.

--- lib/bio/appl/blast.rb
+++ lib/bio/appl/blast.rb
@@ -1,5 +1,5 @@
 #
-# bio/appl/blast.rb - NCBI_BlastOutput.dtd XML Blast Report Parser
+# bio/appl/blast.rb - BLAST Execute Factory and Report Parser
 # 
 #   Copyright (C) 2001 Mitsuteru S. Nakao <n@bioruby.org>
 #
@@ -17,16 +17,127 @@
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
-#  $Id: blast.rb,v 1.2 2001/11/12 20:53:50 katayama Exp $
+#  $Id: blast.rb,v 1.3 2001/11/12 22:07:14 nakao Exp $
 #
 
 require 'xmlparser'
 require 'bio'
 
 module Bio
-
   class Blast
+    class BioBlastError < Exception; end
 
+    # opts = {'-m' => 7, '-e' => 0.0 , ...}
+    def initialize (server, blastall, program, db, opts)
+      @server = server
+      @blastall =  blastall
+      @io = nil
+      @program = program
+      @db = db
+      @opts = opts
+    end
+    attr_accessor :server, :program, :db, :opts
+    ##
+    # constructor for local blastall
+    # blastall: /path/to/blastall
+    # program: (blastp|blastn|tblastx|blasty|...)
+    # db: /path/to/db
+    # opts = {'-e' => 0.1, ... } 
+    def Blast.local (blastall, program, db, opts = nil)
+      self.new(nil, blastall, program, db, opts)
+    end
+
+    ##
+    # constructor for remote blast server
+    # server = URI in String
+    def Blast.remote (server, program, db, opts = nil)
+      self.new(server, nil, program, db, opts)
+    end
+    #
+    # constructor for remote blast server on NCBI
+    def Blast.ncbi (program, db, opts = nil)
+      server = 'http://www.ncbi.nlm.nih.gov/blast/blast.cgi'
+      self.new(server, nil, program, db, opts)
+    end
+    #
+    # constructor for remote blast server on GenomeNet
+    def Blast.genomenet (program, db, opts = nil)
+      server = 'http://blast.genomenet.ad.jp/sit-bin/nph-blast'
+      self.new(server, nil, program, db, opts)
+    end
+
+    
+    ##
+    # query in (multi) fasta format file in String
+    def query(query) 
+      if @server
+	return remote_blast(query)
+      elsif @blastall
+	return local_blast(query)
+      end
+    end
+    alias exec query
+    ##
+    # execute blastall in remote server
+    def remote_blast(query)
+      raise "Not yet implimented"
+    end
+    private :remote_blast
+    ##
+    # execute blastall in local machine
+    def local_blast(query)
+      cmd = "#{@blastall} -p #{@program} -d #{@db} -m 7"
+
+      @opts.each { |k,v|
+	if k =~ /^-[eFGEXIqrvbfgQDaOJMWzkPYSTlUyZRnLA]$/
+	  cmd += " #{k} #{v} "
+	else
+	  raise "Error: Invalid option #{k} #{v}"
+	end
+      } if @opts.type == Hash
+
+      begin 
+	@io = IO.popen(cmd, "w+")
+	@io.sync = 1
+	@io.puts(query)
+	@io.close_write
+
+	#
+	# Multi Queries, Multi XML Reports
+	return @io.read.split("\n<\\?x").collect { |entry|	
+	  unless entry =~ /^<.x/
+	    Report.new('<?x'+entry)
+	  else
+	    Report.new(entry)
+	  end
+	}
+
+      rescue BioBlastError
+	raise "Error: #{$!} #{cmd} \n"
+      ensure
+	@io.close
+      end
+
+    end
+    private :local_blast
+
+    ## ``blastall'' and ``db'' check methods
+    #
+    # Executable file?
+    def blastall?
+      File.stat(@blastall).executable_real?
+    end
+    #
+    # Readable db ?
+    def db?
+      File.stat(@db).readable_real?
+    end
+
+
+
+    #
+    # Blast (-m 7) XML Report Parser Class
+    # This class is tested blastn -m 7 report only.
     class Report
 
       class XMLRetry < Exception; end
@@ -97,7 +208,7 @@ module Bio
 		@query_def = entry[tag_stack.last]
 		entry = Hash.new  
 	      when 'BlastOutput_query-len'
-		@query_len = entry[tag_stack.last]
+		@query_len = entry[tag_stack.last].to_i
 		entry = Hash.new  
 
 	      when 'Parameters'
@@ -105,11 +216,12 @@ module Bio
 		entry = Hash.new
 		  
 	      when 'Iteration_iter-num'
-		@iteration.last.num = entry[tag_stack.last].clone
+		@iteration.last.num = entry[tag_stack.last].clone.to_i
 		entry = Hash.new
 		
 	      when 'Hit_num'
-		@iteration.last.hit.last.num = entry[tag_stack.last].clone
+		@iteration.last.hit.last.num \
+                                           = entry[tag_stack.last].clone.to_i
 		entry = Hash.new
 	      when 'Hit_id'
 		@iteration.last.hit.last._id = entry[tag_stack.last].clone
@@ -118,10 +230,12 @@ module Bio
 		@iteration.last.hit.last._def = entry[tag_stack.last].clone
 		entry = Hash.new
 	      when 'Hit_accession'
-		@iteration.last.hit.last.accession = entry[tag_stack.last].clone
+		@iteration.last.hit.last.accession \
+                                           = entry[tag_stack.last].clone
 		entry = Hash.new
 	      when 'Hit_len'
-		@iteration.last.hit.last.len = entry[tag_stack.last].clone
+		@iteration.last.hit.last.len \
+                                           = entry[tag_stack.last].clone.to_i
 		entry = Hash.new
 
 	      when 'Hsp'
@@ -173,7 +287,7 @@ module Bio
 	def hits
 	  hit
 	end
-      end
+      end                       # class Iteration
 
       # Bio::Blast::Report::Hit
       class Hit
@@ -192,7 +306,7 @@ module Bio
 	def hsps
 	  hsp
 	end
-      end
+      end                       # class Hit
 
       # Bio::Blast::Report::Hsp
       class Hsp
@@ -222,7 +336,7 @@ module Bio
 	  :query_to, :hit_from, :hit_to, :pattern_from, :pattern_to, 
 	  :query_frame, :hit_frame, :identity, :positive, :gaps, :align_len, 
 	  :density, :qseq, :hseq, :midline
-      end
+      end                       # class Hsp
 
       protected
 
@@ -237,7 +351,7 @@ module Bio
 	  'filter' => 'Parameters_filter'
 	}
 	labels.each do |k,v|
-	  @parameters[k] = hash[v]
+	  @parameters[k] = hash[v].to_i unless k == 'filter'
 	end
       end
 
@@ -252,30 +366,38 @@ module Bio
 	  'entropy' => 'Statistics_entropy'
 	}
 	labels.each do |k,v|
-	  @statistics[k] = hash[v]
+	  if k == 'dn-num' or k == 'db-len' or k == 'hsp-len'
+	    @statistics[k] = hash[v].to_i
+	  else
+	    @statistics[k] = hash[v].to_f
+	  end
 	end
       end
 
       def parse_hsp(h)
-	@iteration.last.hit.last.hsp.last.num = h['Hsp_num']
-	@iteration.last.hit.last.hsp.last.bit_score = h['Hsp_bit-score']
-	@iteration.last.hit.last.hsp.last.score = h['Hsp_score']
-	@iteration.last.hit.last.hsp.last.evalue = h['Hsp_evalue']
-	@iteration.last.hit.last.hsp.last.query_from = h['Hsp_query-from']
-	@iteration.last.hit.last.hsp.last.query_to = h['Hsp_query-to']
-	@iteration.last.hit.last.hsp.last.hit_from = h['Hsp_hit-from']
-	@iteration.last.hit.last.hsp.last.hit_to = h['Hsp_hit-to']
-	@iteration.last.hit.last.hsp.last.pattern_from = h['Hsp_pattern-from']
-	@iteration.last.hit.last.hsp.last.pattern_to = h['Hsp_pattern-to']
-	@iteration.last.hit.last.hsp.last.query_frame = h['Hsp_query-frame']
-	@iteration.last.hit.last.hsp.last.hit_frame = h['Hsp_hit-frame']
-	@iteration.last.hit.last.hsp.last.identity = h['Hsp_identity']
-	@iteration.last.hit.last.hsp.last.positive = h['Hsp_positive']
-	@iteration.last.hit.last.hsp.last.gaps = h['Hsp_gaps']
-	@iteration.last.hit.last.hsp.last.align_len = h['Hsp_align-len']
-	@iteration.last.hit.last.hsp.last.density = h['Hsp_density']
-	@iteration.last.hit.last.hsp.last.qseq = h['Hsp_qseq']
-	@iteration.last.hit.last.hsp.last.hseq = h['Hsp_hseq']
+	@iteration.last.hit.last.hsp.last.num = h['Hsp_num'].to_i
+	@iteration.last.hit.last.hsp.last.bit_score = h['Hsp_bit-score'].to_f
+	@iteration.last.hit.last.hsp.last.score = h['Hsp_score'].to_i
+	@iteration.last.hit.last.hsp.last.evalue = h['Hsp_evalue'].to_f
+	@iteration.last.hit.last.hsp.last.query_from \
+                                        = h['Hsp_query-from'].to_i
+	@iteration.last.hit.last.hsp.last.query_to = h['Hsp_query-to'].to_i
+	@iteration.last.hit.last.hsp.last.hit_from = h['Hsp_hit-from'].to_i
+	@iteration.last.hit.last.hsp.last.hit_to = h['Hsp_hit-to'].to_i
+	@iteration.last.hit.last.hsp.last.pattern_from \
+                                       	= h['Hsp_pattern-from'].to_i
+	@iteration.last.hit.last.hsp.last.pattern_to \
+                                	= h['Hsp_pattern-to'].to_i
+   	@iteration.last.hit.last.hsp.last.query_frame \
+	                                = h['Hsp_query-frame'].to_i
+	@iteration.last.hit.last.hsp.last.hit_frame = h['Hsp_hit-frame'].to_i
+	@iteration.last.hit.last.hsp.last.identity = h['Hsp_identity'].to_i
+	@iteration.last.hit.last.hsp.last.positive = h['Hsp_positive'].to_i
+	@iteration.last.hit.last.hsp.last.gaps = h['Hsp_gaps'].to_i
+	@iteration.last.hit.last.hsp.last.align_len = h['Hsp_align-len'].to_i
+	@iteration.last.hit.last.hsp.last.density = h['Hsp_density'].to_i
+	@iteration.last.hit.last.hsp.last.qseq = h['Hsp_qseq']  # to_seq ?
+	@iteration.last.hit.last.hsp.last.hseq = h['Hsp_hseq']  # to_seq ?
 	@iteration.last.hit.last.hsp.last.midline = h['Hsp_midline']
       end
 	
@@ -286,58 +408,102 @@ module Bio
 end				# modlue Bio
 
 
+
 # Testing codes
+
 if __FILE__ == $0
-  b = Bio::Blast::Report.new($<.read)
 
-#  p b
+  fna="> hoge
+cagcatgtacgtgatcgtacgtagctagtcagtcgtatctatggcgcgcgcgcgcatctgactgtacga
+cggcgtatcgctatctgctctcttttagcttatcgggcgctatcttcgatcttagcttcgcgcattg
+ccccccccggggggtgatcgctgcgggctcggcgctcggctcgcgtggggcgggcgggcgcgcggcg
+> fuga
+aaaaaccaacccaccccccccccccacacatcgtatagccacgggcgcggcgtatggctagagatcgga
+acgatagctaggcgcgcgcgcgtatatattatttttttttttatatatttatatattattatatattat
+atatttattatatttaaaaaaaaaaaattatatatatatatttattttatagatgctcagctacggcatc
+> hoge2
+cagcatgtacgtgatcgtacgtagctagtcagtcgtatctatggcgcgcgcgcgcatctgactgtacga
+cggcgtatcgctatctgctctcttttagcttatcgggcgctatcttcgatcttagcttcgcgcattg
+ccccccccggggggtgatcgctgcgggctcggcgctcggctcgcgtggggcgggcgggcgcgcggcg
+"
+  
+  puts "= = Bio::Blast.local = ="
+  opts = {'-e' => 1.0}
 
-  puts "\n= = Bio::Tools::Blast::Report = ="
-  puts "   = = Report#program = ="
-  p b.program
-  puts "   = = Report#version = ="
-  p b.version
-  puts "   = = Report#reference = ="
-  p b.reference
-  puts "   = = Report#db = ="
-  p b.db
-  puts "   = = Report#query_ID = ="
-  p b.query_ID
-  puts "   = = Report#query_def = ="
-  p b.query_def
-  puts "   = = Report#query_len = ="
-  p b.query_len
+  blastall = '/bio/bin/blastall'     # option
+  db = '/bio/db/blast/genome/mtu'    # option
 
-  puts "\n= = Parameters = ="
-  p b.parameters
+  bls = Bio::Blast.local(blastall, 'blastn', db, opts)
+  p bls
 
-  puts "\n= = Statistics = ="
-  p b.statistics
+  puts "\n = Bio::Blast.blastall? =="
+  p bls.blastall?
+
+  puts "\n = Bio::Blast.db? ="
+  p bls.db?
+
+
+  puts "\n = p fna ="
+  puts fna
+
+  puts "\n = Bio::Blast.query(fna) ="
+  bs = bls.query(fna)
+
+  puts "\n = bs.size ="
+  p bs.size
+
+
+  bs.each do |b|
+    puts "\n= = Bio::Tools::Blast::Report = ="
+    puts "   = = Report#program = ="
+    p b.program
+    puts "   = = Report#version = ="
+    p b.version
+    puts "   = = Report#reference = ="
+    p b.reference
+    puts "   = = Report#db = ="
+    p b.db
+    puts "   = = Report#query_ID = ="
+    p b.query_ID
+    puts "   = = Report#query_def = ="
+    p b.query_def
+    puts "   = = Report#query_len = ="
+    p b.query_len
+    
+    puts "\n= = Parameters = ="
+    p b.parameters
+
+    puts "\n= = Statistics = ="
+    p b.statistics
 
   
-  b.iteration.each do |itr|
-    puts "\n= = Bio::Blast::Report::Iteration = ="
+    b.iteration.each do |itr|
+      puts "\n= = Bio::Blast::Report::Iteration = ="
+      
+      puts "   = = Iteration#num = ="
+      p itr.num
 
-    puts "   = = Iteration#num = ="
-    p itr.num
-
-    itr.hits.each do |hit|
-      puts "\n = = Bio::Blast::Report::Hit = ="
-      puts "   = = Hit#num = ="
-      p hit.num
-      puts "   = = Hit#_id = ="
-      p hit._id
-      puts "   = = Hit#_def = ="
-      p hit._def
-      puts "   = = Hit#accession = ="
-      p hit.accession
-      puts "   = = Hit#len = ="
-      p hit.len
-
-      hit.hsps.each do |hsp|
-	puts "\n  = = Bio::Blast::Report::Hsp = ="
-	p hsp
-	p
+      itr.hits.each do |hit|
+	puts "\n = = Bio::Blast::Report::Hit = ="
+	puts "   = = Hit#num = ="
+	p hit.num
+	puts "   = = Hit#_id = ="
+	p hit._id
+	puts "   = = Hit#_def = ="
+	p hit._def
+	puts "   = = Hit#accession = ="
+	p hit.accession
+	puts "   = = Hit#len = ="
+	p hit.len
+	
+	hit.hsps.each do |hsp|
+	  puts "\n  = = Bio::Blast::Report::Hsp = ="
+	  p hsp
+	  p
+	  puts "query: #{hsp.qseq}"
+	  puts "       #{hsp.midline}"
+	  puts "hit:   #{hsp.hseq}"
+	end
       end
     end
   end
@@ -347,19 +513,124 @@ end
 
 =begin
 
-== NAME
+= Bio::Blast
 
-blast_report.rb  - NCBI_BlastOutput.dtd XML Parser
+--- Bio::Blast#local(blastall, program, db, opts)
 
-Copyright (C) 2001 Mitsuteru S. Nakao <n@bioruby.org>
+      An intialize method for local blast search
+      opts = {'-e' => 0.0, ... }
 
-== EXAMPLE
+--- Bio::Blast#remote(uri, program, db, opts)
+
+      An intialize method for remote blast search
+
+--- Bio::Blast#ncbi(program, db, opts)
+
+      An intialize method for remote blast search on NCBI
+
+--- Bio::Blast#genomenet(program, db, opts)
+
+      An intialize method for remote blast search on GenomeNet
+
+
+--- Bio::Blast#query(fna)
+
+      Execute blast search.
+      Returns Bio::Blast::Report Object
+
+--- Bio::Blast#exec(fna)
+
+      Alias for Bio::Blast#query
+
+--- Bio::Blast#blastall?
+
+      Check true if @blastall excutable
+
+--- Bio::Blast#db?
+
+      Check true if @db redable
+
+
+= Bio::Blast::Report
+
+--- Bio::Blast::Report#new(xml)
+
+      Constructor method
+      xml as blastall -m 7 report
+
+--- Bio::Blast::Report#program
+--- Bio::Blast::Report#version
+--- Bio::Blast::Report#reference
+--- Bio::Blast::Report#db
+--- Bio::Blast::Report#query_ID
+--- Bio::Blast::Report#query_def
+--- Bio::Blast::Report#query_len
+--- Bio::Blast::Report#prameters -> hsh
+
+      Keys: expect, include, sc-match, sc-mismatch, gap-open, gap-extend
+            filter
+
+--- Bio::Blast::Report#statistics -> hsh
+
+      Keys: db-num, db-len, hsp-len, eff-space, kappa, db, db-num
+
+--- Bio::Blast::Report#iteration -> hsh
+
+
+= Bio::Blast::Iteration
+--- Bio::Blast::Iteration#num
+--- Bio::Blast::Iteration#add_hit(Bio::Blast::Hit)
+--- Bio::Blast::Iteration#hit -> ary
+--- Bio::Blast::Iteration#hits -> ary
+
+
+= Bio::Blast::Hit
+--- Bio::Blast::Hit#num
+--- Bio::Blast::Hit#_id
+--- Bio::Blast::Hit#_def
+--- Bio::Blast::Hit#accession
+--- Bio::Blast::Hit#len
+--- Bio::Blast::Hit#add_hsp(Bio::Blsat::Hsp)
+--- Bio::Blast::Hit#hsp -> ary
+--- Bio::Blast::Hit#hsps -> ary
+
+= Bio::Blast::Hsp
+--- Bio::Blast::Hsp#num
+--- Bio::Blast::Hsp#bit_score
+--- Bio::Blast::Hsp#score
+--- Bio::Blast::Hsp#evalue
+--- Bio::Blast::Hsp#query_from
+--- Bio::Blast::Hsp#query_to
+--- Bio::Blast::Hsp#hit_from
+--- Bio::Blast::Hsp#hit_to
+--- Bio::Blast::Hsp#pattern_from
+--- Bio::Blast::Hsp#pattern_to
+--- Bio::Blast::Hsp#query_frame
+--- Bio::Blast::Hsp#hit_frame
+--- Bio::Blast::Hsp#identity
+--- Bio::Blast::Hsp#positive
+--- Bio::Blast::Hsp#gaps
+--- Bio::Blast::Hsp#align_len
+--- Bio::Blast::Hsp#density
+--- Bio::Blast::Hsp#qseq
+--- Bio::Blast::Hsp#hseq
+--- Bio::Blast::Hsp#midline
+
+
+
+= EXAMPLE
 
 require 'bio/appl/blast'
 
-xml = $<.read
 
-res = Bio::Blast::Report.new(xml)
+blastall = '/bio/bin/blastall'
+db = '/bio/db/blast/genome/bsu'
+opts = {'-e' => 0.0, '-F' => 'F'}
+bls = Bio::Blast.local(blastall, 'blastn', db, opts)
+
+res = bla.query(fna)
+
+# res = Bio::Blast::Report.new(xml)
 # Bio::Blast::Report
 res.program #=> blastn                        <BlastOutput_program>
 res.version #=> blastn 2.2.1 [Jul-12-2001]    <BlastOutput_version>
@@ -374,7 +645,7 @@ res.parameters['expect'] #=> 10         <Parameters_expect>
 res.parameters['include'] #=> 0         <Parameters_include>  
 res.parameters['sc-match'] #=> 1        <Parameters_so-match>
 res.parameters['sc-mismatch'] #=> -3    <Parameters_so-mismatch>
-res.parameters['gep-open'] #=> 5        <Parameters_gap-open>
+res.parameters['gap-open'] #=> 5        <Parameters_gap-open>
 res.parameters['gap-extend'] #=> 2      <Parameters_gap-extend>
 res.parameters['filter'] #=> D          <Parameters_filter>
 res.iteration #-> Array                       <BlastOutput_iterations>
@@ -419,11 +690,15 @@ it.statistics['kappa'] #=> 0.710605           <Statistics_kappa>
 it.statistics['db'] #=> 1                     <Statistics_db-num>
 it.statistics['db-num'] #=> 1                 <Statistics_db-num>
 
-== DTD files
+= DTD files
 
 * http://www.ncbi.nlm.nih.gov/dtd/NCBI_BlastOutput.dtd
 * http://www.ncbi.nlm.nih.gov/dtd/NCBI_BlastOutput.mod
 * http://www.ncbi.nlm.nih.gov/dtd/NCBI_Entity.mod
+
+= See also
+
+blastall
 
 =end
 
