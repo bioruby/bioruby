9275f17f1618f7d3e18d578adaf707dc5e3347a7
Toshiaki Katayama <k@bioruby.org>
2002-06-25 19:30:26 +0000

    * added licence

--- sample/gbtab2mysql.rb
+++ sample/gbtab2mysql.rb
@@ -1,4 +1,25 @@
 #!/usr/bin/env ruby
+#
+# gbtab2mysql.rb - load tab delimited GenBank data files into MySQL
+#
+#   Copyright (C) 2002 KATAYAMA Toshiaki <k@bioruby.org>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#  $Id: gbtab2mysql.rb,v 1.3 2002/06/25 19:30:26 k Exp $
+#
 
 require 'dbi'
 
