c6e3bb27ebd446bb834745c77f46c92e1914d247
Masumi Itoh <m@bioruby.org>
2002-07-29 04:59:53 +0000

    Change data source from KEGG/GENOME to all_species.tab

--- lib/bio/data/keggorg.rb
+++ lib/bio/data/keggorg.rb
@@ -1,7 +1,8 @@
 # 
 # bio/data/keggorg.rb - KEGG organism code module
 # 
-#   Copyright (C) 2001, 2002 KATAYAMA Toshiaki <k@bioruby.org> 
+#   Copyright (C) 2001, 2002  KATAYAMA Toshiaki <k@bioruby.org> 
+#   Copyright (C) 2002  Masumi Itoh <m@bioruby.org> 
 # 
 #  This library is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU Lesser General Public
@@ -17,125 +18,108 @@
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
-#  $Id: keggorg.rb,v 0.9 2002/07/26 05:37:24 m Exp $
+#  $Id: keggorg.rb,v 0.10 2002/07/29 04:59:53 m Exp $
 #
 
 module Bio
-
-  # % genome2rb.rb /bio/db/kegg/genome/genome | sort
-  #
-  # genome2rb.rb:
-  #
-  #   require 'bio'
-  #
-  #   Bio::FlatFile.new(Bio::KEGG::GENOME,ARGF).each do |x|
-  #     puts "    '#{x.entry_id}' => [ '#{x.name}', '#{x.definition}' ],"
-  #   end
-  #
-  # genome           KEGG Genome Database
-  # gn               Release 22.0+/07-10, Jul 02
-  #                  Institute for Chemical Research, Kyoto University
-  #                  95 entries
-  #                  Last update:  02/07/10
-  #                  <dbget> <fasta> <blast>
-  #
   KEGGORG = {
-    'aae' => [ 'A.aeolicus', 'Aquifex aeolicus VF5' ],
-    'afu' => [ 'A.fulgidus', 'Archaeoglobus fulgidus VC-16' ],
-    'ana' => [ 'Anabaena', 'Anabaena sp. PCC 7120 (Nostoc sp. PCC 7120)' ],
-    'ape' => [ 'A.pernix', 'Aeropyrum pernix K1' ],
-    'atc' => [ 'A.tumefaciens_C', 'Agrobacterium tumefaciens C58 (Cereon)' ],
+    'aae' => [ 'A.aeolicus', 'Aquifex aeolicus' ],
+    'afu' => [ 'A.fulgidus', 'Archaeoglobus fulgidus' ],
+    'ana' => [ 'Anabaena', 'Anabaena sp.' ],
+    'ape' => [ 'A.pernix', 'Aeropyrum pernix' ],
+    'atc' => [ 'A.tumefaciens_C', 'Agrobacterium tumefaciens C58 Cereon' ],
     'ath' => [ 'A.thaliana', 'Arabidopsis thaliana' ],
-    'atu' => [ 'A.tumefaciens', 'Agrobacterium tumefaciens C58 (U.Washington/Dupont)' ],
-    'bas' => [ 'B.aphidicola_Sg', 'Buchnera aphidicola Sg (symbiont of Schizaphis graminum)' ],
-    'bbu' => [ 'B.burgdorferi', 'Borrelia burgdorferi B31' ],
-    'bha' => [ 'B.halodurans', 'Bacillus halodurans C-125' ],
-    'bme' => [ 'B.melitensis', 'Brucella melitensis 16M' ],
-    'bsu' => [ 'B.subtilis', 'Bacillus subtilis 168' ],
-    'buc' => [ 'Buchnera', 'Buchnera sp. APS' ],
-    'cac' => [ 'C.acetobutylicum', 'Clostridium acetobutylicum ATCC 824' ],
-    'ccr' => [ 'C.crescentus', 'Caulobacter crescentus CB15' ],
+    'atu' => [ 'A.tumefaciens', 'Agrobacterium tumefaciens C58 UWash' ],
+    'bas' => [ 'B.aphidicola_Sg', 'Buchnera aphidicola Sg' ],
+    'bbu' => [ 'B.burgdorferi', 'Borrelia burgdorferi' ],
+    'bha' => [ 'B.halodurans', 'Bacillus halodurans' ],
+    'bme' => [ 'B.melitensis', 'Brucella melitensis' ],
+    'bsu' => [ 'B.subtilis', 'Bacillus subtilis' ],
+    'buc' => [ 'Buchnera', 'Buchnera sp.' ],
+    'cac' => [ 'C.acetobutylicum', 'Clostridium acetobutylicum' ],
+    'cal' => [ 'C.albicans', 'Candida albicans' ],
+    'ccr' => [ 'C.crescentus', 'Caulobacter crescentus' ],
     'cel' => [ 'C.elegans', 'Caenorhabditis elegans' ],
-    'cje' => [ 'C.jejuni', 'Campylobacter jejuni NCTC11168' ],
-    'cmu' => [ 'C.muridarum', 'Chlamydia muridarum (Chlamydia trachomatis MoPn)' ],
+    'cje' => [ 'C.jejuni', 'Campylobacter jejuni' ],
+    'cmu' => [ 'C.muridarum', 'Chlamydia muridarum' ],
     'cpa' => [ 'C.pneumoniae_AR39', 'Chlamydophila pneumoniae AR39' ],
-    'cpe' => [ 'C.perfringens', 'Clostridium perfringens 13' ],
+    'cpe' => [ 'C.perfringens', 'Clostridium perfringens' ],
     'cpj' => [ 'C.pneumoniae_J138', 'Chlamydophila pneumoniae J138' ],
     'cpn' => [ 'C.pneumoniae', 'Chlamydophila pneumoniae CWL029' ],
-    'cte' => [ 'C.tepidum', 'Chlorobium tepidum TLS' ],
-    'ctr' => [ 'C.trachomatis', 'Chlamydia trachomatis serovar D' ],
+    'cte' => [ 'C.tepidum', 'Chlorobium tepidum' ],
+    'ctr' => [ 'C.trachomatis', 'Chlamydia trachomatis' ],
     'ddi' => [ 'D.discoideum', 'Dictyostelium discoideum' ],
     'dme' => [ 'D.melanogaster', 'Drosophila melanogaster' ],
-    'dra' => [ 'D.radiodurans', 'Deinococcus radiodurans R1' ],
+    'dra' => [ 'D.radiodurans', 'Deinococcus radiodurans' ],
     'dre' => [ 'D.rerio', 'Danio rerio' ],
-    'ece' => [ 'E.coli_O157', 'Escherichia coli O157:H7 EDL933' ],
+    'ece' => [ 'E.coli_O157', 'Escherichia coli O157 EDL933' ],
     'ecj' => [ 'E.coli_J', 'Escherichia coli K-12 W3110' ],
     'eco' => [ 'E.coli', 'Escherichia coli K-12 MG1655' ],
-    'ecs' => [ 'E.coli_O157J', 'Escherichia coli O157:H7 Sakai' ],
-    'fnu' => [ 'F.nucleatum', 'Fusobacterium nucleatum ATCC 25586' ],
-    'hal' => [ 'Halobacterium', 'Halobacterium sp. NRC-1' ],
-    'hin' => [ 'H.influenzae', 'Haemophilus influenzae Rd KW20' ],
+    'ecs' => [ 'E.coli_O157J', 'Escherichia coli O157 Sakai' ],
+    'fnu' => [ 'F.nucleatum', 'Fusobacterium nucleatum' ],
+    'hal' => [ 'Halobacterium', 'Halobacterium sp.' ],
+    'hin' => [ 'H.influenzae', 'Haemophilus influenzae' ],
     'hpj' => [ 'H.pylori_J99', 'Helicobacter pylori J99' ],
     'hpy' => [ 'H.pylori', 'Helicobacter pylori 26695' ],
     'hsa' => [ 'H.sapiens', 'Homo sapiens' ],
-    'lin' => [ 'L.innocua', 'Listeria innocua CLIP 11262' ],
-    'lla' => [ 'L.lactis', 'Lactococcus lactis subsp. lactis IL1403' ],
-    'lmo' => [ 'L.monocytogenes', 'Listeria monocytogenes EGD-e' ],
-    'mac' => [ 'M.acetivorans', 'Methanosarcina acetivorans C2A' ],
-    'mge' => [ 'M.genitalium', 'Mycoplasma genitalium G-37' ],
-    'mja' => [ 'M.jannaschii', 'Methanococcus jannaschii DSM2661' ],
-    'mka' => [ 'M.kandleri', 'Methanopyrus kandleri AV19' ],
-    'mle' => [ 'M.leprae', 'Mycobacterium leprae TN' ],
-    'mlo' => [ 'M.loti', 'Mesorhizobium loti MAFF303099' ],
-    'mma' => [ 'M.mazei', 'Methanosarcina mazei Goe1' ],
+    'lin' => [ 'L.innocua', 'Listeria innocua' ],
+    'lla' => [ 'L.lactis', 'Lactococcus lactis' ],
+    'lmo' => [ 'L.monocytogenes', 'Listeria monocytogenes' ],
+    'mac' => [ 'M.acetivorans', 'Methanosarcina acetivorans' ],
+    'mge' => [ 'M.genitalium', 'Mycoplasma genitalium' ],
+    'mja' => [ 'M.jannaschii', 'Methanococcus jannaschii' ],
+    'mka' => [ 'M.kandleri', 'Methanopyrus kandleri' ],
+    'mle' => [ 'M.leprae', 'Mycobacterium leprae' ],
+    'mlo' => [ 'M.loti', 'Mesorhizobium loti' ],
+    'mma' => [ 'M.mazei', 'Methanosarcina mazei' ],
     'mmu' => [ 'M.musculus', 'Mus musculus' ],
-    'mpn' => [ 'M.pneumoniae', 'Mycoplasma pneumoniae M129' ],
-    'mpu' => [ 'M.pulmonis', 'Mycoplasma pulmonis UAB CTIP' ],
-    'mtc' => [ 'M.tuberculosis_CDC1551', 'Mycobacterium tuberculosis CDC1551, clinical strain' ],
-    'mth' => [ 'M.thermoautotrophicum', 'Methanobacterium thermoautotrophicum deltaH' ],
-    'mtu' => [ 'M.tuberculosis', 'Mycobacterium tuberculosis H37Rv, laboratory strain' ],
-    'nma' => [ 'N.meningitidis_A', 'Neisseria meningitidis Z2491 (serogroup A)' ],
-    'nme' => [ 'N.meningitidis', 'Neisseria meningitidis MC58 (serogroup B)' ],
+    'mpn' => [ 'M.pneumoniae', 'Mycoplasma pneumoniae' ],
+    'mpu' => [ 'M.pulmonis', 'Mycoplasma pulmonis' ],
+    'mtc' => [ 'M.tuberculosis_CDC1551', 'Mycobacterium tuberculosis CDC1551' ],
+    'mth' => [ 'M.thermoautotrophicum', 'Methanobacterium thermoautotrophicum' ],
+    'mtu' => [ 'M.tuberculosis', 'Mycobacterium tuberculosis H37Rv' ],
+    'nma' => [ 'N.meningitidis_A', 'Neisseria meningitidis serogroup A' ],
+    'nme' => [ 'N.meningitidis', 'Neisseria meningitidis serogroup B' ],
     'osa' => [ 'O.sativa', 'Oryza sativa' ],
-    'pab' => [ 'P.abyssi', 'Pyrococcus abyssi GE5' ],
-    'pae' => [ 'P.aeruginosa', 'Pseudomonas aeruginosa PA01' ],
-    'pai' => [ 'P.aerophilum', 'Pyrobaculum aerophilum IM2' ],
-    'pfa' => [ 'P.falciparum', 'Plasmodium falciparum 3D7' ],
-    'pfu' => [ 'P.furiosus', 'Pyrococcus furiosus DSM 3638' ],
-    'pho' => [ 'P.horikoshii', 'Pyrococcus horikoshii OT3' ],
-    'pmu' => [ 'P.multocida', 'Pasteurella multocida PM70' ],
-    'rco' => [ 'R.conorii', 'Rickettsia conorii Malish 7' ],
+    'pab' => [ 'P.abyssi', 'Pyrococcus abyssi' ],
+    'pae' => [ 'P.aeruginosa', 'Pseudomonas aeruginosa' ],
+    'pai' => [ 'P.aerophilum', 'Pyrobaculum aerophilum ' ],
+    'pfa' => [ 'P.falciparum', 'Plasmodium falciparum' ],
+    'pfu' => [ 'P.furiosus', 'Pyrococcus furiosus' ],
+    'pho' => [ 'P.horikoshii', 'Pyrococcus horikoshii' ],
+    'pmu' => [ 'P.multocida', 'Pasteurella multocida' ],
+    'rco' => [ 'R.conorii', 'Rickettsia conorii' ],
     'rno' => [ 'R.norvegicus', 'Rattus norvegicus' ],
-    'rpr' => [ 'R.prowazekii', 'Rickettsia prowazekii Madrid E' ],
-    'rso' => [ 'R.solanacearum', 'Ralstonia solanacearum GMI1000' ],
+    'rpr' => [ 'R.prowazekii', 'Rickettsia prowazekii' ],
+    'rso' => [ 'R.solanacearum', 'Ralstonia solanacearum' ],
     'sam' => [ 'S.aureus_MW2', 'Staphylococcus aureus MW2' ],
-    'sau' => [ 'S.aureus_N315', 'Staphylococcus aureus N315, meticillin-resistant (MRSA)' ],
-    'sav' => [ 'S.aureus_Mu50', 'Staphylococcus aureus Mu50, MRSA strain with vancomycin resistance (VRSA)' ],
-    'sce' => [ 'S.cerevisiae', 'Saccharomyces cerevisiae S288C' ],
-    'sco' => [ 'S.coelicolor', 'Streptomyces coelicolor A3(2)' ],
-    'sme' => [ 'S.meliloti', 'Sinorhizobium meliloti 1021' ],
-    'spm' => [ 'S.pyogenes_M18', 'Streptococcus pyogenes MGAS8232 (serotype M18)' ],
+    'sau' => [ 'S.aureus_N315', 'Staphylococcus aureus N315' ],
+    'sav' => [ 'S.aureus_Mu50', 'Staphylococcus aureus Mu50' ],
+    'sce' => [ 'S.cerevisiae', 'Saccharomyces cerevisiae' ],
+    'sco' => [ 'S.coelicolor', 'Streptomyces coelicolor' ],
+    'sme' => [ 'S.meliloti', 'Sinorhizobium meliloti' ],
+    'spg' => [ 'S.pyogenes_M3', 'Streptococcus pyogenes M3' ],
+    'spm' => [ 'S.pyogenes_M18', 'Streptococcus pyogenes M18' ],
     'spn' => [ 'S.pneumoniae', 'Streptococcus pneumoniae TIGR4' ],
     'spo' => [ 'S.pombe', 'Schizosaccharomyces pombe' ],
     'spr' => [ 'S.pneumoniae_R6', 'Streptococcus pneumoniae R6' ],
-    'spy' => [ 'S.pyogenes', 'Streptococcus pyogenes SF370 (serotype M1)' ],
+    'spy' => [ 'S.pyogenes', 'Streptococcus pyogenes' ],
     'sso' => [ 'S.solfataricus', 'Sulfolobus solfataricus' ],
-    'stm' => [ 'S.typhimurium', 'Salmonella typhimurium LT2' ],
-    'sto' => [ 'S.tokodaii', 'Sulfolobus tokodaii strain7' ],
+    'stm' => [ 'S.typhimurium', 'Salmonella typhimurium' ],
+    'sto' => [ 'S.tokodaii', 'Sulfolobus tokodaii' ],
     'sty' => [ 'S.typhi', 'Salmonella typhi' ],
-    'syn' => [ 'Synechocystis', 'Synechocystis sp. PCC 6803' ],
+    'syn' => [ 'Synechocystis', 'Synechocystis sp.' ],
     'tac' => [ 'T.acidophilum', 'Thermoplasma acidophilum' ],
-    'tma' => [ 'T.maritima', 'Thermotoga maritima MSB8' ],
-    'tpa' => [ 'T.pallidum', 'Treponema pallidum Nichols' ],
-    'tte' => [ 'T.tengcongensis', 'Thermoanaerobacter tengcongensis MB4T' ],
-    'tvo' => [ 'T.volcanium', 'Thermoplasma volcanium GSS1' ],
-    'uur' => [ 'U.urealyticum', 'Ureaplasma urealyticum serovar 3' ],
-    'vch' => [ 'V.cholerae', 'Vibrio cholerae El Tor N16961' ],
-    'xac' => [ 'X.axonopodis', 'Xanthomonas axonopodis pv. citri 306' ],
-    'xcc' => [ 'X.campestris', 'Xanthomonas campestris pv. campestris ATCC 339131' ],
-    'xfa' => [ 'X.fastidiosa', 'Xylella fastidiosa 9a5c' ],
-    'ype' => [ 'Y.pestis', 'Yersinia pestis CO92' ],
-  }
-
+    'tma' => [ 'T.maritima', 'Thermotoga maritima' ],
+    'tpa' => [ 'T.pallidum', 'Treponema pallidum' ],
+    'tte' => [ 'T.tengcongensis', 'Thermoanaerobacter tengcongensis' ],
+    'tvo' => [ 'T.volcanium', 'Thermoplasma volcanium' ],
+    'uur' => [ 'U.urealyticum', 'Ureaplasma urealyticum' ],
+    'vch' => [ 'V.cholerae', 'Vibrio cholerae' ],
+    'xac' => [ 'X.axonopodis', 'Xanthomonas axonopodis' ],
+    'xcc' => [ 'X.campestris', 'Xanthomonas campestris' ],
+    'xfa' => [ 'X.fastidiosa', 'Xylella fastidiosa' ],
+    'ype' => [ 'Y.pestis', 'Yersinia pestis' ],
+    'zma' => [ 'Z.mays', 'Zea mays' ],
+}
 end
-
