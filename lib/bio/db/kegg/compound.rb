48d0e4a65effbadf5be191e85ffc3f82b66c133d
Toshiaki Katayama <k@bioruby.org>
2002-08-19 02:01:47 +0000

    * methods added (entry_id, names, formula, reactions, pathways, enzymes, dblinks)

--- lib/bio/db/kegg/compound.rb
+++ lib/bio/db/kegg/compound.rb
@@ -17,7 +17,7 @@
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
-#  $Id: compound.rb,v 0.5 2001/12/15 02:50:35 katayama Exp $
+#  $Id: compound.rb,v 0.6 2002/08/19 02:01:47 k Exp $
 #
 
 require 'bio/db'
@@ -36,11 +36,57 @@ module Bio
       end
 
       # ENTRY
+      def entry_id
+	field_fetch('ENTRY')
+      end
+
       # NAME
+      def names
+	unless @data['NAME']
+	  @data['NAME'] = get('NAME').split("\n").map{ |l| tag_cut(l) }
+	end
+	@data['NAME']
+      end
+      def name
+	names[0]
+      end
+
       # FORMULA
+      def formula
+	field_fetch('FORMULA')
+      end
+
+      # REACTION
+      def reactions
+	unless @data['REACTION']
+	  @data['REACTION'] = fetch('REACTION').split("\s+")
+	end
+	@data['REACTION']
+      end
+
       # PATHWAY
+      def pathways
+	unless @data['PATHWAY']
+	  @data['PATHWAY'] = get('PATHWAY').split("\n").map{ |l| tag_cut(l) }
+	end
+	@data['PATHWAY']
+      end
+
       # ENZYME
+      def enzymes
+	unless @data['ENZYME']
+	  @data['ENZYME'] = fetch('ENZYME').scan(/\S+ \(\S+\)/)
+	end
+	@data['ENZYME']
+      end
+
       # DBLINKS
+      def dblinks
+	unless @data['DBLINKS']
+	  @data['DBLINKS'] = get('DBLINKS').split("\n").map{ |l| tag_cut(l) }
+	end
+	@data['DBLINKS']
+      end
 
     end
 
