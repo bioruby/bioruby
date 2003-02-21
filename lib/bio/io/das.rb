06df86ee8544ff48e30d52b9eb419c0a98c04872
Toshiaki Katayama <k@bioruby.org>
2003-02-21 03:36:49 +0000

    * clean up, using classes for many of sub features etc.

--- lib/bio/io/das.rb
+++ lib/bio/io/das.rb
@@ -17,220 +17,367 @@
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
-#  $Id: das.rb,v 1.1 2003/02/18 08:06:32 k Exp $
+#  $Id: das.rb,v 1.2 2003/02/21 03:36:49 k Exp $
 #
 
 begin
   require 'rexml/document'
 rescue LoadError
 end
+require 'uri'
 require 'net/http'
 
 module Bio
 
   class DAS
 
-    def initialize(url = "www.wormbase.org", dir = "db", port = 80)
-      @h = Net::HTTP.new(url, port)
-      @dir = dir
+    def initialize(url = 'http://www.wormbase.org:80/db/')
+      schema, user, host, port, path, = URI.parse(url).to_a
+      @server = Net::HTTP.new(host, port)
+      @prefix = path ? path.chomp('/') : ''
     end
 
     def get_dsn
-      dsns = []
-      result, = @h.get('/' + @dir + '/das/dsn', nil)
-      @source = result.body
-      doc = REXML::Document.new(@source)
-      doc.elements.each("/descendant::DSN") do |elm|
+      ary = []
+      result, = @server.get(@prefix + '/das/dsn')
+      doc = REXML::Document.new(result.body)
+      doc.elements.each('/descendant::DSN') do |e|
         dsn = DSN.new
-        elm.elements.each do |e|
-          if e.name == "SOURCE"
+        e.elements.each do |e|
+	  case e.name
+	  when 'SOURCE'
             dsn.source = e.text
-            dsn.source_id = e.attributes["id"]
-            dsn.source_version = e.attributes["version"]
-          elsif e.name == "MAPMASTER"
+            dsn.source_id = e.attributes['id']
+            dsn.source_version = e.attributes['version']
+          when 'MAPMASTER'
             dsn.mapmaster = e.name
-          elsif e.name == "DESCRIPTION"
+          when 'DESCRIPTION'
             dsn.description = e.text
-            dsn.description_href = e.attributes["href"]
+            dsn.description_href = e.attributes['href']
           end
         end
-        dsns << dsn
-      end
-      dsns
-    end
-
-    def get_entry_points(dsn_src)
-      ary = []
-      hash = {}
-      resp, src = @h.get('/' + @dir + '/das/' + dsn_src +
-      '/entry_points' , nil)
-      doc = REXML::Document.new(src)
-      doc.elements.each("/descendant::SEGMENT") do |elm|
-        ary << [elm.text]
-        elm.attributes.each do |n, v|
-	  hash[n] = v
-	end
-        ary << [elm.text, hash]
+        ary << dsn
       end
       ary
     end
 
-    def get_dna(dsn_source, reference, start, stop)
-      dna = DNA.new
-      result, = @h.get('/' + @dir + '/das/' + dsn_source + '/dna' +
-      "?segment=" + reference + ':' + start.to_s + ',' + stop.to_s, nil)
-      @source = result.body
-      doc = REXML::Document.new(@source)
-      doc.elements.each("/descendant::SEQUENCE") do |elm|
-        hash = {}
-        seq = ""
-        length = 0
-        elm.attributes.each do |n, v|
-	  hash[n] = v
+    def get_entry_point(dsn)
+      entry_point = ENTRY_POINT.new
+      dsn = dsn.source if dsn.instance_of?(Bio::DAS::DSN)
+      result, = @server.get(@prefix + '/das/' + dsn + '/entry_points')
+      doc = REXML::Document.new(result.body)
+      doc.elements.each('/descendant::ENTRY_POINTS') do |e|
+	entry_point.href = e.attributes['href']
+	entry_point.version = e.attributes['version']
+	e.elements.each('/descendant::SEGMENT') do |e|
+	  segment = SEGMENT.new
+	  segment.entry_id = e.attributes['id']
+	  segment.start = e.attributes['start']
+	  segment.stop = e.attributes['stop']
+	  segment.stop = e.attributes['orientation']
+	  segment.subparts = e.attributes['subparts']
+	  segment.description = e.text
+	  entry_point.segments << segment
 	end
-        elm.elements.each do |e|
-          if e.name == "DNA"
-            length = e.attributes["length"].to_i
-            e.text.each do |line|
-              seq << line.chomp
-            end
-          end
-        end
-        dna.sequence << [hash, length, Bio::Sequence.new(seq)]
       end
-      dna
+      entry_point
     end
 
-    def get_features(dsn_src, ref, start, stop)
-      gff = GFF.new
-      result = @h.get('/' + @dir + '/das/' + dsn_src + "/features?segment=" +
-      ref + ':' + start.to_s + ',' + stop.to_s, nil)
-      @source = result.body
-      doc = REXML::Document.new(src)
-      doc.elements.each("/descendant::GFF") do |e|
-        gff.gff_version = e.attributes["version"]
-        gff.gff_href = e.attributes["href"]
-        e.elements.each("/descendant::SEGMENT") do |f|
-          gff.segment[0] = f.attributes["id"]
-          gff.segment[1] = f.attributes["start"]
-          gff.segment[2] = f.attributes["stop"]
-          gff.segment[3] = f.attributes["version"]
-          gff.segment[4] = f.attributes["label"]
-	  if f.has_elements? # FEATURE tag is optional
-            gff.segment[5] = Array.new()
-            f.elements.each("/descendant::FEATURE") do |g|
-              feature_tmp = Array.new(11)
-              g.elements.each do |h|
-                if h.name == "TYPE"
-                  feature_tmp[0] = [ h.text, 
-                                     h.attributes["id"],
-                                     h.attributes["category"],
-                                     h.attributes["reference"] ]
-                elsif h.name == "METHOD"
-                  feature_tmp[1] = [ h.text,
-                                     h.attributes["id"] ]
-                elsif h.name == "START"
-                  feature_tmp[2] = h.text
-                elsif h.name == "END"
-                  feature_tmp[3] = h.text
-                elsif h.name == "SCORE"
-                  feature_tmp[4] = h.text
-                elsif h.name == "ORIENTATION"
-                  feature_tmp[5] = h.text
-                elsif h.name == "TARGET"
-                  feature_tmp[6] = [ h.text,
-                                     h.attributes["id"],
-                                     h.attributes["start"],
-                                     h.attributes["stop"] ]
-                elsif h.name == "PHASE"
-                  feature_tmp[7] = h.text
-                elsif h.name == "NOTE"
-                  feature_tmp[8] = h.text
-                elsif h.name == "LINK"
-                  feature_tmp[9] = [ h.text,
-                                     h.attributes["href"] ]
-                elsif h.name == "GROUP"
-                  g_tmp = Array.new(6)
-                  n_tmp = Array.new()
-                  l_tmp = Array.new()
-                  t_tmp = Array.new()
-                  g_tmp[0] = h.attributes["id"]
-                  g_tmp[1] = h.attributes["label"]
-                  g_tmp[2] = h.attributes["type"]
-                  h.elements.each do |i|
-                    if i.name == "NOTE"
-                      n_tmp << i.text
-                    elsif i.name == "LINK"
-                      l_tmp << [ i.text,
-                                i.attributes["href"] ]
-                    elsif i.name == "TARGET"
-                      t_tmp << [ i.text, 
-                                 i.attributes["id"],
-                                 i.attributes["start"],
-                                 i.attributes["stop"] ]
-                    end
-                  end
-                  g_tmp[3] = n_tmp
-                  g_tmp[4] = l_tmp
-                  g_tmp[5] = t_tmp
-                  if feature_tmp[10] == nil
-                    feature_tmp[10] = [g_tmp]
-                  else
-                    feature_tmp[10] << g_tmp
-                  end
-                end
-              end
-              gff.segment[5] << feature_tmp
-            end
+    def get_dna(dsn, segments)
+      ary = []
+
+      dsn = dsn.source if dsn.instance_of?(DSN)
+      segments = [segments] if segments.instance_of?(SEGMENT)
+
+      opts = []
+      segments.each do |s|
+	opts << "segment=#{s.entry_id}:#{s.start},#{s.stop}"
+      end
+      query = opts.join(';')
+
+      result, = @server.get(@prefix + '/das/' + dsn + '/dna?' + query)
+      doc = REXML::Document.new(result.body)
+      doc.elements.each('/descendant::SEQUENCE') do |e|
+	sequence = DNA.new
+	sequence.entry_id = e.attributes['id']
+	sequence.start = e.attributes['start']
+	sequence.stop = e.attributes['stop']
+	sequence.version = e.attributes['version']
+	e.elements.each('/descendant::DNA') do |e|
+	  sequence.sequence = Bio::Sequence::NA.new(e.text)
+	  sequence.length = e.attributes['length'].to_i
+	end
+        ary << sequence
+      end
+      ary
+    end
+
+    def get_sequence(dsn, segments)
+      ary = []
+
+      dsn = dsn.source if dsn.instance_of?(DSN)
+      segments = [segments] if segments.instance_of?(SEGMENT)
+
+      opts = []
+      segments.each do |s|
+	opts << "segment=#{s.entry_id}:#{s.start},#{s.stop}"
+      end
+      query = opts.join(';')
+
+      result, = @server.get(@prefix + '/das/' + dsn + '/sequence?' + query)
+      doc = REXML::Document.new(result.body)
+      doc.elements.each('/descendant::SEQUENCE') do |e|
+	sequence = SEQUENCE.new
+	sequence.entry_id = e.attributes['id']
+	sequence.start = e.attributes['start']
+	sequence.stop = e.attributes['stop']
+	sequence.moltype = e.attributes['moltype']
+	sequence.version = e.attributes['version']
+	case sequence.moltype
+	when /dna|rna/i		# 'DNA', 'ssRNA', 'dsRNA'
+	  sequence.sequence = Bio::Sequence::NA.new(e.text)
+	when /protein/i		# 'Protein
+	  sequence.sequence = Bio::Sequence::AA.new(e.text)
+	else
+	  sequence.sequence = e.text
+	end
+        ary << sequence
+      end
+      ary
+    end
+
+    def get_types(dsn, segments = [])	# argument 'type' is deprecated
+      types = TYPES.new
+
+      dsn = dsn.source if dsn.instance_of?(DSN)
+      segments = [segments] if segments.instance_of?(SEGMENT)
+
+      opts = []
+      segments.each do |s|
+	opts << "segment=#{s.entry_id}:#{s.start},#{s.stop}"
+      end
+      query = opts.join(';')
+
+      result, = @server.get(@prefix + '/das/' + dsn + '/types?' + query)
+      doc = REXML::Document.new(result.body)
+      doc.elements.each('/descendant::GFF') do |e|
+	types.version = e.attributes['version']
+	types.href = e.attributes['href']
+	e.elements.each('/descendant::SEGMENT') do |e|
+	  segment = SEGMENT.new
+	  segment.entry_id = e.attributes['id']
+	  segment.start = e.attributes['start']
+	  segment.stop = e.attributes['stop']
+	  segment.version = e.attributes['version']
+	  segment.label = e.attributes['label']
+	  e.elements.each('/descendant::TYPE') do |i|
+	    t = TYPE.new
+	    t.entry_id = i.attributes['id']
+	    t.method = i.attributes['method']
+	    t.category = i.attributes['category']
+	    t.count = i.text.to_i
+	    segment.types << t
 	  end
+	  types.segments << segment
+	end
+      end
+      types
+    end
+
+    def get_features(dsn, segments = [], categorize = false, feature_ids = [], group_ids = [])
+      # arguments 'type' and 'category' are deprecated
+      gff = GFF.new
+
+      dsn = dsn.source if dsn.instance_of?(DSN)
+      segments = [segments] if segments.instance_of?(SEGMENT)
+
+      opts = []
+      segments.each do |s|
+	opts << "segment=#{s.entry_id}:#{s.start},#{s.stop}"
+      end
+      if categorize
+	opts << "categorize=yes"	# default is 'no'
+      end
+      feature_ids.each do |fid|
+	opts << "feature_id=#{fid}"
+      end
+      group_ids.each do |gid|
+	opts << "group_id=#{gid}"
+      end
+      query = opts.join(';')
+
+      result, = @server.get(@prefix + '/das/' + dsn + '/features?' + query)
+      doc = REXML::Document.new(result.body)
+      doc.elements.each('/descendant::GFF') do |e|
+        gff.version = e.attributes['version']
+        gff.href = e.attributes['href']
+        e.elements.each('/descendant::SEGMENT') do |e|
+	  segment = SEGMENT.new
+	  segment.entry_id = e.attributes['id']
+	  segment.start = e.attributes['start']
+	  segment.stop = e.attributes['stop']
+	  segment.version = e.attributes['version']
+	  segment.label = e.attributes['label']
+	  e.elements.each('/descendant::FEATURE') do |e|
+	    feature = FEATURE.new
+	    feature.entry_id = e.attributes['id']
+	    feature.label = e.attributes['label']
+	    e.elements.each('/descendant::TYPE') do |e|
+	      t = TYPE.new
+	      t.entry_id = e.attributes['id']
+	      t.category = e.attributes['category']
+	      t.reference = e.attributes['referrence']
+	      t.label = e.text
+	      feature.type << t
+	    end
+	    e.elements.each('/descendant::METHOD') do |e|
+	      feature.method_id = e.attributes['id']
+	      feature.method = e.text
+	    end
+	    e.elements.each('/descendant::START') do |e|
+	      feature.start = e.text
+	    end
+	    e.elements.each('/descendant::STOP') do |e|
+	      feature.stop = e.text
+	    end
+	    e.elements.each('/descendant::SCORE') do |e|
+	      feature.score = e.text
+	    end
+	    e.elements.each('/descendant::ORIENTATION') do |e|
+	      feature.orientation = e.text
+	    end
+	    e.elements.each('/descendant::PHASE') do |e|
+	      feature.phase = e.text
+	    end
+	    e.elements.each('/descendant::NOTE') do |e|
+	      feature.notes << e.text
+	    end
+	    e.elements.each('/descendant::LINK') do |e|
+	      link = LINK.new
+	      link.href = e.attributes['href']
+	      link.text = e.text
+	      feature.links << link
+	    end
+	    e.elements.each('/descendant::TARGET') do |e|
+	      target = TARGET.new
+	      target.entry_id = e.attributes['id']
+	      target.start = e.attributes['start']
+	      target.stop = e.attributes['stop']
+	      target.name = e.text
+	      feature.targets << target
+	    end
+	    e.elements.each('/descendant::GROUP') do |e|
+	      group = GROUP.new
+	      group.entry_id = e.attributes['id']
+	      group.label = e.attributes['label']
+	      group.type = e.attributes['type']
+	      e.elements.each('/descendant::NOTE') do |e|
+		group.notes << e.text
+	      end
+	      e.elements.each('/descendant::LINK') do |e|
+		link = LINK.new
+		link.href = e.attributes['href']
+		link.text = e.text
+		group.linkss << link
+	      end
+	      e.elements.each('/descendant::TARGET') do |e|
+		target = TARGET.new
+		target.entry_id = e.attributes['id']
+		target.start = e.attributes['start']
+		target.stop = e.attributes['stop']
+		target.text = e.text
+		group.targets << target
+	      end
+	      feature.groups << group
+	    end
+	    segment.features << feature
+	  end
+	  gff.segments << segment
 	end
       end
       gff
     end
 
-#    private
 
     class DSN
+      attr_accessor :source, :source_id, :source_version,
+	:mapmaster, :description, :description_href
+    end
 
-      def initialize()
-        @source         = nil
-        @source_id      = nil
-        @source_version = nil
-        @mapmaster      = nil
-        @descriotion    = nil
-        @descriotion_href    = nil
+    class ENTRY_POINT
+      def initialize
+	@segments = Array.new
       end
-      attr_accessor :source
-      attr_accessor :source_id
-      attr_accessor :source_version
-      attr_accessor :mapmaster
-      attr_accessor :description
-      attr_accessor :description_href
+      attr_reader :segments
+      attr_accessor :href, :version
+    end
 
+    class SEGMENT
+      def initialize
+	@features = Array.new		# for FEATURE
+	@types = Array.new		# for TYPE
+      end
+      attr_accessor :entry_id, :start, :stop, :orientation, :description,
+	:subparts,			# optional
+	:features, :version, :label,	# for FEATURE
+	:types				# for TYPE
     end
 
     class DNA
+      attr_accessor :entry_id, :start, :stop, :version, :sequence, :length
+    end
 
-      def initialize()
-        @sequence         = Array.new()
-      end
-      attr_accessor :sequence
+    class SEQUENCE
+      attr_accessor :entry_id, :start, :stop, :moltype, :version, :sequence
+    end
 
+    class TYPES < ENTRY_POINT; end
+
+    class TYPE
+      attr_accessor :entry_id, :method, :category, :count,
+	:reference, :label	# for FEATURE
     end
 
     class GFF
-
-      def initialize()
-        @gff_version        = nil
-        @gff_href           = nil
-        @segment            = Array.new(6)
+      def initialize
+	@segments = Array.new
       end
-      attr_accessor :gff_version
-      attr_accessor :gff_href
-      attr_accessor :segment
+      attr_reader :segments
+      attr_accessor :version, :href
     end
-  end
 
+    class FEATURE
+      def initialize
+	@notes = Array.new
+	@links = Array.new
+	@types = Array.new
+	@targets = Array.new
+	@groups = Array.new
+      end
+      attr_accessor :entry_id, :label,
+	:method_id, :method, :start, :stop, :score, :orientation, :phase
+      attr_reader :notes, :links, :types, :targets, :groups
+    end
+
+    class LINK
+      attr_accessor :href, :text
+    end
+
+    class TARGET
+      attr_accessor :entry_id, :start, :stop, :name
+    end
+
+    class GROUP
+      def initialize
+	@notes = Array.new
+	@links = Array.new
+	@targets = Array.new
+      end
+      attr_accessor :entry_id, :label, :type
+      attr_reader :notes, :links, :targets
+    end
+
+  end
 end
 
 
@@ -242,16 +389,16 @@ if __FILE__ == $0
   rescue LoadError
   end
 
-  wormbase = Bio::DAS.new("www.wormbase.org", "db", 80)
+  wormbase = Bio::DAS.new('www.wormbase.org', 'db', 80)
 
   p wormbase.get_dsn
-  p wormbase.get_dna("elegans", "I", 0, 1000)
-  gff = wormbase.get_features("elegans", "I", 0, 1000)
+  p wormbase.get_dna('elegans', 'I', 0, 1000)
+  gff = wormbase.get_features('elegans', 'I', 0, 1000)
   (0..4).each do |i|
     p gff.segment[i]
   end
   gff.segment[5].each do |i|
-    puts "<----- feature ----->\n"
+    puts '<----- feature ----->\n'
     i.each do |j|
       p j
     end
