f967d85e09772e40aed75d5df2c7f06b1049280a
Naohisa Goto <ng@bioruby.org>
2003-04-25 14:11:10 +0000

    * Added MaXML::Annotations class and MaXML::Annotations#cds_start, cds_stop, gene_name, data_source, evidence methods. * Added MaXML::Cluster#representative_sequence and representative_annotations methods.

--- lib/bio/db/fantom.rb
+++ lib/bio/db/fantom.rb
@@ -17,7 +17,7 @@
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
-#  $Id: fantom.rb,v 1.3 2003/04/24 15:48:01 ng Exp $
+#  $Id: fantom.rb,v 1.4 2003/04/25 14:11:10 ng Exp $
 #
 
 begin
@@ -108,8 +108,17 @@ module Bio
 	  @sequences
 	end
 
-	def sequence(idstr)
-	  sequences[idstr]
+	def sequence(idstr = nil)
+	  idstr ? sequences[idstr] : representative_sequence
+	end
+
+	def representative_sequence
+	  sequences[representative_seqid]
+	end
+
+	def representative_annotations
+	  e = representative_sequence
+	  e ? e.annotations : nil
 	end
 
 	define_element_text_method(%w(fantomid))
@@ -175,14 +184,96 @@ module Bio
 
 	def annotations
 	  unless defined?(@annotations)
-	    @annotations = @elem.get_elements('annotations/annotation')
-	    @annotations.collect! { |e| Annotation.new(e) }
+	    @annotations =
+	      MaXML::Annotations.new(@elem.elements['annotations'])
 	  end
 	  @annotations
 	end
 
 	define_element_text_method(%w(seqid fantomid cloneid rearrayid accession annotator version modified_time comment))
       end #class MaXML::Sequence
+
+      class Annotations < MaXML
+	Data_XPath = nil
+
+	include Enumerable
+	def each
+	  to_a.each { |x| yield x }
+	end
+
+	def to_a
+	  unless defined?(@a)
+	    @a = @elem.get_elements('annotation')
+	    @a.collect! { |e| MaXML::Annotation.new(e) }
+	  end
+	  @a
+	end
+
+	def get_all_by_qualifier(qstr)
+	  unless defined?(@hash)
+	    @hash = {}
+	  end
+	  unless @hash.member?(qstr) then
+	    @hash[qstr] = self.find_all do |x|
+	      x.qualifier == qstr
+	    end
+	  end
+	  @hash[qstr]
+	end
+
+	def get_by_qualifier(qstr)
+	  a = get_all_by_qualifier(qstr)
+	  a ? a[0] : nil
+	end
+
+	def [](*arg)
+	  if arg[0].is_a?(String) and arg.size == 1 then
+	    get_by_qualifier(arg[0])
+	  else
+	    to_a[*arg]
+	  end
+	end
+
+	def cds_start
+	  unless defined?(@cds_start)
+	    e = get_by_qualifier('cds_start')
+	    @cds_start = e ? e.anntext.to_i : nil
+	  end
+	  @cds_start
+	end
+
+	def cds_stop
+	  unless defined?(@cds_stop)
+	    e = get_by_qualifier('cds_stop')
+	    @cds_stop = e ? e.anntext.to_i : nil
+	  end
+	  @cds_stop
+	end
+
+	def gene_name
+	  unless defined?(@gene_name)
+	    e = get_by_qualifier('gene_name')
+	    @gene_name = e ? e.anntext : nil
+	  end
+	  @gene_name
+	end
+
+	def data_source
+	  unless defined?(@data_source)
+	    e = get_by_qualifier('gene_name')
+	    @data_source = e ? e.datasrc[0] : nil
+	  end
+	  @data_source
+	end
+
+	def evidence
+	  unless defined?(@evidence)
+	    e = get_by_qualifier('gene_name')
+	    @evidence = e ? e.evidence : nil
+	  end
+	  @evidence
+	end
+      end #class MaXML::Annotations
 
       class Annotation < MaXML
 	def entry_id
@@ -210,7 +301,6 @@ module Bio
 	end
 
 	define_element_text_method(%w(qualifier srckey anntext evidence))
-
       end #class MaXML::Annotation
 
     end #class MaXML
@@ -232,6 +322,23 @@ end #module Bio
 
 --- Bio::FANTOM::MaXML::Cluster.new(str)
 
+--- Bio::FANTOM::MaXML::Cluster#entry_id
+
+--- Bio::FANTOM::MaXML::Cluster#fantomid
+
+--- Bio::FANTOM::MaXML::Cluster#representative_seqid
+
+--- Bio::FANTOM::MaXML::Cluster#sequences
+
+ Lists sequences in this cluster.
+ Returns Bio::FANTOM::MaXML::Sequences object.
+
+--- Bio::FANTOM::MaXML::Cluster#sequence(id_str)
+
+ Shows a sequence information of given id.
+ Returns Bio::FANTOM::MaXML::Sequence object or nil.
+
+
 = Bio::FANTOM::MaXML::Sequences
 
  The instances of this class are automatically created
@@ -243,6 +350,23 @@ end #module Bio
  In addition, this class can be used for 'allseq.xml' and 'repseq.xml',
  but you'd better not to use them, becase of the speed is very slow.
 
+--- Bio::FANTOM::MaXML::Sequences#to_a
+
+ Returns an Array of Bio::FANTOM::MaXML::Sequence objects.
+
+--- Bio::FANTOM::MaXML::Sequences#each
+
+--- Bio::FANTOM::MaXML::Sequences#[](x)
+
+ Same as to_a[x] when x is a integer.
+ Same as get[x] when x is a string.
+
+--- Bio::FANTOM::MaXML::Sequences#get(id_str)
+
+ Shows a sequence information of given id.
+ Returns Bio::FANTOM::MaXML::Sequence object or nil.
+
+
 = Bio::FANTOM::MaXML::Sequence
 
  This class is for 'allseq.sep.xml' and 'repseq.sep.xml' found at
@@ -253,15 +377,63 @@ end #module Bio
  In addition, the instances of this class are automatically created
  by Bio::FANTOM::MaXML::Sequences class.
 
+--- Bio::FANTOM::MaXML::Sequence.new(str)
+
+--- Bio::FANTOM::MaXML::Sequence#entry_id
+
+--- Bio::FANTOM::MaXML::Sequence#altid(type_str = nil)
+
+ Returns hash of altid if no arguments are given.
+ Returns ID as a string if a type of ID (string) is given.
+
+--- Bio::FANTOM::MaXML::Sequence#annotations
+
+ Get lists of annotation data.
+ Returns an Array of Bio::FANTOM::MaXML::Annotation objects.
+
+--- Bio::FANTOM::MaXML::Sequence#seqid
+
+--- Bio::FANTOM::MaXML::Sequence#fantomid
+
+--- Bio::FANTOM::MaXML::Sequence#cloneid
+
+--- Bio::FANTOM::MaXML::Sequence#rearrayid
+
+--- Bio::FANTOM::MaXML::Sequence#accession
+
+--- Bio::FANTOM::MaXML::Sequence#annotator
+
+--- Bio::FANTOM::MaXML::Sequence#version
+
+--- Bio::FANTOM::MaXML::Sequence#modified_time
+
+--- Bio::FANTOM::MaXML::Sequence#comment
+
 = Bio::FANTOM::MaXML::Annotation
 
  The instances of this class are automatically created
  by Bio::FANTOM::MaXML::Sequence class.
 
+--- Bio::FANTOM::MaXML::Annotation#datasrc
+
+ Returns an Array of Bio::FANTOM::MaXML::Annotation::DataSrc objects.
+
+--- Bio::FANTOM::MaXML::Annotation#qualifier
+
+--- Bio::FANTOM::MaXML::Annotation#srckey
+
+--- Bio::FANTOM::MaXML::Annotation#anntext
+
+--- Bio::FANTOM::MaXML::Annotation#evidence
+
 = Bio::FANTOM::MaXML::Annotation::DataSrc < String
 
  The instances of this class are automatically created
  by Bio::FANTOM::MaXML::Annotation class.
+
+---- Bio::FANTOM::MaXML::Annotation::DataSrc#href
+
+ Shows a link URL to database web page as an String.
 
 = References
 
