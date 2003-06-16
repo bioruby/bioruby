57e1772e6aa9b561b622721282ece8acd9558ce8
Toshiaki Katayama <k@bioruby.org>
2003-06-16 18:50:48 +0000

    * s/S/C/ for n

--- lib/bio/appl/blast/xmlparser.rb
+++ lib/bio/appl/blast/xmlparser.rb
@@ -1,7 +1,7 @@
 #
 # bio/appl/blast/xmlparser.rb - BLAST XML output (-m 7) parser by XMLParser
 # 
-#   Copyright (C) 2001 Mitsuteru S. Nakao <n@bioruby.org>
+#   Copyright (C) 2001 Mitsuteru C. Nakao <n@bioruby.org>
 #   Copyright (C) 2003 KATAYAMA Toshiaki <k@bioruby.org>
 #
 #  This library is free software; you can redistribute it and/or
@@ -18,7 +18,7 @@
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
-#  $Id: xmlparser.rb,v 1.10 2003/02/05 03:28:26 k Exp $
+#  $Id: xmlparser.rb,v 1.11 2003/06/16 18:50:48 k Exp $
 #
 
 begin
