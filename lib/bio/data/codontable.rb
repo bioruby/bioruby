970ad0fe541c1d5496488ea383df215f1d9c0aed
Toshiaki Katayama <k@bioruby.org>
2004-06-26 04:39:07 +0000

    * added test code with colored codon table example * also added generate_stop_re private method

--- lib/bio/data/codontable.rb
+++ lib/bio/data/codontable.rb
@@ -17,7 +17,7 @@
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
-#  $Id: codontable.rb,v 0.9 2004/06/23 03:37:04 k Exp $
+#  $Id: codontable.rb,v 0.10 2004/06/26 04:39:07 k Exp $
 #
 
 module Bio
@@ -36,7 +36,7 @@ class CodonTable
     @table = hash
     @definition = definition
     @start = start_re
-    @stop = stop_re
+    @stop = stop_re || generate_stop_re
   end
   attr_accessor :table, :definition, :start, :stop
 
@@ -63,6 +63,16 @@ class CodonTable
     @stop.match(codon) ? true : false
   end
 
+  def generate_stop_re
+    list = []
+    @table.each do |codon, aa|
+      if aa == '*'
+        list << codon
+      end
+    end
+    Regexp.new(list.join('|'), Regexp::IGNORECASE)
+  end
+  private :generate_stop_re
 
   Definitions = {
 
@@ -88,44 +98,44 @@ class CodonTable
 
 
   Starts = {
-    1	=> /ttg|ctg|atg/,     # /ttg|ctg|atg|gtg/ ? NCBI #SG1 document
-    2	=> /att|atc|ata|atg|gtg/,
-    3	=> /ata|atg/,
-    4	=> /tta|ttg|ctg|att|atc|ata|atg|gtg/,
-    5	=> /ttg|att|atc|ata|atg|gtg/,
-    6	=> /atg/,
-    9	=> /atg|gtg/,
-    10	=> /atg/,
-    11	=> /ttg|ctg|att|atc|ata|atg|gtg/,
-    12	=> /ctg|atg/,
-    13	=> /atg/,
-    14	=> /atg/,
-    15	=> /atg/,
-    16	=> /atg/,
-    21	=> /atg|gtg/,
-    22	=> /atg/,
-    23	=> /att|atg|gtg/,
+    1	=> /ttg|ctg|atg/i,     # /ttg|ctg|atg|gtg/i ? NCBI #SG1 document
+    2	=> /att|atc|ata|atg|gtg/i,
+    3	=> /ata|atg/i,
+    4	=> /tta|ttg|ctg|att|atc|ata|atg|gtg/i,
+    5	=> /ttg|att|atc|ata|atg|gtg/i,
+    6	=> /atg/i,
+    9	=> /atg|gtg/i,
+    10	=> /atg/i,
+    11	=> /ttg|ctg|att|atc|ata|atg|gtg/i,
+    12	=> /ctg|atg/i,
+    13	=> /atg/i,
+    14	=> /atg/i,
+    15	=> /atg/i,
+    16	=> /atg/i,
+    21	=> /atg|gtg/i,
+    22	=> /atg/i,
+    23	=> /att|atg|gtg/i,
   }
 
 
   Stops = {
-    1	=> /taa|tag|tga/,
-    2	=> /taa|tag|aga|agg/,
-    3	=> /taa|tag/,
-    4	=> /taa|tag/,
-    5	=> /taa|tag/,
-    6	=> /tga/,
-    9	=> /taa|tag/,
-    10	=> /taa|tag/,
-    11	=> /taa|tag|tga/,
-    12	=> /taa|tag|tga/,
-    13	=> /taa|tag/,
-    14	=> /tag/,
-    15	=> /taa|tga/,
-    16	=> /taa|tga/,
-    21	=> /taa|tag/,
-    22	=> /tca|taa|tga/,
-    23	=> /tta|taa|tag|tga/,
+    1	=> /taa|tag|tga/i,
+    2	=> /taa|tag|aga|agg/i,
+    3	=> /taa|tag/i,
+    4	=> /taa|tag/i,
+    5	=> /taa|tag/i,
+    6	=> /tga/i,
+    9	=> /taa|tag/i,
+    10	=> /taa|tag/i,
+    11	=> /taa|tag|tga/i,
+    12	=> /taa|tag|tga/i,
+    13	=> /taa|tag/i,
+    14	=> /tag/i,
+    15	=> /taa|tga/i,
+    16	=> /taa|tga/i,
+    21	=> /taa|tag/i,
+    22	=> /tca|taa|tga/i,
+    23	=> /tta|taa|tag|tga/i,
   }
 
 
@@ -529,6 +539,212 @@ class CodonTable
 end # CodonTable
 
 end # module Bio
+
+
+if __FILE__ == $0
+
+  class ColoredCodonTable
+
+    @@colors = {
+      :text		=> "\e[30m",    # black
+      :aa		=> "\e[32m",    # green
+      :start		=> "\e[31m",    # red
+      :stop		=> "\e[31m",    # red
+      :basic		=> "\e[36m",    # cyan
+      :polar		=> "\e[34m",    # blue
+      :acidic		=> "\e[35m",    # magenta
+      :nonpolar		=> "\e[33m",    # yellow
+    }
+
+    @@properties = {
+      :basic => %w( H K R ),
+      :polar => %w( S T Y Q N S ),
+      :acidic => %w( D E ),
+      :nonpolar => %w( F L I M V P A C W G ),
+      :stop => %w( * ),
+    }
+
+    def initialize(number)
+      require 'bio/data/aa'
+      @aacode = Bio::AminoAcid.names
+      @ctable = Bio::CodonTable[number]
+      @number = number
+      table_number_check
+      generate_colored_text
+    end
+
+    def table_number_check
+      unless @ctable.definition
+        puts "ERROR: Codon table No.#{@number} not found."
+        Bio::CodonTable::Definitions.sort.each do |i, definition|
+          puts "#{i}\t#{definition}"
+        end
+        exit 1
+      end
+    end
+
+    def generate_colored_text
+      @ctable.table.each do |codon, aa|
+        property, = @@properties.detect {|key, list| list.include?(aa)}
+        if aa == '*'
+          color_code = "#{@@colors[:stop]}STOP"
+          color_aa = ""
+        else
+          color_code = "#{@@colors[property]}#{@aacode[aa]}"
+          if @ctable.start_codon?(codon)
+            color_aa = "#{@@colors[:start]}#{aa}"
+          else
+            color_aa = "#{@@colors[:aa]}#{aa}"
+          end
+        end
+        eval("@#{codon} = ' #{color_code} #{color_aa}#{@@colors[:text]} '")
+      end
+
+      @hydrophilic = [
+        "#{@@colors[:basic]}basic#{@@colors[:text]},",
+        "#{@@colors[:polar]}polar#{@@colors[:text]},",
+        "#{@@colors[:acidic]}acidic#{@@colors[:text]}"
+      ].join(" ")
+      @hydrophobic = "#{@@colors[:nonpolar]}nonpolar"
+    end
+
+    def output
+      text = <<-END
+        #
+        #= Codon table #{@number} : #{@ctable.definition}
+        #
+        #  hydrophilic: #{@hydrophilic}
+        #  hydrophobic: #{@hydrophobic}
+        #
+        #*---------------------------------------------*
+        #|       |              2nd              |     |
+        #|  1st  |-------------------------------| 3rd |
+        #|       |  U    |  C    |  A    |  G    |     |
+        #|-------+-------+-------+-------+-------+-----|
+        #| U   U |#{@ttt}|#{@tct}|#{@tat}|#{@tgt}|  u  |
+        #| U   U |#{@ttc}|#{@tcc}|#{@tac}|#{@tgc}|  c  |
+        #| U   U |#{@tta}|#{@tca}|#{@taa}|#{@tga}|  a  |
+        #|  UUU  |#{@ttg}|#{@tcg}|#{@tag}|#{@tgg}|  g  |
+        #|-------+-------+-------+-------+-------+-----|
+        #|  CCCC |#{@ctt}|#{@cct}|#{@cat}|#{@cgt}|  u  |
+        #| C     |#{@ctc}|#{@ccc}|#{@cac}|#{@cgc}|  c  |
+        #| C     |#{@cta}|#{@cca}|#{@caa}|#{@cga}|  a  |
+        #|  CCCC |#{@ctg}|#{@ccg}|#{@cag}|#{@cgg}|  g  |
+        #|-------+-------+-------+-------+-------+-----|
+        #|   A   |#{@att}|#{@act}|#{@aat}|#{@agt}|  u  |
+        #|  A A  |#{@atc}|#{@acc}|#{@aac}|#{@agc}|  c  |
+        #| AAAAA |#{@ata}|#{@aca}|#{@aaa}|#{@aga}|  a  |
+        #| A   A |#{@atg}|#{@acg}|#{@aag}|#{@agg}|  g  |
+        #|-------+-------+-------+-------+-------+-----|
+        #|  GGGG |#{@gtt}|#{@gct}|#{@gat}|#{@ggt}|  u  |
+        #| G     |#{@gtc}|#{@gcc}|#{@gac}|#{@ggc}|  c  |
+        #| G GGG |#{@gta}|#{@gca}|#{@gaa}|#{@gga}|  a  |
+        #|  GG G |#{@gtg}|#{@gcg}|#{@gag}|#{@ggg}|  g  |
+        #*---------------------------------------------*
+        #
+      END
+      text.gsub(/^\s+#/, @@colors[:text])
+    end
+
+  end
+
+  if ARGV.size > 0
+
+    number = ARGV.shift.to_i
+    cct = ColoredCodonTable.new(number)
+    puts cct.output
+
+  else
+
+    # Boring test code comes here as usual
+    
+    begin
+      require 'pp'
+      alias :p :pp
+    rescue LoadError
+    end
+
+    puts "### Bio::CodonTable[1]"
+    ct1 = Bio::CodonTable[1]
+
+    puts ">>> Bio::CodonTable#table"
+    p ct1.table
+
+    puts ">>> Bio::CodonTable#definition"
+    p ct1.definition
+
+    puts ">>> Bio::CodonTable#['atg']"
+    p ct1['atg']
+
+    puts ">>> Bio::CodonTable#revtrans('A')"
+    p ct1.revtrans('A')
+
+    puts ">>> Bio::CodonTable#start_codon?('atg')"
+    p ct1.start_codon?('atg')
+    
+    puts ">>> Bio::CodonTable#start_codon?('aaa')"
+    p ct1.start_codon?('aaa')
+
+    puts ">>> Bio::CodonTable#stop_codon?('tag')"
+    p ct1.stop_codon?('tag')
+    
+    puts ">>> Bio::CodonTable#stop_codon?('aaa')"
+    p ct1.stop_codon?('aaa')
+
+    puts "### ct = Bio::CodonTable.new(hash, definition)"
+    hash = {
+      'ttt' => 'F', 'tct' => 'S', 'tat' => 'Y', 'tgt' => 'C',
+      'ttc' => 'F', 'tcc' => 'S', 'tac' => 'Y', 'tgc' => 'C',
+      'tta' => 'L', 'tca' => 'S', 'taa' => '*', 'tga' => 'U',
+      'ttg' => 'L', 'tcg' => 'S', 'tag' => '*', 'tgg' => 'W',
+
+      'ctt' => 'L', 'cct' => 'P', 'cat' => 'H', 'cgt' => 'R',
+      'ctc' => 'L', 'ccc' => 'P', 'cac' => 'H', 'cgc' => 'R',
+      'cta' => 'L', 'cca' => 'P', 'caa' => 'Q', 'cga' => 'R',
+      'ctg' => 'L', 'ccg' => 'P', 'cag' => 'Q', 'cgg' => 'R',
+
+      'att' => 'I', 'act' => 'T', 'aat' => 'N', 'agt' => 'S',
+      'atc' => 'I', 'acc' => 'T', 'aac' => 'N', 'agc' => 'S',
+      'ata' => 'I', 'aca' => 'T', 'aaa' => 'K', 'aga' => 'R',
+      'atg' => 'M', 'acg' => 'T', 'aag' => 'K', 'agg' => 'R',
+
+      'gtt' => 'V', 'gct' => 'A', 'gat' => 'D', 'ggt' => 'G',
+      'gtc' => 'V', 'gcc' => 'A', 'gac' => 'D', 'ggc' => 'G',
+      'gta' => 'V', 'gca' => 'A', 'gaa' => 'E', 'gga' => 'G',
+      'gtg' => 'V', 'gcg' => 'A', 'gag' => 'E', 'ggg' => 'G',
+    }
+    my_ct = Bio::CodonTable.new(hash, "my codon table")
+
+    puts ">>> ct.definition"
+    puts my_ct.definition
+
+    puts ">>> ct.definition=(str)"
+    my_ct.definition = "selenoproteins (Eukaryote)"
+    puts my_ct.definition
+
+    puts ">>> ct['tga']"
+    puts my_ct['tga']
+
+    puts ">>> ct.revtrans('U')"
+    puts my_ct.revtrans('U')
+
+    puts ">>> ct.stop_codon?('tga')"
+    puts my_ct.stop_codon?('tga')
+
+    puts ">>> ct.stop_codon?('tag')"
+    puts my_ct.stop_codon?('tag')
+
+    puts "#"
+    puts "# Example (try to run this module with a codon table number)"
+    puts "#   % ruby codontable.rb 1"
+    puts "#"
+
+    cct = ColoredCodonTable.new(1)
+    puts cct.output
+
+  end
+
+end
 
 
 
