082629c8acca85414935e60e876ec36475bf5b66
Mitsuteru Nakao <n@bioruby.org>
2005-10-23 10:40:40 +0000

    * Fixed bioruby_libpath.

--- test/runner.rb
+++ test/runner.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/env ruby
 
 require 'test/unit'
+require 'pathname'
+
+bioruby_libpath = Pathname.new(File.join(File.dirname(__FILE__), ['..'], 'lib')).cleanpath.to_s
+$:.unshift(bioruby_libpath) unless $:.include?(bioruby_libpath)
 
 exit Test::Unit::AutoRunner.run(false, File.dirname($0))
 
