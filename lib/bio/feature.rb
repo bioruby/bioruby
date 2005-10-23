7d325c60c32390de218e99a5f49c7c7e9a7c294c
Toshiaki Katayama <k@bioruby.org>
2005-10-23 07:21:00 +0000

    * Changed to RDoc format

--- lib/bio/feature.rb
+++ lib/bio/feature.rb
@@ -1,7 +1,44 @@
 #
-# bio/feature.rb - Features/Feature class (GenBank Feature table)
+# = bio/feature.rb - Features/Feature class (GenBank Feature table)
 #
-#   Copyright (C) 2002 KATAYAMA Toshiaki <k@bioruby.org>
+# Author::	Toshiaki Katayama <k@bioruby.org>
+# Copyright::	Copyright (c) 2002, 2005 BioRuby project
+# License::	LGPL
+#
+# $Id: feature.rb,v 1.7 2005/10/23 07:21:00 k Exp $
+#
+# == INSD Feature table definition
+#
+# See http://www.ddbj.nig.ac.jp/FT/full_index.html for the INSD
+# (GenBank/EMBL/DDBJ) Feature table definition.
+#
+# === Example
+#
+#  # suppose features is a Bio::Features object
+#  features.each do |feature|
+#    f_name = feature.feature
+#    f_pos  = feature.position
+#    puts "#{f_name}:\t#{f_pos}"
+#    feature.each do |qualifier|
+#      q_name = qualifier.qualifier
+#      q_val  = qualifier.value
+#      puts "- #{q_name}:\t#{q_val}"
+#    end
+#  end
+#
+#  # Iterates only on CDS features and extract translated amino acid sequences
+#  features.each("CDS") do |feature|
+#    hash = feature.assoc
+#    name = hash["gene"] || hash["product"] || hash["note"] 
+#    seq  = hash["translation"]
+#    pos  = feature.position
+#    if gene and seq
+#      puts ">#{gene} #{feature.position}"
+#      puts aaseq
+#    end
+#  end
+#
+#--
 #
 #  This library is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU Lesser General Public
@@ -17,113 +54,111 @@
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
-#  $Id: feature.rb,v 1.6 2005/09/08 01:22:08 k Exp $
+#++
 #
 
 require 'bio/location'
 
 module Bio
 
-  class Feature
+# Container for the sequence annotation.
+class Feature
 
-    def initialize(feature = '', position = '', qualifiers = [])
-      @feature, @position, @qualifiers = feature, position, qualifiers
+  def initialize(feature = '', position = '', qualifiers = [])
+    @feature, @position, @qualifiers = feature, position, qualifiers
+  end
+
+  # Returns type of feature in String (e.g 'CDS', 'gene')
+  attr_accessor :feature
+
+  # Returns position of the feature in String (e.g. 'complement(123..146)')
+  attr_accessor :position
+
+  # Returns an Array of Qualifier objects.
+  attr_accessor :qualifiers
+
+  # Returns a Bio::Locations object translated from the position string.
+  def locations
+    Locations.new(@position)
+  end
+
+  # Appends a Qualifier object to the Feature.
+  #
+  # * Returns an Array of Qualifier objects.
+  # * If the argument is not a Qualifier object, returns nil.
+  #
+  def append(a)
+    @qualifiers.push(a) if a.is_a? Qualifier
+    return self
+  end
+
+  # Iterates on each qualifier.
+  def each
+    @qualifiers.each do |x|
+      yield x
     end
-    attr_accessor :feature, :position, :qualifiers
+  end
 
-    def locations
-      Locations.new(@position)
+  # Returns a Hash constructed from qualifier objects.
+  def assoc
+    hash = Hash.new
+    @qualifiers.each do |x|
+      hash[x.qualifier] = x.value
     end
-
-    def append(a)
-      @qualifiers.push(a) if a.is_a? Qualifier
-      return self
-    end
-
-    def each
-      @qualifiers.each do |x|
-        yield x
-      end
-    end
-
-    def assoc
-      hash = Hash.new
-      @qualifiers.each do |x|
-        hash[x.qualifier] = x.value
-      end
-      return hash
-    end
-
-
-    class Qualifier
-
-      def initialize(key, value)
-        @qualifier, @value = key, value
-      end
-      attr_reader :qualifier, :value
-
-    end
-
+    return hash
   end
 
 
-  class Features
+  # Container for the qualifier-value pair.
+  class Qualifier
 
-    def initialize(ary = [])
-      @features = ary
-    end
-    attr_accessor :features
-
-    def append(a)
-      @features.push(a) if a.is_a? Feature
-      return self
+    def initialize(key, value)
+      @qualifier, @value = key, value
     end
 
-    def each(arg = nil)
-      @features.each do |x|
-        next if arg and x.feature != arg
-        yield x
-      end
-    end
+    # Qualifier name in String
+    attr_reader :qualifier
 
-    def [](*arg)
-      @features[*arg]
-    end
+    # Qualifier value in String
+    attr_reader :value
 
   end
 
 end
 
 
-=begin
+# Container for the list of Feature objects.
+class Features
 
-= Bio::Feature
+  def initialize(ary = [])
+    @features = ary
+  end
 
---- Bio::Feature.new(feature = '', position = '', qualifiers = [])
+  # Returns an Array of Feature objects.
+  attr_accessor :features
 
---- Bio::Feature#feature -> String
---- Bio::Feature#position -> String
---- Bio::Feature#qualifiers -> Array
+  # Appends a Feature object to Features.
+  def append(a)
+    @features.push(a) if a.is_a? Feature
+    return self
+  end
 
---- Bio::Feature#locations -> Bio::Locations
---- Bio::Feature#append -> Bio::Feature
---- Bio::Feature#each -> Array
+  # Iterates on each feature.  If a feature name is given as an argument,
+  # only iterates on each feature belongs to the name (e.g. 'CDS' etc.)
+  def each(arg = nil)
+    @features.each do |x|
+      next if arg and x.feature != arg
+      yield x
+    end
+  end
 
-== Bio::Feature::Qualifier
+  # Short cut for the Features#features[n]
+  def [](*arg)
+    @features[*arg]
+  end
 
---- Bio::Feature::Qualifier.new(key, value)
+end
 
---- Bio::Feature::Qualifier#qualifier -> String
---- Bio::Feature::Qualifier#value -> String
-
-= Bio::Features
-
---- Bio::Features.new(ary = [])
-
---- Bio::Features#features -> Array
---- Bio::Features#append(a) -> Bio::Features
---- Bio::Features#each -> Array
-
-=end
+end # Bio
 
 
