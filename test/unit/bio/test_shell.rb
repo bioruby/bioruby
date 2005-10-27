55eb79a5a02ab8ec3ff5cd23ac7bbccb9499fef8
Mitsuteru Nakao <n@bioruby.org>
2005-10-27 15:10:48 +0000

    * Initially imported unit test for Bio::Shell.

---
+++ test/unit/bio/test_shell.rb
@@ -0,0 +1,42 @@
+#
+# test/unit/bio/test_shell.rb - Unit test for Bio::Shell
+#
+#   Copyright (C) 2005 Mitsuteru Nakao <n@bioruby.org>
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+#
+#  $Id: test_shell.rb,v 1.1 2005/10/27 15:10:48 nakao Exp $
+#
+
+require 'pathname'
+libpath = Pathname.new(File.join(File.dirname(__FILE__), ['..'] * 3, 'lib')).cleanpath.to_s
+$:.unshift(libpath) unless $:.include?(libpath)
+
+require 'test/unit'
+require 'bio/shell'
+
+module Bio
+  class TestShell < Test::Unit::TestCase
+
+    def test_const_bioruby_config
+      assert_equal($bioruby_config, {})
+    end
+
+    def test_const_bioruby_cache
+      assert_equal($bioruby_cache, {})
+    end
+
+  end
+end
