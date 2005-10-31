c4043a704676adc6f290d545c7ce51998914015b
Naohisa Goto <ng@bioruby.org>
2005-10-31 13:17:18 +0000

    changed document format form RD to RDoc. added references to the document. added MAFFT version 5 information.

--- lib/bio/appl/mafft.rb
+++ lib/bio/appl/mafft.rb
@@ -1,8 +1,10 @@
 #
-# bio/appl/mafft.rb - MAFFT wrapper class
+# = bio/appl/mafft.rb - MAFFT wrapper class
 #
-#   Copyright (C) 2003 GOTO Naohisa <ngoto@gen-info.osaka-u.ac.jp>
+# Copyright:: Copyright (C) 2003 GOTO Naohisa <ngoto@gen-info.osaka-u.ac.jp>
+# Licence::   LGPL
 #
+#--
 #  This library is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU Lesser General Public
 #  License as published by the Free Software Foundation; either
@@ -16,21 +18,49 @@
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+#++
 #
-#  $Id: mafft.rb,v 1.6 2005/09/09 15:56:20 ngoto Exp $
+#  $Id: mafft.rb,v 1.7 2005/10/31 13:17:18 ngoto Exp $
+#
+# Bio::MAFFT is a wrapper class to execute MAFFT.
+# MAFFT is a very fast multiple sequence alignment software.
+#
+# = Important Notes
+#
+# Though Bio::MAFFT class currently supports only MAFFT version 3,
+# you can use MAFFT version 5 because the class is a wrapper class.
+#
+# == References
+#
+# * K. Katoh, K. Misawa, K. Kuma and T. Miyata.
+#   MAFFT: a novel method for rapid multiple sequence alignment based
+#   on fast Fourier transform. Nucleic Acids Res. 30: 3059-3066, 2002.
+#   http://nar.oupjournals.org/cgi/content/abstract/30/14/3059
+# * http://www.biophys.kyoto-u.ac.jp/~katoh/programs/align/mafft/
 #
 
 require 'bio/db/fasta'
 require 'bio/io/flatfile'
 
+#--
 # We use Open3.popen3, because MAFFT on win32 requires Cygwin.
+#++
 require 'open3'
 
-module Bio
+module Bio #:nodoc:
+
+  # Bio::MAFFT is a wrapper class to execute MAFFT.
+  # MAFFT is a very fast multiple sequence alignment software.
+  #
+  # Though Bio::MAFFT class currently supports only MAFFT version 3,
+  # you can use MAFFT version 5 because the class is a wrapper class.
   class MAFFT
 
     autoload :Report,       'bio/appl/mafft/report'
 
+    # Creates a new alignment factory.
+    # When +n+ is a number (1,2,3, ...), performs 'fftns n'.
+    # When +n+ is :i or 'i', performs 'fftnsi'.
     def self.fftns(n = nil)
       opt = []
       if n.to_s == 'i' then
@@ -41,10 +71,16 @@ module Bio
       end
     end
 
+    # Creates a new alignment factory.
+    # Performs 'fftnsi'.
     def self.fftnsi
       self.new2(nil, 'fftnsi')
     end
 
+    # Creates a new alignment factory.
+    # When +n+ is a number (1,2,3, ...), performs 'nwns n'.
+    # When +n+ is :i or 'i', performs 'nwnsi'.
+    # In both case, if all_positive is true, add option '--all-positive'.
     def self.nwns(n = nil, ap = nil)
       opt = []
       opt << '--all-positive' if ap
@@ -56,16 +92,26 @@ module Bio
       end
     end
 
-    def self.nwnsi(ap = nil)
+    # Creates a new alignment factory.
+    # Performs 'nwnsi'.
+    # If +all_positive+ is true, add option '--all-positive'.
+    def self.nwnsi(all_positive = nil)
       opt = []
-      opt << '--all-positive' if ap
+      opt << '--all-positive' if all_positive
       self.new2(nil, 'nwnsi', *opt)
     end
 
+    # Creates a new alignment factory.
+    # Performs 'nwns --all-positive n' or 'nwnsi --all-positive'.
+    # Same as Bio::MAFFT.nwap(n, true).
     def self.nwap(n = nil)
       self.nwns(n, true)
     end
 
+    # Creates a new alignment factory.
+    # +dir+ is the path of the MAFFT program.
+    # +prog+ is the name of the program.
+    # +opt+ is options of the program.
     def self.new2(dir, prog, *opt)
       if dir then
         prog = File.join(dir, prog)
@@ -73,6 +119,9 @@ module Bio
       self.new(prog, opt)
     end
 
+    # Creates a new alignment factory.
+    # +program+ is the name of the program.
+    # +opt+ is options of the program.
     def initialize(program, option)
       @program = program
       @option = option
@@ -81,10 +130,34 @@ module Bio
       @report = nil
       @log = nil
     end
-    attr_accessor :program, :option
-    attr_reader :command, :log
-    attr_reader :output, :report
-    
+
+    # program name
+    attr_accessor :program
+
+    # options
+    attr_accessor :option
+
+    # Shows last command-line string. Returns nil or an array of String.
+    # Note that filenames described in the command-line may already
+    # be removed because they are temporary files.
+    attr_reader :command
+
+    # last message to STDERR when executing the program.
+    attr_reader :log
+
+    # Shows latest raw alignment result.
+    # Since a result of MAFFT is simply a multiple-fasta format,
+    # it returns an array of Bio::FastaFormat instances
+    # instead of raw string.
+    attr_reader :output
+
+    # Shows last alignment result (instance of Bio::MAFFT::Report class)
+    # performed by the factory.
+    attr_reader :report
+
+    # Executes the program.
+    # If +seqs+ is not nil, perform alignment for seqs.
+    # If +seqs+ is nil, simply executes the program.
     def query(seqs)
       if seqs then
         query_align(seqs)
@@ -93,14 +166,17 @@ module Bio
       end
     end
 
+    # Performs alignment for seqs.
+    # +seqs+ should be Bio::Alignment or Array of sequences or nil.
     def query_align(seqs, *arg)
-      # seqs should be Bio::Alignment or Array of sequences or nil
       unless seqs.is_a?(Bio::Alignment)
         seqs = Bio::Alignment.new(seqs, *arg)
       end
       query_string(seqs.to_fasta(70))
     end
 
+    # Performs alignment for +str+.
+    # Str should be a string that can be recognized by the program.
     def query_string(str, *arg)
       begin
         tf_in = Tempfile.open('align')
@@ -113,6 +189,7 @@ module Bio
       r
     end
 
+    # Performs alignment of sequences in the file named +fn+.
     def query_by_filename(fn, seqtype = nil)
       opt = @option + [ fn ]
       exec_local(opt)
@@ -121,6 +198,7 @@ module Bio
     end
 
     private
+    # Executes a program in the local machine.
     def exec_local(opt)
       @command = [ @program, *opt ]
       #STDERR.print "DEBUG: ", @command.join(" "), "\n"
@@ -142,93 +220,3 @@ module Bio
   end #class MAFFT
 end #module Bio
 
-
-=begin
-
-= Bio::MAFFT
-
- Bio::MAFFT is a wrapper class of MAFFT, multiple sequence alignment software.
- ((<URL:http://www.biophys.kyoto-u.ac.jp/~katoh/programs/align/mafft/>))
-
---- Bio::MAFFT.fftns(n = nil)
-
-      Create new alignment factory.
-      When n is a number (1,2,3, ...), performs 'fftns n'.
-      When n is :i or 'i', performs 'fftnsi'.
-
---- Bio::MAFFT.fftnsi
-
-      Create new alignment factory.
-      Performs 'fftnsi'.
-
---- Bio::MAFFT.nwns(n = nil, all_positive = nil)
-
-      Create new alignment factory.
-      When n is a number (1,2,3, ...), performs 'nwns n'.
-      When n is :i or 'i', performs 'nwnsi'.
-      In both case, if all_positive is true, add option '--all-positive'.
-
---- Bio::MAFFT.nwnsi(all_positive = true)
-
-      Create new alignment factory.
-      Performs 'nwnsi'.
-      If all_positive is true, add option '--all-positive'.
-
---- Bio::MAFFT.nwap(n = nil)
-
-      Create new alignment factory.
-      Performs 'nwns --all-positive n' or 'nwnsi --all-positive'.
-      Same as Bio::MAFFT.nwap(n, true).
-
---- Bio::MAFFT.new(program, option)
-
-      Creates new alignment factory.
-
---- Bio::MAFFT#program
---- Bio::MAFFT#option
-
-      Access to the variables specified in initialize.
-
---- Bio::MAFFT#query(seqs)
-
-      Executes the program(clustalw).
-      If 'seqs' is not nil, perform alignment for seqs.
-      If 'seqs' is nil, simply executes the program.
-
---- Bio::MAFFT#query_align(seqs)
-
-      Performs alignment for seqs.
-
---- Bio::MAFFT#query_string(str)
-
-      Performs alignment for str.
-      Str should be a string that can be recognized by the program.
-
---- Bio::MAFFT#query_by_filename(filename)
-
-      Performs alignment of sequences in the file named filename.
-
---- Bio::MAFFT#command
-
-      Shows latest command-line executed by this factory.
-      Note that filenames described in the command-line may already
-      be removed because they are temporary files.
-      Returns an array of string.
-
---- Bio::MAFFT#log
-
-      Shows latest messages of execution.
-
---- Bio::MAFFT#report
-
-      Shows latest alignment result (instance of Bio::MAFFT::Report class)
-      performed by this factory.
-
---- Bio::MAFFT#output
-
-      Shows latest raw alignment result.
-      Since a result of MAFFT is simply a multiple-fasta format,
-      it returns an array of Bio::FastaFormat instances
-      instead of raw string.
-
-=end
