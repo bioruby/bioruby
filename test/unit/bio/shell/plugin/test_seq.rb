15c183bde1557f4a01440fdfe25e40b92c45ee46
Mitsuteru Nakao <n@bioruby.org>
2005-11-23 11:40:45 +0000

    * Fixed the order of argument of assert_equal.

--- test/unit/bio/shell/plugin/test_seq.rb
+++ test/unit/bio/shell/plugin/test_seq.rb
@@ -17,7 +17,7 @@
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
-#  $Id: test_seq.rb,v 1.1 2005/10/28 02:03:34 nakao Exp $
+#  $Id: test_seq.rb,v 1.2 2005/11/23 11:40:45 nakao Exp $
 #
 
 require 'pathname'
@@ -34,39 +34,39 @@ module Bio
 
     def test_naseq
       str = 'ACGT'
-      assert_equal(naseq(str).class, Bio::Sequence::NA)
-      assert_equal(naseq(str), Bio::Sequence::NA.new(str))
-      assert_equal(naseq(str), 'acgt')
+      assert_equal(Bio::Sequence::NA, naseq(str).class)
+      assert_equal(Bio::Sequence::NA.new(str), naseq(str))
+      assert_equal('acgt', naseq(str))
     end
 
     def test_aaseq
       str = 'WD'
-      assert_equal(aaseq(str).class, Bio::Sequence::AA)
-      assert_equal(aaseq(str), Bio::Sequence::AA.new('WD'))
-      assert_equal(aaseq(str), 'WD')
+      assert_equal(Bio::Sequence::AA, aaseq(str).class)
+      assert_equal(Bio::Sequence::AA.new('WD'), aaseq(str))
+      assert_equal('WD', aaseq(str))
     end
 
     def test_revseq
       str = 'acgta'
-      assert_equal(revseq(str), 'tacgt')
+      assert_equal('tacgt', revseq(str))
     end
 
     def test_translate
       str = 'ATGATG'
-      assert_equal(translate(str), Bio::Sequence::AA.new('MM'))
+      assert_equal(Bio::Sequence::AA.new('MM'), translate(str))
     end
 
     def test_seq_report_na
       str = 'ACGT'
       output = ''
-      assert_equal(seq_report(str), output)
+      assert_equal(output, seq_report(str))
     end
 
 
     def test_seq_report_aa
       str = 'WD'
       output = ''
-      assert_equal(seq_report(str), output)
+      assert_equal(output, seq_report(str))
     end
 
 
@@ -88,7 +88,7 @@ complemnet weight  : 1245.88148
 protein weight     : 119.12
 //
 END
-      assert_equal(na_report(naseq), output)
+      assert_equal(output, na_report(naseq))
     end
 
     def test_aa_report
@@ -101,7 +101,7 @@ amino acid codes  : [\"Trp\", \"Asp\"]
 amino acid names  : [\"tryptophan\", \"aspartic acid\"]
 //
 END
-      assert_equal(aa_report(aaseq), output)
+      assert_equal(output, aa_report(aaseq))
     end
 
     def test_double_helix
@@ -124,7 +124,7 @@ c---g
     c--g
      at
 END
-      assert_equal(double_helix(seq), output)
+      assert_equal(output, doublehelix(seq))
     end
 
   end
