fd78f89e6878e22b9d37dcd6383bf2bc9fdcaab1
Mitsuteru Nakao <n@bioruby.org>
2005-11-23 10:02:42 +0000

    * Fixed the order of arguments of assert_equal.

--- test/unit/bio/db/embl/test_embl.rb
+++ test/unit/bio/db/embl/test_embl.rb
@@ -17,7 +17,7 @@
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
-#  $Id: test_embl.rb,v 1.2 2005/10/27 09:38:12 nakao Exp $
+#  $Id: test_embl.rb,v 1.3 2005/11/23 10:02:42 nakao Exp $
 #
 
 require 'pathname'
@@ -46,66 +46,66 @@ module Bio
     end
 
     def test_id_line_entry_name
-      assert_equal(@obj.id_line('ENTRY_NAME'), 'AB090716')
+      assert_equal('AB090716', @obj.id_line('ENTRY_NAME'))
     end
 
     def test_id_line_data_class
-      assert_equal(@obj.id_line('DATA_CLASS'), 'standard')
+      assert_equal('standard', @obj.id_line('DATA_CLASS'))
     end
 
     def test_id_line_molecule_type
-      assert_equal(@obj.id_line('MOLECULE_TYPE'), 'genomic DNA')
+      assert_equal('genomic DNA', @obj.id_line('MOLECULE_TYPE'))
     end
 
     def test_id_line_division
-      assert_equal(@obj.id_line('DIVISION'), 'VRT')
+      assert_equal('VRT', @obj.id_line('DIVISION'))
     end
 
     def test_id_line_sequence_length
-      assert_equal(@obj.id_line('SEQUENCE_LENGTH'), 166)
+      assert_equal(166, @obj.id_line('SEQUENCE_LENGTH'))
     end
     
     def test_entry
       entry_id = 'AB090716'
-      assert_equal(@obj.entry, entry_id)
-      assert_equal(@obj.entry_name, entry_id)
-      assert_equal(@obj.entry_id, entry_id)
+      assert_equal(entry_id, @obj.entry)
+      assert_equal(entry_id, @obj.entry_name)
+      assert_equal(entry_id, @obj.entry_id)
     end
 
     def test_molecule
       molecule = 'genomic DNA'
-      assert_equal(@obj.molecule, molecule)
-      assert_equal(@obj.molecule_type, molecule)
+      assert_equal(molecule, @obj.molecule)
+      assert_equal(molecule, @obj.molecule_type)
     end
 
     def test_division
-      assert_equal(@obj.division, 'VRT')
+      assert_equal('VRT', @obj.division)
     end
     
     def test_sequence_length
       seqlen = 166
-      assert_equal(@obj.sequence_length, seqlen)
-      assert_equal(@obj.seqlen, seqlen)
+      assert_equal(seqlen, @obj.sequence_length)
+      assert_equal(seqlen, @obj.seqlen)
     end
 
     # Bio::EMBLDB::COMMON#ac
     def test_ac 
       ac = ['AB090716']
-      assert_equal(@obj.ac, ac)
-      assert_equal(@obj.accessions, ac)
+      assert_equal(ac, @obj.ac)
+      assert_equal(ac, @obj.accessions)
     end
 
     # Bio::EMBLDB::COMMON#accession
     def test_accession
-      assert_equal(@obj.accession, 'AB090716')
+      assert_equal('AB090716', @obj.accession)
     end
 
     def test_sv
-      assert_equal(@obj.sv, 'AB090716.1')
+      assert_equal('AB090716.1', @obj.sv)
     end
 
     def test_version
-      assert_equal(@obj.version, 1)
+      assert_equal(1, @obj.version)
     end
 
     def test_dt
@@ -117,92 +117,92 @@ module Bio
     end
 
     def test_dt_created
-      assert_equal(@obj.dt('created'), '25-OCT-2002 (Rel. 73, Created)')
+      assert_equal('25-OCT-2002 (Rel. 73, Created)', @obj.dt('created'))
     end
 
     def test_dt_updated
-      assert_equal(@obj.dt('updated'), '29-NOV-2002 (Rel. 73, Last updated, Version 2)')
+      assert_equal('29-NOV-2002 (Rel. 73, Last updated, Version 2)', @obj.dt('updated'))
     end
 
     # Bio::EMBLDB::COMMON#de
     def test_de
-      assert_equal(@obj.de, "Haplochromis sp. 'muzu, rukwa' LWS gene for long wavelength-sensitive opsin, partial cds, specimen_voucher:specimen No. HT-9361.")
+      assert_equal("Haplochromis sp. 'muzu, rukwa' LWS gene for long wavelength-sensitive opsin, partial cds, specimen_voucher:specimen No. HT-9361.", @obj.de)
     end
 
     # Bio::EMBLDB::COMMON#kw
     def test_kw 
       k = []
-      assert_equal(@obj.kw, [])
-      assert_equal(@obj.keywords, [])
+      assert_equal([], @obj.kw)
+      assert_equal([], @obj.keywords)
     end
 
     def test_os
-#      assert_equal(@obj.os, '')
+#      assert_equal('', @obj.os)
       assert_raises(RuntimeError) { @obj.os }
     end
 
     def test_os_valid
       @obj.instance_eval { @data['OS'] = "Haplochromis sp. 'muzu rukwa'" }
-      assert_equal(@obj.os, "Haplochromis sp. 'muzu rukwa'")
+      assert_equal("Haplochromis sp. 'muzu rukwa'", @obj.os)
     end
 
     # Bio::EMBLDB::COMMON#oc
     def test_oc 
-      assert_equal(@obj.oc.first, 'Eukaryota')
+      assert_equal('Eukaryota', @obj.oc.first)
     end
 
     # Bio::EMBLDB::COMMON#og
     def test_og 
-      assert_equal(@obj.og, [])
+      assert_equal([], @obj.og)
     end
 
     # Bio::EMBLDB::COMMON#ref
     def test_ref
-      assert_equal(@obj.ref.size, 2)
+      assert_equal(2, @obj.ref.size)
     end
 
     # Bio::EMBLDB::COMMON#references
     def test_references 
-      assert_equal(@obj.references.class, Bio::References)
+      assert_equal(Bio::References, @obj.references.class)
     end
 
     # Bio::EMBLDB::COMMON#dr
     def test_dr
-      assert_equal(@obj.dr, {})
+      assert_equal({}, @obj.dr)
     end
 
     def test_fh
-      assert_equal(@obj.fh, 'Key Location/Qualifiers')
+      assert_equal('Key Location/Qualifiers', @obj.fh)
     end
 
     def test_ft
-      assert_equal(@obj.ft.class, Bio::Features)
+      assert_equal(Bio::Features, @obj.ft.class)
     end
 
     def test_ft_iterator
       @obj.ft.each do |feature|
-        assert_equal(feature.class, Bio::Feature)
+        assert_equal(Bio::Feature, feature.class)
       end
     end
 
     def test_ft_accessor
-      assert_equal(@obj.ft.features[1].feature, 'CDS')
+      assert_equal('CDS', @obj.ft.features[1].feature)
     end
 
     def test_each_cds
       @obj.each_cds do |x|
-        assert_equal(x.feature, 'CDS')
+        assert_equal('CDS', x.feature)
       end
     end
 
     def test_each_gene
       @obj.each_gene do |x| 
-        assert_equal(x.feature, 'gene')
+        assert_equal('gene', x.feature)
       end
     end
 
     def test_cc
-      assert_equal(@obj.cc, '')
+      assert_equal('', @obj.cc)
     end
 
 #    def test_xx
@@ -210,18 +210,18 @@ module Bio
 
     def test_sq
       data = {"a"=>29, "c"=>42, "ntlen"=>166, "g"=>41, "t"=>54, "other"=>0}
-      assert_equal(@obj.sq, data)
+      assert_equal(data, @obj.sq)
     end
 
     def test_sq_get
-      assert_equal(@obj.sq("a"), 29)
+      assert_equal(29, @obj.sq("a"))
     end
 
     def test_seq
       seq = 'gttctggcctcatggactgaagacttcctgtggacctgatgtgttcagtggaagtgaagaccctggagtacagtcctacatgattgttctcatgattacttgctgtttcatccccctggctatcatcatcctgtgctaccttgctgtgtggatggccatccgtgct'
-      assert_equal(@obj.seq, seq)
-      assert_equal(@obj.naseq, seq)
-      assert_equal(@obj.ntseq, seq)
+      assert_equal(seq, @obj.seq)
+      assert_equal(seq, @obj.naseq)
+      assert_equal(seq, @obj.ntseq)
     end
   end
 end
--- test/unit/bio/db/embl/test_sptr.rb
+++ test/unit/bio/db/embl/test_sptr.rb
@@ -17,7 +17,7 @@
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
-#  $Id: test_sptr.rb,v 1.1 2005/10/27 09:28:43 nakao Exp $
+#  $Id: test_sptr.rb,v 1.2 2005/11/23 10:06:12 nakao Exp $
 #
 
 require 'pathname'
@@ -41,47 +41,47 @@ module Bio
     end
 
     def test_id_line_entry_name
-      assert_equal(@obj.id_line('ENTRY_NAME'), 'P53_HUMAN')
+      assert_equal('P53_HUMAN', @obj.id_line('ENTRY_NAME'))
     end   
 
     def test_id_line_data_class
-      assert_equal(@obj.id_line('DATA_CLASS'), 'STANDARD')
+      assert_equal('STANDARD', @obj.id_line('DATA_CLASS'))
     end
 
     def test_id_line_molecule_type
-      assert_equal(@obj.id_line('MOLECULE_TYPE'), 'PRT')
+      assert_equal('PRT', @obj.id_line('MOLECULE_TYPE'))
     end
 
     def test_id_line_sequence_length
-      assert_equal(@obj.id_line('SEQUENCE_LENGTH'), 393)
+      assert_equal(393, @obj.id_line('SEQUENCE_LENGTH'))
     end
 
     def test_entry
       entry = 'P53_HUMAN'
-      assert_equal(@obj.entry, entry)
-      assert_equal(@obj.entry_name, entry)
-      assert_equal(@obj.entry_id, entry)
+      assert_equal(entry, @obj.entry)
+      assert_equal(entry, @obj.entry_name)
+      assert_equal(entry, @obj.entry_id)
     end
 
     def test_molecule
-      assert_equal(@obj.molecule, 'PRT')
-      assert_equal(@obj.molecule_type, 'PRT')
+      assert_equal('PRT', @obj.molecule)
+      assert_equal('PRT', @obj.molecule_type)
     end
 
     def test_sequence_length
       seqlen = 393
-      assert_equal(@obj.sequence_length, seqlen)
-      assert_equal(@obj.aalen, seqlen)
+      assert_equal(seqlen, @obj.sequence_length)
+      assert_equal(seqlen, @obj.aalen)
     end
 
     def test_ac
       acs = ["P04637", "Q15086", "Q15087", "Q15088", "Q16535", "Q16807", "Q16808", "Q16809", "Q16810", "Q16811", "Q16848", "Q86UG1", "Q8J016", "Q99659", "Q9BTM4", "Q9HAQ8", "Q9NP68", "Q9NPJ2", "Q9NZD0", "Q9UBI2", "Q9UQ61"]
-      assert_equal(@obj.ac, acs)
-      assert_equal(@obj.accessions, acs)
+      assert_equal(acs, @obj.ac)
+      assert_equal(acs, @obj.accessions)
     end
 
     def test_accession
-      assert_equal(@obj.accession, 'P04637')
+      assert_equal('P04637', @obj.accession)
     end
 
     def test_dt
@@ -89,15 +89,15 @@ module Bio
     end
 
     def test_dt_created
-      assert_equal(@obj.dt('created'), '13-AUG-1987 (Rel. 05, Created)')
+      assert_equal('13-AUG-1987 (Rel. 05, Created)', @obj.dt('created'))
     end
 
     def test_dt_sequence
-      assert_equal(@obj.dt('sequence'), '01-MAR-1989 (Rel. 10, Last sequence update)')
+      assert_equal('01-MAR-1989 (Rel. 10, Last sequence update)', @obj.dt('sequence'))
     end
 
     def test_dt_annotation
-      assert_equal(@obj.dt('annotation'), '13-SEP-2005 (Rel. 48, Last annotation update)')
+      assert_equal('13-SEP-2005 (Rel. 48, Last annotation update)', @obj.dt('annotation'))
     end
 
     def test_de
@@ -105,33 +105,33 @@ module Bio
     end
 
     def test_protein_name
-      assert_equal(@obj.protein_name, "Cellular tumor antigen p53")
+      assert_equal("Cellular tumor antigen p53", @obj.protein_name)
     end
 
     def test_synonyms
-      assert_equal(@obj.synonyms, ["Tumor suppressor p53", "Phosphoprotein p53", "Antigen NY-CO-13"])
+      assert_equal(["Tumor suppressor p53", "Phosphoprotein p53", "Antigen NY-CO-13"], @obj.synonyms)
     end
 
     def test_gn
-      assert_equal(@obj.gn, [{:orfs=>[], :synonyms=>["P53"], :name=>"TP53", :loci=>[]}])
+      assert_equal([{:orfs=>[], :synonyms=>["P53"], :name=>"TP53", :loci=>[]}], @obj.gn)
     end
 
     def test_gn_uniprot_parser
       gn_uniprot_data = ''
-      assert_equal(@obj.instance_eval("gn_uniprot_parser"), [{:orfs=>[], :loci=>[], :name=>"TP53", :synonyms=>["P53"]}])
+      assert_equal([{:orfs=>[], :loci=>[], :name=>"TP53", :synonyms=>["P53"]}], @obj.instance_eval("gn_uniprot_parser"))
     end
 
     def test_gn_old_parser
       gn_old_data = ''
-      assert_equal(@obj.instance_eval("gn_old_parser"), [["Name=TP53; Synonyms=P53;"]])
+      assert_equal([["Name=TP53; Synonyms=P53;"]], @obj.instance_eval("gn_old_parser"))
     end
 
     def test_gene_names
-      assert_equal(@obj.gene_names, ["TP53"])
+      assert_equal(["TP53"], @obj.gene_names)
     end
 
     def test_gene_name
-      assert_equal(@obj.gene_name, 'TP53')
+      assert_equal('TP53', @obj.gene_name)
     end
 
     def test_os
@@ -139,61 +139,61 @@ module Bio
     end
 
     def test_os_access
-      assert_equal(@obj.os(0), "Homo sapiens (Human)")
+      assert_equal("Homo sapiens (Human)", @obj.os(0))
     end
 
     def test_os_access2
-      assert_equal(@obj.os[0], {"name"=>"(Human)", "os"=>"Homo sapiens"})
+      assert_equal({"name"=>"(Human)", "os"=>"Homo sapiens"}, @obj.os[0])
     end
 
     def test_og_1
       og = "OG   Plastid; Chloroplast."
       ary = ['Plastid', 'Chloroplast']
       @obj.instance_eval("@orig['OG'] = '#{og}'")
-      assert_equal(@obj.og, ary)
+      assert_equal(ary, @obj.og)
     end
 
     def test_og_2
       og = "OG   Mitochondrion."
       ary = ['Mitochondrion']
       @obj.instance_eval("@orig['OG'] = '#{og}'")
-      assert_equal(@obj.og, ary)
+      assert_equal(ary, @obj.og)
     end
 
     def test_og_3
       og = "OG   Plasmid sym pNGR234a."
       ary = ["Plasmid sym pNGR234a"]
       @obj.instance_eval("@orig['OG'] = '#{og}'")
-      assert_equal(@obj.og, ary)
+      assert_equal(ary, @obj.og)
     end
 
     def test_og_4
       og = "OG   Plastid; Cyanelle."
       ary = ['Plastid', 'Cyanelle']
       @obj.instance_eval("@orig['OG'] = '#{og}'")
-      assert_equal(@obj.og, ary)
+      assert_equal(ary, @obj.og)
     end
 
     def test_og_5
       og = "OG   Plasmid pSymA (megaplasmid 1)." 
       ary = ["Plasmid pSymA (megaplasmid 1)"]
       @obj.instance_eval("@orig['OG'] = '#{og}'")
-      assert_equal(@obj.og, ary)
+      assert_equal(ary, @obj.og)
     end
 
     def test_og_6
       og = "OG   Plasmid pNRC100, Plasmid pNRC200, and Plasmid pHH1." 
       ary = ['Plasmid pNRC100', 'Plasmid pNRC200', 'Plasmid pHH1']
       @obj.instance_eval("@orig['OG'] = '#{og}'")
-      assert_equal(@obj.og, ary)
+      assert_equal(ary, @obj.og)
     end
 
     def test_oc
-      assert_equal(@obj.oc, ["Eukaryota", "Metazoa", "Chordata", "Craniata", "Vertebrata", "Euteleostomi", "Mammalia", "Eutheria", "Euarchontoglires", "Primates", "Catarrhini", "Hominidae", "Homo"])
+      assert_equal(["Eukaryota", "Metazoa", "Chordata", "Craniata", "Vertebrata", "Euteleostomi", "Mammalia", "Eutheria", "Euarchontoglires", "Primates", "Catarrhini", "Hominidae", "Homo"], @obj.oc)
     end
 
     def test_ox
-      assert_equal(@obj.ox, {"NCBI_TaxID"=>["9606"]})
+      assert_equal({"NCBI_TaxID"=>["9606"]}, @obj.ox)
     end
 
     def test_ref # Bio::EMBL::COMMON#ref
@@ -201,7 +201,7 @@ module Bio
     end
 
     def test_cc
-      assert_equal(@obj.cc.class, Hash)
+      assert_equal(Hash, @obj.cc.class)
     end
    
     def test_cc_database
@@ -209,59 +209,59 @@ module Bio
             {"NAME"=>"Tokyo p53", "WWW"=>"http://p53.genome.ad.jp/", "FTP"=>nil, "NOTE"=>"University of Tokyo db of p53 mutations"},
             {"NAME"=>"p53 web site at the Institut Curie", "WWW"=>"http://p53.curie.fr/", "FTP"=>nil, "NOTE"=>nil},
             {"NAME"=>"Atlas Genet. Cytogenet. Oncol. Haematol.", "WWW"=>"http://www.infobiogen.fr/services/chromcancer/Genes/P53ID88.html", "FTP"=>nil, "NOTE"=>nil}]
-      assert_equal(@obj.cc('DATABASE'), db)
+      assert_equal(db, @obj.cc('DATABASE'))
     end
 
     def test_cc_alternative_products
       ap = {"Comment"=>nil, "Named isoforms"=>"2", "Variants"=>  [{"IsoId"=>"P04637-1", "Name"=>"1", "Sequence"=>["Displayed"]},
                                                                   {"IsoId"=>"P04637-2", "Name"=>"2", "Synonyms"=>"I9RET", "Sequence"=>["VSP_006535", "VSP_006536"]}],
             "Event"=>"Alternative splicing"}
-      assert_equal(@obj.cc('ALTERNATIVE PRODUCTS'), ap)
+      assert_equal(ap, @obj.cc('ALTERNATIVE PRODUCTS'))
     end
 
     def test_cc_mass_spectrometry
-      assert_equal(@obj.cc('MASS SPECTROMETRY'), nil)
+      assert_equal(nil, @obj.cc('MASS SPECTROMETRY'))
     end
 
 
 
     def test_kw
       keywords = ["3D-structure", "Acetylation", "Activator", "Alternative splicing", "Anti-oncogene", "Apoptosis", "Cell cycle", "Disease mutation", "DNA-binding", "Glycoprotein", "Li-Fraumeni syndrome", "Metal-binding", "Nuclear protein", "Phosphorylation", "Polymorphism", "Transcription", "Transcription regulation", "Zinc"]
-      assert_equal(@obj.kw, keywords)
+      assert_equal(keywords, @obj.kw)
     end
     
     def test_ft
       assert(@obj.ft)
       name = 'DNA_BIND'
-      assert_equal(@obj.ft(name), [{"FTId"=>nil, "From"=>102, "diff"=>[], "To"=>292, "Description"=>nil}])
+      assert_equal([{"FTId"=>nil, "From"=>102, "diff"=>[], "To"=>292, "Description"=>nil}], @obj.ft(name))
     end
 
     def test_sq
-      assert_equal(@obj.sq, {"CRC64"=>"AD5C149FD8106131", "aalen"=>393, "MW"=>43653})
+      assert_equal({"CRC64"=>"AD5C149FD8106131", "aalen"=>393, "MW"=>43653}, @obj.sq)
     end
 
     def test_sq_crc64
-      assert_equal(@obj.sq('CRC64'), "AD5C149FD8106131")
+      assert_equal("AD5C149FD8106131", @obj.sq('CRC64'))
     end
 
     def test_sq_mw
       mw = 43653
-      assert_equal(@obj.sq('mw'), mw)
-      assert_equal(@obj.sq('molecular'), mw)
-      assert_equal(@obj.sq('weight'), mw)
+      assert_equal(mw, @obj.sq('mw'))
+      assert_equal(mw, @obj.sq('molecular'))
+      assert_equal(mw, @obj.sq('weight'))
     end
 
     def test_sq_len
       length = 393
-      assert_equal(@obj.sq('len'), length)
-      assert_equal(@obj.sq('length'), length)
-      assert_equal(@obj.sq('AA'), length)
+      assert_equal(length, @obj.sq('len'))
+      assert_equal(length, @obj.sq('length'))
+      assert_equal(length, @obj.sq('AA'))
     end
 
     def test_seq
       seq = 'MEEPQSDPSVEPPLSQETFSDLWKLLPENNVLSPLPSQAMDDLMLSPDDIEQWFTEDPGPDEAPRMPEAAPPVAPAPAAPTPAAPAPAPSWPLSSSVPSQKTYQGSYGFRLGFLHSGTAKSVTCTYSPALNKMFCQLAKTCPVQLWVDSTPPPGTRVRAMAIYKQSQHMTEVVRRCPHHERCSDSDGLAPPQHLIRVEGNLRVEYLDDRNTFRHSVVVPYEPPEVGSDCTTIHYNYMCNSSCMGGMNRRPILTIITLEDSSGNLLGRNSFEVRVCACPGRDRRTEEENLRKKGEPHHELPPGSTKRALPNNTSSSPQPKKKPLDGEYFTLQIRGRERFEMFRELNEALELKDAQAGKEPGGSRAHSSHLKSKKGQSTSRHKKLMFKTEGPDSD'
-      assert_equal(@obj.seq, seq)
-      assert_equal(@obj.aaseq, seq)
+      assert_equal(seq, @obj.seq)
+      assert_equal(seq, @obj.aaseq)
     end
 
   end
