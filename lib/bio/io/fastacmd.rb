7fa55e356f950da93095d98b51853e9818af6436
Mitsuteru Nakao <n@bioruby.org>
2006-01-28 07:41:59 +0000

    * Added RDoc.

--- lib/bio/io/fastacmd.rb
+++ lib/bio/io/fastacmd.rb
@@ -1,8 +1,43 @@
 #
-# bio/io/fastacmd.rb - NCBI fastacmd wrapper class
+# = bio/io/fastacmd.rb - NCBI fastacmd wrapper class
 #
-#   Copyright (C) 2005 Shuji SHIGENOBU <shige@nibb.ac.jp>
-#   Copyright (C) 2005 Toshiaki Katayama <k@bioruby.org>
+# Copyright::  Copyright (C) 2005 
+#              Shuji SHIGENOBU <shige@nibb.ac.jp>,
+#              Toshiaki Katayama <k@bioruby.org>
+# Lisence::    LGPL
+#
+# $Id: fastacmd.rb,v 1.9 2006/01/28 07:41:59 nakao Exp $
+#
+# == Description
+#
+# Retrives FASTA formatted sequences from a blast database using 
+# NCBI fastacmd command.
+# 
+# == Examples
+#
+#    database = ARGV.shift || "/db/myblastdb"
+#    entry_id = ARGV.shift || "sp:128U_DROME"
+#    ent_list = ["sp:1433_SPIOL", "sp:1432_MAIZE"]
+#
+#    fastacmd = Bio::Blast::Fastacmd.new(database)
+#
+#    entry = fastacmd.get_by_id(entry_id)
+#    fastacmd.fetch(entry_id)
+#    fastacmd.fetch(ent_list)
+#
+#    fastacmd.fetch(ent_list).each do |fasta|
+#      puts fasta
+#    end
+#
+# == References
+#
+# * NCBI tool
+#   ftp://ftp.ncbi.nih.gov/blast/executables/LATEST/ncbi.tar.gz
+#
+# * fastacmd.html
+#   http://biowulf.nih.gov/apps/blast/doc/fastacmd.html
+#
+#--
 #
 #  This library is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU Lesser General Public
@@ -18,7 +53,7 @@
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
-#  $Id: fastacmd.rb,v 1.8 2005/09/26 13:00:08 k Exp $
+#++
 #
 
 require 'bio/db/fasta'
@@ -28,23 +63,52 @@ require 'bio/command'
 module Bio
 class Blast
 
+# NCBI fastacmd wrapper class
+#
 class Fastacmd
 
   include Enumerable
   include Bio::Command::Tools
 
-  def initialize(db)
-    @database = db
+  # Database file path.
+  attr_accessor :database
+
+  # fastcmd command file path.
+  attr_accessor :fastacmd
+
+  # 
+  attr_accessor :errorlog
+
+  # Initalize a fastacmd object.
+  #    
+  #    fastacmd = Bio::Blast::Fastacmd.new("/db/myblastdb")
+  def initialize(blast_database_file_path)
+    @database = blast_database_file_path
     @fastacmd = 'fastacmd'
   end
-  attr_accessor :database, :fastacmd, :errorlog
 
-  # get an entry_id and returns a Bio::FastaFormat object
+
+  # get an entry_id and returns a Bio::FastaFormat object.
+  #
+  #   entry_id = "sp:128U_DROME"
+  #   entry = fastacmd.get_by_id(entry_id)
   def get_by_id(entry_id)
     fetch(entry_id).shift
   end
 
-  # get one or more entry_id and returns an Array of Bio::FastaFormat objects
+  # get one or more entry_id and returns an Array of Bio::FastaFormat objects.
+  #
+  # Fastacmd#fetch(entry_id) returns an Array of a Bio::FastaFormat
+  # object even when the result is a single entry.
+  #
+  #    p fastacmd.fetch(entry_id)
+  #
+  # Fastacmd#fetch method also accepts a list of entry_id and returns
+  # an Array of Bio::FastaFormat objects.
+  #    
+  #    ent_list = ["sp:1433_SPIOL", "sp:1432_MAIZE"]
+  #    p fastacmd.fetch(ent_list)
+  #
   def fetch(list)
     if list.respond_to?(:join)
       entry_id = list.join(",")
@@ -59,13 +123,20 @@ class Fastacmd
     end
   end
 
+  # Iterates each entry.
+  #
+  # You can also iterate on all sequences in the database!
+  #    fastacmd.each do |fasta|
+  #      p [ fasta.definition[0..30], fasta.seq.size ]
+  #    end
+  #
   def each_entry
     cmd = [ @fastacmd, '-d', @database, '-D', 'T' ]
     call_command_local(cmd) do |inn, out|
       inn.close_write
       Bio::FlatFile.open(Bio::FastaFormat, out) do |f|
-        f.each_entry do |e|
-          yield e
+        f.each_entry do |entry|
+          yield entry
         end
       end
     end
@@ -73,51 +144,9 @@ class Fastacmd
   end
   alias each each_entry
 
-end
+end # class Fastacmd
 
-end
-end
+end # class Blast
+end # module Bio
 
-
-if __FILE__ == $0
-
-  database = ARGV.shift || "/db/myblastdb"
-  entry_id = ARGV.shift || "sp:128U_DROME"
-  ent_list = ["sp:1433_SPIOL", "sp:1432_MAIZE"]
-
-  fastacmd = Bio::Blast::Fastacmd.new(database)
-
-  ### Retrieve one sequence
-  entry = fastacmd.get_by_id(entry_id)
-
-  # Fastacmd#get_by_id(entry_id) returns a Bio::FastaFormat object.
-  p entry
-
-  # Bio::FastaFormat becomes a fasta format string when printed by puts.
-  puts entry
-
-  # Fastacmd#fetch(entry_id) returns an Array of a Bio::FastaFormat
-  # object even when the result is a single entry.
-  p fastacmd.fetch(entry_id)
-
-  ### Retrieve more sequences
-
-  # Fastacmd#fetch method also accepts a list of entry_id and returns
-  # an Array of Bio::FastaFormat objects.
-  p fastacmd.fetch(ent_list)
-
-  # So, you can iterate on the results.
-  fastacmd.fetch(ent_list).each do |fasta|
-    puts fasta
-  end
-
-
-  ### Iterates on all entries
-
-  # You can also iterate on all sequences in the database!
-  fastacmd.each do |fasta|
-    p [ fasta.definition[0..30], fasta.seq.size ]
-  end
-
-end
 
