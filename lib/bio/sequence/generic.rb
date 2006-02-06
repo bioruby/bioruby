3078b31f725080007a11a894e29a5a1b02d0b11e
Toshiaki Katayama <k@bioruby.org>
2006-02-06 14:26:04 +0000

    * fixed to inherit String

--- lib/bio/sequence/generic.rb
+++ lib/bio/sequence/generic.rb
@@ -5,18 +5,20 @@
 #               Toshiaki Katayama <k@bioruby.org>
 # License::     Ruby's
 #
-# $Id: generic.rb,v 1.2 2006/02/06 14:22:04 k Exp $
+# $Id: generic.rb,v 1.3 2006/02/06 14:26:04 k Exp $
 #
 
 require 'bio/sequence/common'
 
 module Bio
 class Sequence
-class Generic
+
+class Generic < String
 
   include Bio::Sequence::Common
 
 end # Generic
+
 end # Sequence
 end # Bio
 
