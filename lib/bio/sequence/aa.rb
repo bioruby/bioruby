72d8cb7b2bf49fbb32eac1f431ec39b2cf94370a
Toshiaki Katayama <k@bioruby.org>
2006-02-06 14:11:31 +0000

    * added RDoc header with Ruby's license * changed to use autoload * use Bio::AminoAcid explicitly instead of AminoAcid

--- lib/bio/sequence/aa.rb
+++ lib/bio/sequence/aa.rb
@@ -1,9 +1,23 @@
+#
+# = bio/sequence/aa.rb - amino acid sequence class
+#
+# Copyright::   Copyright (C) 2006
+#               Toshiaki Katayama <k@bioruby.org>
+# License::     Ruby's
+#
+# $Id: aa.rb,v 1.2 2006/02/06 14:11:31 k Exp $
+#
+
+require 'bio/sequence/common'
+
 module Bio
+
+  autoload :AminoAcid, 'bio/data/aa'
 
 class Sequence
 
-# Amino Acid sequence
 
+# Amino Acid sequence
 class AA < String
 
   include Bio::Sequence::Common
@@ -15,14 +29,14 @@ class AA < String
     self.tr!(" \t\n\r",'')
   end
 
+
   # Estimate the weight of this protein.
-  # AminoAcid is defined in bio/data/aa.rb
   def molecular_weight
-    AminoAcid.weight(self)
+    Bio::AminoAcid.weight(self)
   end
 
   def to_re
-    AminoAcid.to_re(self)
+    Bio::AminoAcid.to_re(self)
   end
 
   # Generate the list of the names of the each residue along with the
@@ -30,7 +44,7 @@ class AA < String
   def codes
     array = []
     self.each_byte do |x|
-      array.push(AminoAcid.names[x.chr])
+      array.push(Bio::AminoAcid.names[x.chr])
     end
     return array
   end
@@ -38,7 +52,7 @@ class AA < String
   # Similar to codes but returns long names.
   def names
     self.codes.map do |x|
-      AminoAcid.names[x]
+      Bio::AminoAcid.names[x]
     end
   end
 
@@ -47,3 +61,4 @@ end # AA
 end # Sequence
 
 end # Bio
+
