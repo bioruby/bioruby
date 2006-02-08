d6010dd43de62d767555c74a132cf772467f2c46
Toshiaki Katayama <k@bioruby.org>
2006-02-08 12:15:42 +0000

    * to_re method is fixed to convert input sequence to downcase and not to   change original sequence itself.

--- lib/bio/data/na.rb
+++ lib/bio/data/na.rb
@@ -3,9 +3,9 @@
 #
 # Copyright::	Copyright (C) 2001, 2005
 #		Toshiaki Katayama <k@bioruby.org>
-# License::	LGPL
+# License::	Ruby's
 #
-# $Id: na.rb,v 0.19 2005/12/10 18:14:22 k Exp $
+# $Id: na.rb,v 0.20 2006/02/08 12:15:42 k Exp $
 #
 # == Synopsis
 #
@@ -37,24 +37,6 @@
 #
 #   puts "# Bio::NucleicAcid.weight('atgc')"
 #   p Bio::NucleicAcid.weight('atgc')
-#
-#--
-#
-#  This library is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU Lesser General Public
-#  License as published by the Free Software Foundation; either
-#  version 2 of the License, or (at your option) any later version.
-#
-#  This library is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#  Lesser General Public License for more details.
-#
-#  You should have received a copy of the GNU Lesser General Public
-#  License along with this library; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
-#
-#++
 #
 
 module Bio
@@ -176,7 +158,7 @@ class NucleicAcid
     end
 
     def to_re(seq, rna = false)
-      str = seq.to_s
+      str = seq.to_s.downcase
       str.gsub!(/[^atgcu]/) { |base|
         NAMES[base] || '.'
       }
