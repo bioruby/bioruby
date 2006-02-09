28fbffdd2c1c7baf47e5b4c7b9bea487d464f6fb
Toshiaki Katayama <k@bioruby.org>
2006-02-09 16:21:47 +0000

    * license changed, prepared for 0.8.0

--- lib/bio.rb
+++ lib/bio.rb
@@ -3,32 +3,14 @@
 #
 # Copyright::	Copyright (C) 2001-2006
 #		Toshiaki Katayama <k@bioruby.org>
-# License::	LGPL
+# License::	Ruby's
 #
-# $Id: bio.rb,v 1.63 2006/02/06 14:07:11 k Exp $
-#
-#--
-#
-#  This library is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU Lesser General Public
-#  License as published by the Free Software Foundation; either
-#  version 2 of the License, or (at your option) any later version.
-#
-#  This library is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#  Lesser General Public License for more details.
-#
-#  You should have received a copy of the GNU Lesser General Public
-#  License along with this library; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
-#
-#++
+# $Id: bio.rb,v 1.64 2006/02/09 16:21:47 k Exp $
 #
 
 module Bio
 
-  BIORUBY_VERSION = [0, 7, 2].extend(Comparable)
+  BIORUBY_VERSION = [0, 8, 0].extend(Comparable)
 
   ### Basic data types
 
