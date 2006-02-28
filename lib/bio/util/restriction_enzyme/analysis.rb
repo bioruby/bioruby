0b57652e5143533e04222a627bc7b82e37a030a3
Trevor Wennblom <trevor@corevx.com>
2006-02-28 22:21:48 +0000

    Error handling for when a match isn't found.

--- lib/bio/util/restriction_enzyme/analysis.rb
+++ lib/bio/util/restriction_enzyme/analysis.rb
@@ -29,7 +29,7 @@ class Bio::RestrictionEnzyme
 # Copyright::  Copyright (C) 2006 Trevor Wennblom <trevor@corevx.com>
 # License::    LGPL
 #
-#  $Id: analysis.rb,v 1.3 2006/02/18 22:11:35 trevor Exp $
+#  $Id: analysis.rb,v 1.4 2006/02/28 22:21:48 trevor Exp $
 #
 #
 #--
@@ -92,6 +92,7 @@ class Analysis
     return nil if !sequence.kind_of?(String) or sequence.empty?
     sequence = Bio::Sequence::NA.new( sequence )
     enzyme_actions = create_enzyme_actions( sequence, *args )
+    return nil if enzyme_actions.empty?
     permutations = permute(enzyme_actions.size)
 
     # Indexed by permutation.
@@ -200,6 +201,8 @@ puts "EA.right: #{enzyme_action.right}"
 
   def unique_fragments_for_display( hash_of_sequence_ranges_with_cuts )
     uf_ary = UniqueFragments.new
+    return uf_ary if hash_of_sequence_ranges_with_cuts == nil or hash_of_sequence_ranges_with_cuts.empty?
+
     hash_of_sequence_ranges_with_cuts.each do |permutation, sr_with_cuts|
       sr_with_cuts.fragments.for_display.each do |fragment|
         uf = UniqueFragment.new
