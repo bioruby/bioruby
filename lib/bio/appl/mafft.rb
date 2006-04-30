af669123eec49d6e72947cbc2c46d4acb6b3ce4d
Naohisa Goto <ng@bioruby.org>
2006-04-30 05:50:19 +0000

    changed license to Ruby's

--- lib/bio/appl/mafft.rb
+++ lib/bio/appl/mafft.rb
@@ -2,25 +2,9 @@
 # = bio/appl/mafft.rb - MAFFT wrapper class
 #
 # Copyright:: Copyright (C) 2003 GOTO Naohisa <ngoto@gen-info.osaka-u.ac.jp>
-# License::   LGPL
+# License::   Ruby's
 #
-#--
-#  This library is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU Lesser General Public
-#  License as published by the Free Software Foundation; either
-#  version 2 of the License, or (at your option) any later version.
-#
-#  This library is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#  Lesser General Public License for more details.
-#
-#  You should have received a copy of the GNU Lesser General Public
-#  License along with this library; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
-#++
-#
-#  $Id: mafft.rb,v 1.9 2005/12/18 15:58:40 k Exp $
+#  $Id: mafft.rb,v 1.10 2006/04/30 05:50:19 ngoto Exp $
 #
 # Bio::MAFFT is a wrapper class to execute MAFFT.
 # MAFFT is a very fast multiple sequence alignment software.
--- lib/bio/appl/sim4/report.rb
+++ lib/bio/appl/sim4/report.rb
@@ -2,25 +2,9 @@
 # = bio/appl/sim4/report.rb - sim4 result parser
 #
 # Copyright:: Copyright (C) 2004 GOTO Naohisa <ng@bioruby.org>
-# License::   LGPL
+# License::   Ruby's
 #
-#--
-#  This library is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU Lesser General Public
-#  License as published by the Free Software Foundation; either
-#  version 2 of the License, or (at your option) any later version.
-#
-#  This library is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#  Lesser General Public License for more details.
-#
-#  You should have received a copy of the GNU Lesser General Public
-#  License along with this library; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
-#++
-#
-#  $Id: report.rb,v 1.7 2005/12/18 15:58:40 k Exp $
+#  $Id: report.rb,v 1.8 2006/04/30 05:50:19 ngoto Exp $
 #
 # The sim4 report parser classes.
 #
