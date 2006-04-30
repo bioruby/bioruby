b7b0088a61893f87dd2e82b00abf47a4a28efd32
Naohisa Goto <ng@bioruby.org>
2006-04-30 05:40:23 +0000

    changes license to Ruby's.

--- lib/bio/appl/bl2seq/report.rb
+++ lib/bio/appl/bl2seq/report.rb
@@ -2,25 +2,9 @@
 # = bio/appl/bl2seq/report.rb - bl2seq (BLAST 2 sequences) parser
 # 
 # Copyright:: Copyright (C) 2005 GOTO Naohisa <ng@bioruby.org>
-# License::   LGPL
+# License::   Ruby's
 #
-#--
-#  This library is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU Lesser General Public
-#  License as published by the Free Software Foundation; either
-#  version 2 of the License, or (at your option) any later version.
-#
-#  This library is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#  Lesser General Public License for more details.
-#
-#  You should have received a copy of the GNU Lesser General Public
-#  License along with this library; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
-#++
-#
-#  $Id: report.rb,v 1.6 2005/12/18 15:58:39 k Exp $
+#  $Id: report.rb,v 1.7 2006/04/30 05:40:23 ngoto Exp $
 #
 # Bio::Bl2seq::Report is a NCBI bl2seq (BLAST 2 sequences) output parser.
 #
