4d06cb5e777a5af49a35427086a996b56c39931b
Toshiaki Katayama <k@bioruby.org>
2006-05-08 14:31:58 +0000

    * license is changed to Ruby's

--- lib/bio/db/gff.rb
+++ lib/bio/db/gff.rb
@@ -4,27 +4,9 @@
 # Copyright::  Copyright (C) 2003, 2005
 #              Toshiaki Katayama <k@bioruby.org>
 #              2006  Jan Aerts <jan.aerts@bbsrc.ac.uk>
-# License::    LGPL
+# License::    Ruby's
 #
-# $Id: gff.rb,v 1.6 2006/03/28 13:42:32 aerts Exp $
-#
-#--
-#
-#  This library is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU Lesser General Public
-#  License as published by the Free Software Foundation; either
-#  version 2 of the License, or (at your option) any later version.
-#
-#  This library is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#  Lesser General Public License for more details.
-#
-#  You should have received a copy of the GNU Lesser General Public
-#  License along with this library; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
-#
-#++
+# $Id: gff.rb,v 1.7 2006/05/08 14:31:58 k Exp $
 #
 
 module Bio
