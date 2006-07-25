67822a9e32ab080efe2a9a249d770312516bceab
Toshiaki Katayama <k@bioruby.org>
2006-07-25 19:17:39 +0000

    * please note that the method names of some accessors are changed! * bug fixed * @dom is obsoleted
    Note:
    # === Incompatible attribute names with KGML tags
    #
    # <entry>
    #  :id -> :entry_id
    #  :type -> :entry_type
    #  names()
    #  <graphics>
    #  :name -> :label
    #  :type -> :shape
    # <relation>
    #  :entry1 -> :node1
    #  :entry2 -> :node2
    #  :type -> :rel
    #  <subtype>
    #  edge()
    # <reaction>
    #  :name -> :entry_id
    #  :type -> :direction

--- lib/bio/db/kegg/kgml.rb
+++ lib/bio/db/kegg/kgml.rb
@@ -5,15 +5,34 @@
 # 		Toshiaki Katayama <k@bioruby.org>
 # License::	Ruby's
 #
-# $Id: kgml.rb,v 1.3 2006/05/08 14:25:25 k Exp $
+# $Id: kgml.rb,v 1.4 2006/07/25 19:17:39 k Exp $
 #
 # == KGML (KEGG XML) parser
 #
 # See http://www.genome.jp/kegg/xml/ for more details on KGML.
 #
+# === Incompatible attribute names with KGML tags
+#
+# <entry>
+#  :id -> :entry_id
+#  :type -> :entry_type
+#  names()
+#  <graphics>
+#  :name -> :label
+#  :type -> :shape
+# <relation>
+#  :entry1 -> :node1
+#  :entry2 -> :node2
+#  :type -> :rel
+#  <subtype>
+#  edge()
+# <reaction>
+#  :name -> :entry_id
+#  :type -> :direction
+#
 # === Examples
 #
-#  file = ARGF.read
+#  file = ARGF.read("kgml/hsa/hsa00010.xml")
 #  kgml = Bio::KEGG::KGML.new(file)
 #
 #  # <pathway> attributes
@@ -26,64 +45,63 @@
 #
 #  kgml.entries.each do |entry|
 #    # <entry> attributes
-#    puts entry.id
+#    puts entry.entry_id
 #    puts entry.name
-#    puts entry.names
-#    puts entry.type
+#    puts entry.entry_type
 #    puts entry.link
 #    puts entry.reaction
 #    puts entry.map
 #    # <graphics> attributes
-#    puts entry.label	# This is an accessor for <graphics name="">
+#    puts entry.label	      # name
+#    puts entry.shape         # type
 #    puts entry.x
 #    puts entry.y
-#    puts entry.type
 #    puts entry.width
 #    puts entry.height
 #    puts entry.fgcolor
 #    puts entry.bgcolor
+#    # methood
+#    puts entry.names
 #  end
 #
 #  kgml.relations.each do |relation|
 #    # <relation> attributes
-#    puts relation.entry1
-#    puts relation.entry2
-#    puts relation.type
+#    puts relation.node1      # entry1
+#    puts relation.node2      # entry2
+#    puts relation.rel        # type
+#    # method
+#    puts relation.edge
 #    # <subtype> attributes
 #    puts relation.name
 #    puts relation.value
-#    # or
-#    relation.subtype.each do |value, name|
-#      puts value, name
-#    end
 #  end
 #
 #  kgml.reactions.each do |reaction|
 #    # <reaction> attributes
-#    puts reaction.name
-#    puts reaction.type
+#    puts reaction.entry_id   # name
+#    puts reaction.direction  # type
 #    # <substrate> attributes
-#    reaction.substrates.each do |name|
-#      puts name
+#    reaction.substrates.each do |entry_id|
+#      puts entry_id
 #      # <alt> attributes
-#      altnames = reaction.alt[name]
-#      altnames.each do |altname|
-#        puts altname
+#      altnames = reaction.alt[entry_id]
+#      altnames.each do |name|
+#        puts name
 #      end
 #    end
 #    # <product> attributes
-#    reaction.products.each do |name|
-#      puts name
+#    reaction.products.each do |entry_id|
+#      puts entry_id
 #      # <alt> attributes
-#      altnames = reaction.alt[name]
-#      altnames.each do |altname|
-#        puts altname
+#      altnames = reaction.alt[entry_id]
+#      altnames.each do |name|
+#        puts name
 #      end
 #    end
 #  end
 #
 
-require 'rexml/document'
+autoload :REXML, 'rexml/document'
 
 module Bio
 class KEGG
@@ -91,34 +109,41 @@ class KEGG
 class KGML
 
   def initialize(xml)
-    @dom = REXML::Document.new(xml)
-    parse_root
-    parse_entry
-    parse_relation
-    parse_reaction
+    dom = REXML::Document.new(xml)
+    parse_root(dom)
+    parse_entry(dom)
+    parse_relation(dom)
+    parse_reaction(dom)
   end
   attr_reader :name, :org, :number, :title, :image, :link
-  attr_reader :entries, :relations, :reactions		# Array
+  attr_accessor :entries, :relations, :reactions
 
   class Entry
-    attr_accessor :id, :name, :names, :type, :link, :reaction, :map
-    attr_accessor :label, :x, :y, :type, :width, :height, :fgcolor, :bgcolor
+    attr_accessor :entry_id, :name, :entry_type, :link, :reaction, :map
+    attr_accessor :label, :shape, :x, :y, :width, :height, :fgcolor, :bgcolor
+    def names
+      @name.split(/\s+/)
+    end
   end
 
   class Relation
-    attr_accessor :entry1, :entry2, :type
+    attr_accessor :node1, :node2, :rel
     attr_accessor :name, :value
-    attr_accessor :subtype      			# Hash
+    def edge
+      @value.to_i
+    end
   end
 
   class Reaction
-    attr_accessor :name, :type
-    attr_accessor :substrates, :products		# Array
-    attr_accessor :alt					# Hash
+    attr_accessor :entry_id, :direction
+    attr_accessor :substrates, :products	# Array
+    attr_accessor :alt				# Hash
   end
 
-  def parse_root
-    root    = @dom.root.attributes
+  private
+
+  def parse_root(dom)
+    root    = dom.root.attributes
     @name   = root["name"]
     @org    = root["org"]
     @number = root["number"]
@@ -127,69 +152,66 @@ class KGML
     @link   = root["link"]
   end
 
-  def parse_entry
+  def parse_entry(dom)
     @entries = Array.new
 
-    @dom.elements.each("/pathway/entry") { |node|
+    dom.elements.each("/pathway/entry") { |node|
       attr = node.attributes
       entry = Entry.new
-      entry.id        = attr["id"].to_i
-      entry.map       = attr["map"]
-      entry.name      = attr["name"]
-      entry.names     = entry.name.split(/\s+/)
-      entry.type      = attr["type"]
-      entry.link      = attr["link"]
-      entry.reaction  = attr["reaction"]
+      entry.entry_id   = attr["id"].to_i
+      entry.name       = attr["name"]
+      entry.entry_type = attr["type"]
+      # implied
+      entry.link       = attr["link"]
+      entry.reaction   = attr["reaction"]
+      entry.map        = attr["map"]
 
       node.elements.each("graphics") { |graphics|
         attr = graphics.attributes
-        entry.x       = attr["x"].to_i
-        entry.y       = attr["y"].to_i
-        entry.type    = attr["type"]
-        entry.label   = attr["label"]	# name
-        entry.width   = attr["width"].to_i
-        entry.height  = attr["height"].to_i
-        entry.fgcolor = attr["fgcolor"]
-        entry.bgcolor = attr["bgcolor"]
+        entry.x        = attr["x"].to_i
+        entry.y        = attr["y"].to_i
+        entry.shape    = attr["type"]
+        entry.label    = attr["name"]
+        entry.width    = attr["width"].to_i
+        entry.height   = attr["height"].to_i
+        entry.fgcolor  = attr["fgcolor"]
+        entry.bgcolor  = attr["bgcolor"]
       }
       @entries << entry
     }
   end
 
-  def parse_relation
+  def parse_relation(dom)
     @relations = Array.new
 
-    @dom.elements.each("/pathway/relation") { |node|
+    dom.elements.each("/pathway/relation") { |node|
       attr = node.attributes
       relation = Relation.new
-      relation.entry1 = attr["entry1"].to_i
-      relation.entry2 = attr["entry2"].to_i
-      relation.type   = attr["type"]
+      relation.node1   = attr["entry1"].to_i
+      relation.node2   = attr["entry2"].to_i
+      relation.rel     = attr["type"]
 
-      hash = Hash.new
       node.elements.each("subtype") { |subtype|
         attr = subtype.attributes
-        relation.name  = name  = attr["name"]
-        relation.value = value = attr["value"].to_i
-        hash[value] = name
+        relation.name  = attr["name"]
+        relation.value = attr["value"]
       }
-      relation.subtype = hash
       @relations << relation
     }
   end
 
-  def parse_reaction
+  def parse_reaction(dom)
     @reactions = Array.new
 
-    @dom.elements.each("/pathway/reaction") { |node|
+    dom.elements.each("/pathway/reaction") { |node|
       attr = node.attributes
       reaction = Reaction.new
-      reaction.name = attr["name"]
-      reaction.type = attr["type"]
+      reaction.entry_id  = attr["name"]
+      reaction.direction = attr["type"]
 
       substrates = Array.new
       products   = Array.new
-      hash       = Hash.new
+      hash        = Hash.new
 
       node.elements.each("substrate") { |substrate|
         name = substrate.attributes["name"]
@@ -207,75 +229,17 @@ class KGML
           hash[name] << alt.attributes["name"]
         }
       }
-
       reaction.substrates = substrates
-      reaction.products   = products
-      reaction.alt        = hash
+      reaction.products = products
+      reaction.alt = hash
 
       @reactions << reaction
     }
   end
 
 end # KGML
+
 end # KEGG
 end # Bio
 
 
-if __FILE__ == $0
-  require 'pp'
-  xml = ARGF.read
-  pp Bio::KEGG::KGML.new(xml)
-end
-
-
-=begin
-
-# This is a test implementation which reflects original KGML data structure.
-
-class KGML
-
-  class Pathway
-    attr_accessor :name, :org, :number, :title, :image, :link
-    attr_accessor :entries, :relations, :reactions
-
-    class Entry
-      attr_accessor :id, :name, :type, :link, :reaction, :map
-      attr_accessor :components, :graphics
-
-      class Component
-        attr_accessor :id
-      end
-
-      class Graphics
-        attr_accessor :name, :x, :y, :type, :width, :height, :fgcolor, :bgcolor
-      end
-    end
-
-    class Relation
-      attr_accessor :entry1, :entry2, :type
-      attr_accessor :
-
-      class Subtype
-        attr_accessor :name, :value
-      end
-    end
-
-    class Reaction
-      attr_accessor :name, :type
-
-      class Substrate
-        attr_accessor :name
-      end
-
-      class Product
-        attr_accessor :name
-      end
-
-      class Alt
-        attr_accessor :name
-      end
-    end
-  end
-
-end
-=end
