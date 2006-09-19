495f67be37a4a831c135e957493a080084102d97
Toshiaki Katayama <k@bioruby.org>
2006-09-19 05:46:22 +0000

    * changed from LGPL to Ruby's

--- lib/bio/io/higet.rb
+++ lib/bio/io/higet.rb
@@ -1,23 +1,9 @@
 #
-# bio/io/higet.rb - SOAP interface for HGC HiGet
+# = bio/io/higet.rb - SOAP interface for HGC HiGet
 #
-#   Copyright (C) 2005 KATAYAMA Toshiaki <k@bioruby.org>
+# Copyright::  Copyright (C) 2005 Toshiaki Katayama <k@bioruby.org>
 #
-#  This library is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU Lesser General Public
-#  License as published by the Free Software Foundation; either
-#  version 2 of the License, or (at your option) any later version.
-#
-#  This library is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#  Lesser General Public License for more details.
-#
-#  You should have received a copy of the GNU Lesser General Public
-#  License along with this library; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
-#
-#  $Id: higet.rb,v 1.2 2005/09/26 13:00:08 k Exp $
+# $Id: higet.rb,v 1.3 2006/09/19 05:46:22 k Exp $
 #
 
 require 'bio/io/soapwsdl'
@@ -25,6 +11,15 @@ require 'bio/io/soapwsdl'
 module Bio
 class HGC
 
+# == Description
+#
+# Interface for the HiGet service provided by Human Genome Center (HGC), Japan.
+# HiGet performs full-text search against various biological databases.
+#
+# == References
+#
+# * http://higet.hgc.jp/
+#
 class HiGet < Bio::SOAPWSDL
 
   SERVER_URI = "http://higet.hgc.jp/soap/higet.wsdl"
@@ -75,18 +70,4 @@ if __FILE__ == $0
   puts serv.higet_in_xml("genbank", "S40289")
 
 end
-
-
-=begin
-
-= Bio::HGC::HiGet
-
-* ((<URL:http://higet.hgc.jp/>))
-
-== HiGet#hifind
-== HiGet#higet
-== HiGet#higet_in_fasta
-== HiGet#higet_in_xml
-
-=end
 
