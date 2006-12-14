624dfc93a3efdfd2ec4b9133a5f8ee39a1bd7167
Mitsuteru Nakao <n@bioruby.org>
2006-12-14 16:22:12 +0000

    * Newly added files for InterProScan.

---
+++ lib/bio/appl/iprscan/report.rb
@@ -0,0 +1,258 @@
+#
+# = bio/appl/iprscan/report.rb - a class for iprscan output.
+#
+# Copyright::   Copyright (C) 2006
+#               Mitsuteru C. Nakao <mn@kazusa.or.jp>
+# License::     Ruby's
+#
+#  $Id: report.rb,v 1.1 2006/12/14 16:22:12 nakao Exp $
+#
+# == Report classes for the iprscan program.
+# 
+
+
+module Bio
+
+  class Iprscan
+
+    # = DESCRIPTION
+    # Class for InterProScan report. It is used to parse results and reformat 
+    # results from (raw|xml|txt) into (html, xml, ebihtml, txt, gff3) format.
+    #
+    # See ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/README.html
+    # 
+    # == USAGE
+    #  # Read a marged.txt and split each entry.
+    #  Bio::Iprscan::Report.reports_in_txt(File.read("marged.txt") do |report| 
+    #    report.query_id
+    #    report.matches.size
+    #    report.matches.each do |match|
+    #      match.ipr_id #=> 'IPR...'
+    #      match.ipr_description
+    #      match.method
+    #      match.accession
+    #      match.description
+    #      match.match_start
+    #      match.match_end
+    #      match.evalue    
+    #    end
+    #    # report.to_gff3 
+    #    # report.to_html
+    #  end
+    #
+    #  Bio::Iprscan::Report.reports_in_raw(File.read("marged.raw") do |report| 
+    #    report.class #=> Bio::Iprscan::Report
+    #  end
+    #
+    class Report
+      # Entry delimiter pattern.
+      RS = DELIMITER = "\n\/\/\n"
+
+      # Qeury sequence name (entry_id).
+      attr_accessor :query_id
+      alias :entry_id :query_id
+
+      # Qeury sequence length.
+      attr_accessor :query_length
+
+      # Matched InterPro motifs in Hash. Each InterPro motif have :name, 
+      # :definition, :accession and :motifs keys. And :motifs key contains
+      # motifs in Array. Each motif have :method, :accession, :definition, 
+      # :score, :location_from and :location_to keys.
+      attr_accessor :matches
+      
+      # == USAGE
+      #  Bio::Iprscan::Report.reports_in_raw(File.open("merged.raw")) do |report|
+      #    report
+      #  end
+      #
+      def self.reports_in_raw(io)
+        entry = ''
+        while line = io.gets
+          if entry != '' and entry.split("\t").first == line.split("\t").first
+            entry << line
+          elsif entry != ''
+            yield Bio::Iprscan::Report.parse_in_raw(entry)
+            entry = line
+          else
+            entry << line
+          end
+        end
+      end
+    
+      # Parser method for a raw formated entry. Retruns a Bio::Iprscan::Report 
+      # object.
+      def self.parse_in_raw(str)
+        report = self.new
+        str.split(/\n/).each do |line|
+          line = line.split("\t")
+          report.matches << Match.new(:query_id => line[0],
+                                      :crc64    => line[1],
+                                      :query_length => line[2].to_i,
+                                      :method       => line[3], 
+                                      :accession    => line[4],
+                                      :description => line[5], 
+                                      :match_start => line[6].to_i,
+                                      :match_end   => line[7].to_i,
+                                      :evalue => line[8],
+                                      :status => line[9],
+                                      :date   => line[10])
+          if line[11]
+            report.matches.last.ipr_id = line[11]
+            report.matches.last.ipr_description = line[12]
+          end
+          report.matches.last.go_terms = line[13].split(', ') if line[13]          
+        end
+        report.query_id = report.matches.first.query_id
+        report.query_length = report.matches.first.query_length
+        report
+      end
+
+
+
+      # Parser method for a xml formated entry. Retruns a Bio::Iprscan::Report 
+      # object.
+      def self.parse_in_xml(str)
+        NotImplementedError
+      end
+
+      # Splits entry stream.
+      # 
+      # == Usage
+      #  Bio::Iprscan::Report.reports_in_txt(File.open("merged.txt")) do |report|
+      #    report
+      #  end
+      def self.reports_in_txt(io)
+        io.each(/\n\/\/\n/m) do |entry|
+          yield self.parse_in_txt(entry)
+        end
+      end
+
+      # Parser method for a txt formated entry. Retruns a Bio::Iprscan::Report 
+      # object.
+      # 
+      # == Usage
+      #
+      #  File.read("marged.txt").each(Bio::Iprscan::Report::RS) do |e| 
+      #    report = Bio::Iprscan::Report.parse_in_txt(e)
+      #  end
+      #
+      def self.parse_in_txt(str)
+        report = self.new
+        ipr_line = ''
+        str.split(/\n/).each do |line|
+          line = line.split("\t")
+          if line.size == 2
+            report.query_id = line[0]
+            report.query_length = line[1].to_i
+          elsif line.first == '//'
+          elsif line.first == 'InterPro'
+            ipr_line = line
+          else
+            startp, endp = line[4].split("-")
+            report.matches << Match.new(:ipr_id => ipr_line[1], 
+                                        :ipr_description => ipr_line[2],
+                                        :method => line[0], 
+                                        :accession => line[1],
+                                        :description => line[2], 
+                                        :evalue => line[3],
+                                        :match_start => startp.to_i,
+                                        :match_end => endp.to_i)
+          end
+        end
+        report
+      end
+
+      # 
+      def initialize
+        @query_id = nil
+        @query_length = nil
+        @matches = []
+      end
+
+      def to_html
+        NotImplementedError
+      end
+      
+      def to_xml
+        NotImplementedError
+      end
+      
+      def to_ebihtml
+        NotImplementedError        
+      end
+      
+      def to_txt
+        NotImplementedError        
+      end
+      
+      def to_raw
+        NotImplementedError        
+      end
+      
+      def to_gff3
+        NotImplementedError        
+      end
+
+      # == DESCRIPTION
+      # Container class for InterProScan matches.
+      #
+      # == USAGE
+      #  match = Match.new(:query_id => ...)
+      #
+      #  match.ipr_id = 'IPR001234'
+      #  match.ipr_id #=> 'IPR1234'
+      #
+      class Match
+        def initialize(hash)
+          @data = Hash.new
+          hash.each do |key, value|
+            @data[key.to_sym] = value
+          end
+        end
+
+        # Date for computation.
+        def date;            @data[:date];            end
+        # CRC64 checksum of query sequence.
+        def crc64;           @data[:crc64];           end
+        # E-value of the match
+        def evalue;          @data[:evalue];          end
+        # Status of the match (T for true / M for marginal).
+        def status;          @data[:status];          end
+        # the corresponding InterPro entry (if any).
+        def ipr_id;          @data[:ipr_id];          end
+        # the length of the sequence in AA.
+        def length;          @data[:length];          end
+        # the analysis method launched.
+        def method;          @data[:method];          end  # Object#metod overrided by Match#method
+        # the Gene Ontology description for the InterPro entry, in "Aspect:term (ID)" format.
+        def go_terms;        @data[:go_terms];        end
+        # Id of the input sequence.
+        def query_id;        @data[:query_id];        end
+        # the end of the domain match.
+        def match_end;       @data[:match_end];       end
+        # the database members entry for this match.
+        def accession;       @data[:accession];       end
+        # the database mambers description for this match.
+        def description;     @data[:description];     end
+        # the start of the domain match.
+        def match_start;     @data[:match_start];     end
+        # the descriotion of the InterPro entry.
+        def ipr_description; @data[:ipr_description]; end
+
+        def method_missing(name, arg = nil)
+          if arg
+            name = name.to_s.sub(/=$/, '') 
+            @data[name.to_sym] = arg 
+          else
+            @data[name.to_sym]
+          end
+        end
+
+      end # class Match
+
+    end # class Report
+
+  end # class Iprscan
+
+end # module Bio
---
+++ test/unit/bio/appl/iprscan/test_report.rb
@@ -0,0 +1,181 @@
+#
+# test/unit/bio/appl/iprscan/test_report.rb - Unit test for Bio::InterProScan::Report
+#
+#   Copyright (C) 2006 Mitsuteru Nakao <n@bioruby.org>
+#
+#  $Id: test_report.rb,v 1.1 2006/12/14 16:22:12 nakao Exp $
+#
+
+require 'pathname'
+libpath = Pathname.new(File.join(File.dirname(__FILE__), ['..'] * 5, 'lib')).cleanpath.to_s
+$:.unshift(libpath) unless $:.include?(libpath)
+
+require 'test/unit'
+require 'bio/appl/iprscan/report'
+
+
+module Bio
+  class TestIprscanData
+    bioruby_root = Pathname.new(File.join(File.dirname(__FILE__), [".."] * 5)).cleanpath.to_s
+    TestDataIprscan = Pathname.new(File.join(bioruby_root, "test", "data", "iprscan")).cleanpath.to_s
+    def self.raw_format
+      File.open(File.join(TestDataIprscan, "merged.raw"))
+    end
+  end
+  
+  
+  class TestIprscanTxtReport < Test::Unit::TestCase
+
+    def setup
+      test_entry=<<-END
+slr0002\t860
+InterPro\tIPR001264\tGlycosyl transferase, family 51
+BlastProDom\tPD001895\tsp_Q55683_SYNY3_Q55683\t2e-37\t292-370
+HMMPfam\tPF00912\tTransglycosyl\t8e-104\t204-372
+InterPro\tIPR001460\tPenicillin-binding protein, transpeptidase domain
+HMMPfam\tPF00905\tTranspeptidase\t5.7e-30\t451-742
+InterPro\tNULL\tNULL
+ProfileScan\tPS50310\tALA_RICH\t10.224\t805-856
+//
+END
+      @obj = Bio::Iprscan::Report.parse_in_txt(test_entry)
+    end 
+   
+ 
+    def test_query_id
+      assert_equal('slr0002', @obj.query_id)
+    end
+
+    def test_query_length
+      assert_equal(860, @obj.query_length)
+    end
+
+    def test_matches_size
+      assert_equal(4, @obj.matches.size)
+    end
+    
+    def test_match_ipr_id
+      assert_equal('IPR001264', @obj.matches.first.ipr_id)
+    end
+
+    def test_match_ipr_description
+      assert_equal('Glycosyl transferase, family 51', @obj.matches.first.ipr_description)
+    end
+
+    def test_match_method
+      assert_equal('BlastProDom', @obj.matches.first.method)
+    end
+
+    def test_match_accession
+      assert_equal('PD001895', @obj.matches.first.accession)
+    end
+
+    def test_match_description
+      assert_equal('sp_Q55683_SYNY3_Q55683', @obj.matches.first.description)
+    end
+
+    def test_match_evalue
+      assert_equal('2e-37', @obj.matches.first.evalue)
+    end
+
+    def test_match_match_start
+      assert_equal(292, @obj.matches.first.match_start)
+    end
+
+    def test_match_match_end
+      assert_equal(370, @obj.matches.first.match_end)
+    end
+
+  end # TestIprscanTxtReport
+
+  class TestIprscanRawReport < Test::Unit::TestCase
+    def setup
+      test_raw = Bio::TestIprscanData.raw_format
+      entry = ''
+      @obj = []
+      while line = test_raw.gets
+        if entry != '' and entry.split("\t").first == line.split("\t").first
+          entry << line
+        elsif entry != ''
+          @obj << Bio::Iprscan::Report.parse_in_raw(entry)
+          entry = line
+        else
+          entry << line
+        end
+      end
+    end
+    
+    def test_obj
+      assert_equal(2, @obj.size)
+    end
+    
+    def test_query_id
+      assert_equal('Q9RHD9', @obj.first.query_id)
+    end
+
+    def test_entry_id
+      assert_equal('Q9RHD9', @obj.first.entry_id)
+    end
+
+    def test_query_length
+      assert_equal(267, @obj.first.query_length)
+    end
+    
+    def test_match_query_id
+      assert_equal('Q9RHD9', @obj.first.matches.first.query_id)
+    end
+    
+    def test_match_crc64
+      assert_equal('D44DAE8C544CB7C1', @obj.first.matches.first.crc64)
+    end
+    
+    def test_match_query_length
+      assert_equal(267, @obj.first.matches.first.query_length)
+    end
+    
+    def test_match_method
+      assert_equal('HMMPfam', @obj.first.matches.first.method)
+    end
+    
+    def test_match_accession
+      assert_equal('PF00575', @obj.first.matches.first.accession)
+    end
+    
+    def test_match_description
+      assert_equal('S1', @obj.first.matches.first.description)
+    end
+    
+    def test_match_match_start
+      assert_equal(1, @obj.first.matches.first.match_start)
+    end
+    
+    def test_match_match_end
+      assert_equal(55, @obj.first.matches.first.match_end)
+    end
+    
+    def test_match_evalue
+      assert_equal('3.3E-6', @obj.first.matches.first.evalue)
+    end
+    
+    def test_match_status
+      assert_equal('T', @obj.first.matches.first.status)
+    end
+
+    def test_match_date
+      assert_equal('11-Nov-2005', @obj.first.matches.first.date)
+    end
+
+    def test_match_ipr_id
+      assert_equal('IPR003029', @obj.first.matches.first.ipr_id)
+    end
+
+    def test_match_ipr_description
+      assert_equal('RNA binding S1', @obj.first.matches.first.ipr_description)
+    end
+
+    def test_match_go_terms
+      assert_equal(["Molecular Function:RNA binding (GO:0003723)"], @obj.first.matches.first.go_terms)
+    end
+
+  end
+end
