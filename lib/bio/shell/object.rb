b95458eccf0272d42d7b2d13facefc6e81409809
Toshiaki Katayama <k@bioruby.org>
2006-12-24 08:40:00 +0000

    * skeletal attempt for object formatting

--- lib/bio/shell/object.rb
+++ lib/bio/shell/object.rb
@@ -6,11 +6,12 @@
 #               Toshiaki Katayama <k@bioruby.org>
 # License::     Ruby's
 #
-# $Id: object.rb,v 1.1 2006/02/27 09:16:13 k Exp $
+# $Id: object.rb,v 1.2 2006/12/24 08:40:00 k Exp $
 #
 
-require 'cgi'
 require 'pp'
+require 'cgi'
+require 'yaml'
 
 ### Object extention
 
@@ -18,35 +19,53 @@ class Object
   # Couldn't work for Fixnum (Marshal)
   attr_accessor :memo
 
-  # *TODO*
-  def to_html
-    if self.is_a?(String)
-      "<pre>" + self + "</pre>"
+  def output(format = :yaml)
+    case format
+    when :yaml
+      self.to_yaml
+    when :html
+      format_html
+    when :inspect
+      format_pp
+    when :png
+      # *TODO*
+    when :svg
+      # *TODO*
+    when :graph
+      # *TODO* (Gruff, RSRuby etc.)
     else
-      str = ""
-      PP.pp(self, str)
-      "<pre>" + str + "</pre>"
-      #"<pre>" + CGI.escapeHTML(str) + "</pre>"
-      #self.inspect
-      #"<pre>" + self.inspect + "</pre>"
-      #"<pre>" + self.to_s + "</pre>"
+      #self.inspect.to_s.fold(80)
+      self.to_s
     end
   end
-end
 
-=begin
-module Bio
-  class DB
-    def to_html
-      html = ""
-      html += "<table>"
-      @data.each do |k, v|
-        html += "<tr><td>#{k}</td><td>#{v}</td></tr>"
-      end
-      html += "</table>"
-    end
+  private
+
+  def format_html
+    "<pre>#{CGI.escapeHTML(format_pp)}</pre>"
   end
-end
-=end
 
- 
+  def format_pp
+    str = ""
+    PP.pp(self, str)
+    return str
+  end
+
+end
+
+class Hash
+
+  private
+
+  def format_html
+    html = ""
+    html += "<table>"
+    @data.each do |k, v|
+      html += "<tr><td>#{k}</td><td>#{v}</td></tr>"
+    end
+    html += "</table>"
+    return html
+  end
+
+end
+
