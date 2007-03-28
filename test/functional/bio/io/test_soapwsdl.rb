1df15421749d57ba7b18e301eb10eae4007a2552
Mitsuteru Nakao <n@bioruby.org>
2007-03-28 20:48:11 +0000

    * Changed licence (GPL2 -> Ruby's)

--- test/functional/bio/io/test_soapwsdl.rb
+++ test/functional/bio/io/test_soapwsdl.rb
@@ -1,23 +1,11 @@
 #
-# test/functional/bio/io/test_soapwsdl.rb - Unit test for SOAP/WSDL
+# test/functional/bio/io/test_soapwsdl.rb - Functional test for SOAP/WSDL
 #
-#   Copyright (C) 2005 Mitsuteru Nakao <n@bioruby.org>
+# Copyright::   Copyright (C) 2005,2007
+#               Mitsuteru C. Nakao <n@bioruby.org>
+# License::     Ruby's
 #
-#  This library is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU Lesser General Public
-#  License as published by the Free Software Foundation; either
-#  version 2 of the License, or (at your option) any later version.
-#
-#  This library is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#  Lesser General Public License for more details.
-#
-#  You should have received a copy of the GNU Lesser General Public
-#  License along with this library; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
-#
-#  $Id: test_soapwsdl.rb,v 1.2 2006/01/20 12:04:03 k Exp $ 
+#  $Id: test_soapwsdl.rb,v 1.3 2007/03/28 20:48:11 nakao Exp $ 
 #
 
 require 'pathname'
