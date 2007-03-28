577c3a8aeeb2ec3d1177b95ebb9dfb1b038e43ed
Toshiaki Katayama <k@bioruby.org>
2007-03-28 10:31:48 +0000

    * modified to use Bio::Command.post_form * newly introduced Bio::Ensembl.new accepts organism and server url as its   argument, so that user does not need to create subclass. * several bug fixed

--- lib/bio/io/ensembl.rb
+++ lib/bio/io/ensembl.rb
@@ -5,7 +5,7 @@
 #               Mitsuteru C. Nakao <n@bioruby.org>
 # License::     Ruby's
 #
-# $Id: ensembl.rb,v 1.3 2006/07/14 14:28:44 ngoto Exp $
+# $Id: ensembl.rb,v 1.4 2007/03/28 10:31:48 k Exp $
 #
 # == Description
 #
@@ -38,17 +38,16 @@ module Bio
 #
 # == Examples
 #
-#  seq = Bio::Ensembl::Human.exportview(1, 1000, 100000)
-#  gff = Bio::Ensembl::Human.exportview(1, 1000, 100000, ['gene'])
+#  human = Bio::Ensembl.new('Homo_sapiens')
+#  seq = human.exportview(1, 1000, 100000)
+#  gff = human.exportview(1, 1000, 100000, ['gene'])
 #
-#  seq = Bio::Ensembl::Mouse.exportview(1, 1000, 100000)
-#  gff = Bio::Ensembl::Mouse.exportview(1, 1000, 100000, ['gene', 'variation', 'genscan'])
+#  mouse = Bio::Ensembl.new('Mus_musculus')
+#  seq = mouse.exportview(1, 1000, 100000)
+#  gff = mouse.exportview(1, 1000, 100000, ['gene', 'variation', 'genscan'])
 #
-#  Bio::Enesmbl.server_uri("http://www.gramene.org")
-#  class Rice < Base
-#    Organism = 'Oryza_sativa'
-#  end
-#  seq = Bio::Ensembl::Rice.exportview(1, 1000, 100000)
+#  rice = Bio::Enesmbl.new('Oryza_sativa', 'http://www.gramene.org')
+#  seq = rice.exportview(1, 1000, 100000)
 #
 # == References
 #
@@ -60,200 +59,110 @@ module Bio
 #
 class Ensembl
 
-  # Hostname of the Ensembl Genome Browser.
-  EBIServerURI = 'http://www.ensembl.org'
+  ENSEMBL_URL = 'http://www.ensembl.org'
 
-  # An Alternative Hostname for Ensembl Genome Browser.
-  @@server_uri = nil
-
-  # Sets and uses an alternative hostname for ensembl genome browser.
-  #
-  # == Example
-  #
-  #   require 'bio'
-  #   p Bio::Enesmbl.server_uri #=> 'http://www.ensembl.org'
-  #   Bio::Enesmbl.server_uri("http://www.gramene.org")
-  #   p Bio::Enesmbl.server_uri #=> "http://www.gramene.org"
-  #
-  def self.server_uri(uri = nil)
-    if uri
-      @@server_uri = uri
-    else
-      @@server_uri || EBIServerURI
-    end
+  def initialize(organism, server = nil)
+    @server = server || ENSEMBL_URL
+    @organism = organism
+    @uri = [ server.chomp('/'), @organism ].join('/')
   end
 
+  def self.human
+    self.new("Homo_sapiens")
+  end
 
-  # Ensembl Genome Browser Client Super Class
+  def self.mouse
+    self.new("Mus_musculus")
+  end
+
+  # Ensembl ExportView Client.
+  #
+  # Retrieve genomic sequence/features from Ensembl ExportView in plain text.
+  # Ensembl ExportView exports genomic data (sequence and features) in 
+  # several file formats including fasta, GFF and tab.
+  #
+  # * ExportViwe (http://www.ensembl.org/Homo_sapiens/exportview).
   #
   # == Examples
-  #   
-  #   module Bio
-  #     class Ensembl::Kumamushi < Base
-  #       Organism = 'Milnesium_tardigradum'
-  #     end
-  #   end
-  #   fna = Bio::Ensembl::Kumamushi.exportview(1, 1000, 20000)
   #
-  class Base
-
-    # Ensembl ExportView Client.
-    #
-    # Retrieve genomic sequence/features from Ensembl ExportView in plain text.
-    # Ensembl ExportView exports genomic data (sequence and features) in 
-    # several file formats including fasta, GFF and tab.
-    #
-    # * ExportViwe (http://www.ensembl.org/Homo_sapiens/exportview).
-    #
-    # == Examples
-    #
-    #   # Genomic sequence in Fasta format
-    #   Bio::Ensembl::Human.exportview(:seq_region_name => 1, 
-    #                                  :anchor1 => 1149206, :anchor2 => 1149229)
-    #   Bio::Ensembl::Human.exportview(1, 1149206, 1149229)
-    #
-    #   # Feature in GFF
-    #   Bio::Ensembl::Human.exportview(:seq_region_name => 1, 
-    #                                  :anchor1 => 1149206, :anchor2 => 1150000, 
-    #                                  :options => ['similarity', 'repeat', 
-    #                                               'genscan', 'variation', 
-    #                                               'gene'])
-    #   Bio::Ensembl::Human.exportview(1, 1149206, 1150000, 
-    #                                  ['variation', 'gene'])
-    #
-    # == Arguments
-    #
-    # Bio::Ensembl::Base#exportview method allow both orderd arguments and 
-    # named arguments. 
-    # Note: mandatory arguments marked '*'.
-    #
-    # === Orderd Arguments
-    #
-    # 1. seq_region_name - Chromosome number (*)
-    # 2. anchor1         - From coordination (*)
-    # 3. anchor2         - To coordination (*)
-    # 4. options         - Features to export (in :format => 'gff' or 'tab')
-    #                      ['similarity', 'repeat', 'genscan', 'variation', 
-    #                       'gene']
-    #
-    # === Named Arguments
-    # 
-    # * :seq_region_name - Chromosome number (*)
-    # * :anchor1         - From coordination (*)
-    # * :anchor2         - To coordination (*)
-    # * :type1           - From coordination type ['bp', ]
-    # * :type2           - To coordination type ['bp', ]
-    # * :upstream        - Bp upstream
-    # * :downstream      - Bp downstream
-    # * :format          - File format ['fasta', 'gff', 'tab']
-    # * :options         - Features to export (for :format => 'gff' or 'tab')
-    #                      ['similarity', 'repeat', 'genscan', 'variation', 
-    #                       'gene']
-    # 
-    def self.exportview(*args)
-      if args.first.class == Hash then opts = args.first
-      else
-        options = {:seq_region_name => args[0], 
-                   :anchor1 => args[1], 
-                   :anchor2 => args[2]}
-        case args.size
-        when 3 then 
-          options.update({:format => 'fasta'})
-        when 4 then 
-          options.update({:format => 'gff', :options => args[3]})
-        end
+  #   human = Bio::Ensembl.new('Homo_sapiens')
+  #     or
+  #   human = Bio::Ensembl.human
+  #
+  #   # Genomic sequence in Fasta format
+  #   human.exportview(:seq_region_name => 1, 
+  #                    :anchor1 => 1149206, :anchor2 => 1149229)
+  #   human.exportview(1, 1149206, 1149229)
+  #
+  #   # Feature in GFF
+  #   human.exportview(:seq_region_name => 1, 
+  #                    :anchor1 => 1149206, :anchor2 => 1150000, 
+  #                    :options => ['similarity', 'repeat', 
+  #                                 'genscan', 'variation', 'gene'])
+  #   human.exportview(1, 1149206, 1150000, ['variation', 'gene'])
+  #
+  # == Arguments
+  #
+  # Bio::Ensembl#exportview method allow both orderd arguments and 
+  # named arguments. (Note: mandatory arguments are marked by '*').
+  #
+  # === Orderd Arguments
+  #
+  # 1. seq_region_name - Chromosome number (*)
+  # 2. anchor1         - From coordination (*)
+  # 3. anchor2         - To coordination (*)
+  # 4. options         - Features to export (in :format => 'gff' or 'tab')
+  #                      ['similarity', 'repeat', 'genscan', 'variation', 
+  #                       'gene']
+  #
+  # === Named Arguments
+  # 
+  # * :seq_region_name - Chromosome number (*)
+  # * :anchor1         - From coordination (*)
+  # * :anchor2         - To coordination (*)
+  # * :type1           - From coordination type ['bp', ]
+  # * :type2           - To coordination type ['bp', ]
+  # * :upstream        - Bp upstream
+  # * :downstream      - Bp downstream
+  # * :format          - File format ['fasta', 'gff', 'tab']
+  # * :options         - Features to export (for :format => 'gff' or 'tab')
+  #                      ['similarity', 'repeat', 'genscan', 'variation', 
+  #                       'gene']
+  # 
+  def exportview(*args)
+    if args.first.class == Hash
+      options = args.first
+    else
+      options = {
+        :seq_region_name => args[0], 
+        :anchor1 => args[1], 
+        :anchor2 => args[2],
+      }
+      case args.size
+      when 3 then 
+        options.update({:format => 'fasta'})
+      when 4 then 
+        options.update({:format => 'gff', :options => args[3]})
       end
-
-      @data = {:type1 => 'bp', 
-               :type2 => 'bp', 
-               :downstream => '', 
-               :upstream => '', 
-               :format => 'fasta',
-               :options => [],
-               :action => 'export', 
-               :_format => 'Text', 
-               :output => 'txt', 
-               :submit => 'Continue >>'}
-
-      cgi = Client.new('exportview', self::Organism)
-      cgi.exec(@data.update(options))
     end
+    
+    defaults = {
+      :type1 => 'bp', 
+      :type2 => 'bp', 
+      :downstream => '', 
+      :upstream => '', 
+      :format => 'fasta',
+      :options => [],
+      :action => 'export', 
+      :_format => 'Text', 
+      :output => 'txt', 
+      :submit => 'Continue >>'
+    }
 
+    params = defaults.update(options)
 
-
-    # An Ensembl CGI client class
-    #
-    # Enable the use of HTTP access via a proxy by setting the proxy address up
-    # as the 'http_proxy' enviroment variable. 
-    # 
-    # === Examples
-    #
-    #  cgi = Client.new('martview', 'Homo_sapiens')
-    #  result_body = cgi.exec(hash_data)
-    #
-    class Client
-
-      # Sets cgi_name and genome_name.
-      #
-      # === Example
-      #
-      #  cgi = Client.new('martview', 'Homo_sapiens')
-      #
-      def initialize(cgi_name, genome_name)
-        @uri = URI.parse(Ensembl.server_uri)
-        @path = ['', genome_name, cgi_name].join('/')
-      end
-
-      # Executes query with data.
-      #
-      # === Example
-      #
-      #  result_body = cgi.exec(hash_data)
-      #
-      def exec(data_hash)
-        data = make_args(data_hash)
-
-        result = nil      
-        Bio::Command.start_http(@uri.host, @uri.port) {|http|
-          result, = http.post(@path, data)
-        }
-        result.body
-      end
-
-      private
-
-      def make_args(hash)
-        tmp = []
-        hash.each do |key, value|
-          if value.class == Array then 
-            value.each { |val| tmp << [key, val] } 
-          else 
-            tmp << [key, value] 
-          end
-        end
-        tmp.map {|e| e.map {|x| CGI.escape(x.to_s) }.join("=") }.join('&')
-      end
-
-    end # class Client
-
-  end # class Base
-
-
-  # Ensembl Human Genome
-  # 
-  # See Bio::Ensembl::Base class.
-  # 
-  class Human < Base
-    Organism = 'Homo_sapiens'
-  end # class Human
-
-  # Ensembl Mouse Genome
-  #
-  # See Bio::Ensembl::Base class.
-  #
-  class Mouse < Base
-    Organism = 'Mus_musculus'
-  end # class Mouse
+    Bio::Command.post_form("#{@uri}/exportview", params)
+  end
 
 end # class Ensembl
 
