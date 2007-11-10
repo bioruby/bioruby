0c09e90bad39a1e66a8d6b6c52da716f198f2475
Mitsuteru Nakao <n@bioruby.org>
2007-11-10 16:57:43 +0000

    * Updated some expected values of test_gff_exportview*.

--- test/functional/bio/io/test_ensembl.rb
+++ test/functional/bio/io/test_ensembl.rb
@@ -5,7 +5,7 @@
 #               Mitsuteru C. Nakao <n@bioruby.org>
 # License::     The Ruby License
 #
-#  $Id: test_ensembl.rb,v 1.4 2007/04/05 23:35:42 trevor Exp $
+#  $Id: test_ensembl.rb,v 1.5 2007/11/10 16:57:43 nakao Exp $
 #
 
 require 'pathname'
@@ -73,13 +73,47 @@ class FuncTestEnsemblHuman < Test::Unit::TestCase
   end
 
    def test_gff_exportview
-     line = "chromosome:NCBI36:4:1149206:1149209:1\tEnsembl\tGene\t-839\t2747\t.\t+\t.\tgene_id=ENSG00000206158; transcript_id=ENST00000382964; exon_id=ENSE00001494097; gene_type=KNOWN_protein_coding\n"
+     line = ["chromosome:NCBI36:4:1149206:1149209:1", 
+             "Ensembl", 
+             "Gene", 
+             "-839",
+             "2747", 
+             ".", 
+             "+",
+             ".",
+             "gene_id=ENSG00000206158; transcript_id=ENST00000382964; exon_id=ENSE00001494097; gene_type=KNOWN_protein_coding\n"].join("\t")  + "\n"
+     line = ["4", 
+             "Ensembl", 
+             "Gene", 
+             "1148366", 
+             "1151952", 
+             ".", 
+             "+", 
+             "1", 
+             "gene_id=ENSG00000206158; transcript_id=ENST00000382964; exon_id=ENSE00001494097; gene_type=KNOWN_protein_coding"].join("\t") + "\n"
      gff = @serv.exportview(4, 1149206, 1149209, ['gene'])
      assert_equal(line, gff)
    end
 
    def test_gff_exportview_with_named_args
-     line = "chromosome:NCBI36:4:1149206:1149209:1\tEnsembl\tGene\t-839\t2747\t.\t+\t.\tgene_id=ENSG00000206158; transcript_id=ENST00000382964; exon_id=ENSE00001494097; gene_type=KNOWN_protein_coding\n"
+     line = ["chromosome:NCBI36:4:1149206:1149209:1",
+             "Ensembl",
+             "Gene",
+             "-839",
+             "2747",
+             ".",
+             "+",
+             ".",
+             "gene_id=ENSG00000206158; transcript_id=ENST00000382964; exon_id=ENSE00001494097; gene_type=KNOWN_protein_coding"].join("\t") + "\n"
+     line = ["4", 
+             "Ensembl", 
+             "Gene", 
+             "1148366", 
+             "1151952", 
+             ".", 
+             "+", 
+             "1", 
+             "gene_id=ENSG00000206158; transcript_id=ENST00000382964; exon_id=ENSE00001494097; gene_type=KNOWN_protein_coding"].join("\t") + "\n"
      gff = @serv.exportview(:seq_region_name => 4,
                             :anchor1 => 1149206,
                             :anchor2 => 1149209, 
@@ -88,7 +122,56 @@ class FuncTestEnsemblHuman < Test::Unit::TestCase
    end 
 
    def test_tab_exportview_with_named_args
-     line = "seqname\tsource\tfeature\tstart\tend\tscore\tstrand\tframe\tgene_id\ttranscript_id\texon_id\tgene_type\nchromosome:NCBI36:4:1149206:1149209:1\tEnsembl\tGene\t-839\t2747\t.\t+\t.\tENSG00000206158\tENST00000382964\tENSE00001494097\tKNOWN_protein_coding\n"
+     line = [["seqname",
+             "source",
+             "feature",
+             "start",
+             "end",
+             "score",
+             "strand",
+             "frame",
+             "gene_id",
+             "transcript_id",
+             "exon_id",
+             "gene_type"].join("\t"),
+             ["chromosome:NCBI36:4:1149206:1149209:1",
+              "Ensembl",
+              "Gene",
+              "-839",
+              "2747",
+              ".",
+              "+",
+              ".",
+              "ENSG00000206158",
+              "ENST00000382964",
+              "ENSE00001494097",
+              "KNOWN_protein_coding"].join("\t") + "\n"
+     ].join("\n")
+     line = [["seqname",
+             "source",
+             "feature",
+             "start",
+             "end",
+             "score",
+             "strand",
+             "frame",
+             "gene_id",
+             "transcript_id",
+             "exon_id",
+             "gene_type"].join("\t"),
+             ["4",
+              "Ensembl",
+              "Gene",
+              "1148366",
+              "1151952",
+              ".",
+              "+",
+              "1",
+              "ENSG00000206158",
+              "ENST00000382964",
+              "ENSE00001494097",
+              "KNOWN_protein_coding"].join("\t") + "\n"
+     ].join("\n")
      gff = @serv.exportview(:seq_region_name => 4,
                             :anchor1 => 1149206,
                             :anchor2 => 1149209, 
