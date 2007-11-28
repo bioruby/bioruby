d67299e868319f532898398113ad422c17079406
Toshiaki Katayama <k@bioruby.org>
2007-11-28 06:34:33 +0000

    * all class methods are changed to instance methods (class methods are   still remained for the backward compatibility)

--- lib/bio/io/pubmed.rb
+++ lib/bio/io/pubmed.rb
@@ -5,7 +5,7 @@
 # Copyright::  Copyright (C) 2006 Jan Aerts <jan.aerts@bbsrc.ac.uk>
 # License::    The Ruby License
 #
-# $Id: pubmed.rb,v 1.21 2007/11/20 15:22:03 k Exp $
+# $Id: pubmed.rb,v 1.22 2007/11/28 06:34:33 k Exp $
 #
 
 require 'bio/command'
@@ -78,7 +78,9 @@ class PubMed
   NCBI_INTERVAL = 3
   @@last_access = nil
 
-  def self.ncbi_access_wait(wait = NCBI_INTERVAL)
+  private
+
+  def ncbi_access_wait(wait = NCBI_INTERVAL)
     if @@last_access
       duration = Time.now - @@last_access
       if wait > duration
@@ -87,6 +89,8 @@ class PubMed
     end
     @@last_access = Time.now
   end
+
+  public
 
   # Search the PubMed database by given keywords using E-Utils and returns 
   # an array of PubMed IDs.
@@ -106,7 +110,7 @@ class PubMed
   # * _retmode_
   # * _rettype_
   # *Returns*:: array of PubMed IDs or a number of results
-  def self.esearch(str, hash = {})
+  def esearch(str, hash = {})
     return nil if str.empty?
 
     serv = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
@@ -118,7 +122,7 @@ class PubMed
     }
     opts.update(hash)
 
-    self.ncbi_access_wait
+    ncbi_access_wait
 
     response, = Bio::Command.post_form(serv, opts)
     result = response.body
@@ -138,7 +142,7 @@ class PubMed
   # *Arguments*:
   # * _ids_: list of PubMed IDs (required)
   # *Returns*:: Array of MEDLINE formatted String
-  def self.efetch(ids, hash = {})
+  def efetch(ids, hash = {})
     return nil if ids.to_s.empty?
     ids = ids.join(",") if ids === Array
 
@@ -152,7 +156,7 @@ class PubMed
     }
     opts.update(hash)
 
-    self.ncbi_access_wait
+    ncbi_access_wait
 
     response, = Bio::Command.post_form(serv, opts)
     result = response.body
@@ -169,9 +173,11 @@ class PubMed
   # *Arguments*:
   # * _id_: query string (required)
   # *Returns*:: array of PubMed IDs
-  def self.search(str)
+  def search(str)
     host = "www.ncbi.nlm.nih.gov"
     path = "/sites/entrez?tool=bioruby&cmd=Search&doptcmdl=Brief&db=PubMed&term="
+
+    ncbi_access_wait
 
     http = Bio::Command.new_http(host)
     response, = http.get(path + CGI.escape(str))
@@ -186,11 +192,12 @@ class PubMed
   # *Arguments*:
   # * _id_: PubMed ID (required)
   # *Returns*:: MEDLINE formatted String
-  def self.query(*ids)
+  def query(*ids)
     host = "www.ncbi.nlm.nih.gov"
     path = "/sites/entrez?tool=bioruby&cmd=Text&dopt=MEDLINE&db=PubMed&uid="
-
     list = ids.join(",")
+
+    ncbi_access_wait
 
     http = Bio::Command.new_http(host)
     response, = http.get(path + list)
@@ -215,9 +222,11 @@ class PubMed
   # *Arguments*:
   # * _id_: PubMed ID (required)
   # *Returns*:: MEDLINE formatted String
-  def self.pmfetch(id)
+  def pmfetch(id)
     host = "www.ncbi.nlm.nih.gov"
     path = "/entrez/utils/pmfetch.fcgi?tool=bioruby&mode=text&report=medline&db=PubMed&id="
+
+    ncbi_access_wait
 
     http = Bio::Command.new_http(host)
     response, = http.get(path + id.to_s)
@@ -230,12 +239,78 @@ class PubMed
     end
   end
 
+  def self.esearch(*args)
+    self.new.esearch(*args)
+  end
+
+  def self.efetch(*args)
+    self.new.efetch(*args)
+  end
+
+  def self.search(*args)
+    self.new.search(*args)
+  end
+
+  def self.query(*args)
+    self.new.query(*args)
+  end
+
+  def self.pmfetch(*args)
+    self.new.pmfetch(*args)
+  end
+
 end # PubMed
 
 end # Bio
 
 
 if __FILE__ == $0
+
+  puts "=== instance methods ==="
+
+  pubmed = Bio::PubMed.new
+
+  puts "--- Search PubMed by E-Utils ---"
+  opts = {"rettype" => "count"}
+  puts Time.now
+  puts pubmed.esearch("(genome AND analysis) OR bioinformatics)", opts)
+  puts Time.now
+  puts pubmed.esearch("(genome AND analysis) OR bioinformatics)", opts)
+  puts Time.now
+  puts pubmed.esearch("(genome AND analysis) OR bioinformatics)", opts)
+  puts Time.now
+  pubmed.esearch("(genome AND analysis) OR bioinformatics)").each do |x|
+    puts x
+  end
+
+  puts "--- Retrieve PubMed entry by E-Utils ---"
+  puts Time.now
+  puts pubmed.efetch(16381885)
+  puts Time.now
+  puts pubmed.efetch("16381885")
+  puts Time.now
+  puts pubmed.efetch("16381885")
+  puts Time.now
+  opts = {"retmode" => "xml"}
+  puts pubmed.efetch([10592173, 14693808], opts)
+  puts Time.now
+  puts pubmed.efetch(["10592173", "14693808"], opts)
+
+  puts "--- Search PubMed by Entrez CGI ---"
+  pubmed.search("(genome AND analysis) OR bioinformatics)").each do |x|
+    p x
+  end
+
+  puts "--- Retrieve PubMed entry by Entrez CGI ---"
+  puts pubmed.query("16381885")
+
+
+  puts "--- Retrieve PubMed entry by PMfetch ---"
+  puts pubmed.pmfetch("16381885")
+
+
+  puts "=== class methods ==="
+
 
   puts "--- Search PubMed by E-Utils ---"
   opts = {"rettype" => "count"}
