bff186b43039bb49e068b0bfb7a139c342f8d18a
Naohisa Goto <ng@bioruby.org>
2008-03-04 10:12:22 +0000

    Bio::Features is moved to lib/bio/compat/features.rb, and a module to keep backward compatibility (Bio::Features::BackwardCompatibility) is added.

--- lib/bio/feature.rb
+++ lib/bio/feature.rb
@@ -5,7 +5,7 @@
 #                             2006        Jan Aerts <jan.aerts@bbsrc.ac.uk>
 # License::	The Ruby License
 #
-# $Id: feature.rb,v 1.13 2007/04/05 23:35:39 trevor Exp $
+# $Id: feature.rb,v 1.13.2.1 2008/03/04 10:12:22 ngoto Exp $
 
 require 'bio/location'
 
@@ -134,93 +134,6 @@ class Feature
   end #Qualifier
 
 end #Feature
-
-
-# = DESCRIPTION
-# Container for a list of Feature objects.
-#
-# = USAGE
-#  # First, create some Bio::Feature objects
-#  feature1 = Bio::Feature.new('intron','3627..4059')
-#  feature2 = Bio::Feature.new('exon','4060..4236')
-#  feature3 = Bio::Feature.new('intron','4237..4426')
-#  feature4 = Bio::Feature.new('CDS','join(2538..3626,4060..4236)',
-#                   [ Bio::Feature::Qualifier.new('gene', 'CYP2D6'),
-#                     Bio::Feature::Qualifier.new('translation','MGXXTVMHLL...')
-#                   ])
-#
-#  # And create a container for them
-#  feature_container = Bio::Features.new([ feature1, feature2, feature3, feature4 ])
-#
-#  # Iterate over all features and print
-#  feature_container.each do |feature|
-#    puts feature.feature + "\t" + feature.position
-#    feature.each do |qualifier|
-#      puts "- " + qualifier.qualifier + ": " + qualifier.value
-#    end
-#  end
-#
-#  # Iterate only over CDS features and extract translated amino acid sequences
-#  features.each("CDS") do |feature|
-#    hash = feature.to_hash
-#    name = hash["gene"] || hash["product"] || hash["note"] 
-#    aaseq  = hash["translation"]
-#    pos  = feature.position
-#    if name and seq
-#      puts ">#{gene} #{feature.position}"
-#      puts aaseq
-#    end
-#  end
-class Features
-  # Create a new Bio::Features object.
-  #
-  # *Arguments*:
-  # * (optional) _list of features_: list of Bio::Feature objects
-  # *Returns*:: Bio::Features object
-  def initialize(ary = [])
-    @features = ary
-  end
-
-  # Returns an Array of Feature objects.
-  attr_accessor :features
-
-  # Appends a Feature object to Features.
-  # 
-  # *Arguments*:
-  # * (required) _feature_: Bio::Feature object
-  # *Returns*:: Bio::Features object
-  def append(a)
-    @features.push(a) if a.is_a? Feature
-    return self
-  end
-
-  # Iterates on each feature object.
-  #
-  # *Arguments*:
-  # * (optional) _key_: if specified, only iterates over features with this key
-  def each(arg = nil)
-    @features.each do |x|
-      next if arg and x.feature != arg
-      yield x
-    end
-  end
-
-  # Short cut for the Features#features[n]
-  def [](*arg)
-    @features[*arg]
-  end
-
-  # Short cut for the Features#features.first
-  def first
-    @features.first
-  end
-
-  # Short cut for the Features#features.last
-  def last
-    @features.last
-  end
-
-end # Features
 
 end # Bio
 
