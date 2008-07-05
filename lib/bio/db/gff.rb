a37910ba80ea0ebe3876970e7396285034c602f5
Naohisa Goto <ngoto@gen-info.osaka-u.ac.jp>
2008-07-05 21:47:14 +0900

    Character escaping in GFF3 is fully implemented with incompatible changes
    * Escaping of characters described in GFF3 spec document is implemented.
      All columns and attributes are now escaped/unescaped in GFF3.
      (Note that GFF1 and GFF2 classes are still not aware of escaping,
      because their escaping rules are ambiguous.)
    * The last character dropping bug in attributes which might also happen in
      GFF1 and GFF2 are fixed.
    * Incompatible changes for GFF3: an attribute tag which have multiple
      attributes is now stored in an Array of String (or some special class).
      For example, "Alias=AAA,bbb" is stored as { "Alias" => [ "AAA", "bbb" ] }.
    * New class Bio::GFF::GFF3::Record::Target to store "Target" attribute.
    * Some alias of methods are added to follow terms used in GFF3 spec.

--- lib/bio/db/gff.rb
+++ lib/bio/db/gff.rb
@@ -8,7 +8,8 @@
 #
 # $Id: gff.rb,v 1.9 2007/05/18 15:23:42 k Exp $
 #
-require 'cgi'
+require 'uri'
+require 'strscan'
 
 module Bio
   # == DESCRIPTION
@@ -114,16 +115,20 @@ module Bio
         @attributes = parse_attributes(attributes) if attributes
       end
 
-      protected
+      private
 
       def parse_attributes(attributes)
         hash = Hash.new
-        attributes.split(/[^\\];/).each do |atr|
-          key, value = atr.split(' ', 2)
+        scanner = StringScanner.new(attributes)
+        while scanner.scan(/(.*[^\\])\;/) or scanner.scan(/(.+)/)
+          key, value = scanner[1].split(' ', 2)
+          key.strip!
+          value.strip! if value
           hash[key] = value
         end
-        return hash
+        hash
       end
+
     end #Class Record
 
     # = DESCRIPTION
@@ -140,73 +145,256 @@ module Bio
     class GFF3 < GFF
       VERSION = 3
       
-      # The characters that are escaped
-      ESCAPED_CHARACTERS = [',','=',';']
-      
+      # Creates a Bio::GFF::GFF3 object by building a collection of
+      # Bio::GFF::GFF3::Record objects.
+      # 
+      # ---
+      # *Arguments*:
+      # * _str_: string in GFF format
+      # *Returns*:: Bio::GFF object
       def initialize(str = '')
         @records = Array.new
         str.each_line do |line|
           @records << GFF3::Record.new(line)
         end
       end
-      
+
+      # string representation
       def to_s
-        return '' if !@records
-        return  @records.collect{|r| r.to_s}.join("\n")+"\n"
+        return '' unless @records
+        @records.collect{ |r| r.to_s }.join('')
       end
 
-      protected
+      # Represents a single line of a GFF3-formatted file.
+      # See Bio::GFF::GFF3 for more information.
       class Record < GFF::Record
+
+        # aliases for Column 1 (formerly "seqname")
+        alias seqid seqname
+        alias seqid= seqname=
+
+        # aliases for Column 3 (formerly "feature").
+        # In the GFF3 document http://song.sourceforge.net/gff3.shtml,
+        # column3 is called "type", but we used "feature_type"
+        # because "type" is already used by Ruby itself.
+        alias feature_type feature
+        alias feature_type= feature=
+
+        # aliases for Column 8
+        alias phase frame
+        alias phase= frame=
+        
+        # Creates a Bio::GFF::GFF3::Record object.
+        # Is typically not called directly, but
+        # is called automatically when creating a Bio::GFF::GFF3 object.
+        # ---
+        # *Arguments*:
+        # * _str_: a tab-delimited line in GFF3 format
+        def initialize(str = nil)
+          parse(str) if str
+          @attributes ||= {}
+        end
+
+        # Parses a GFF3-formatted line and stores data from the string.
+        # Note that all existing data is wiped out.
+        def parse(string)
+          string, comments = string.chomp.split(/\#/, 2)
+          columns = string.split("\t")
+          @seqname, @source, @feature,
+          start, endp, @score, @strand, @frame =
+            columns[0, 8].collect { |x|
+            str = unescape(x)
+            str == '.' ? nil : str
+          }
+          @start = start ? start.to_i : nil
+          @end   = endp  ? endp.to_i : nil
+
+          @attributes = parse_attributes(columns[8])
+        end
+
         # Return the record as a GFF3 compatible string
         def to_s
           [
-            @seqname, @source, @feature, @start, @end, @score, @strand, @frame,
-            attributes_to_s
-          ].join("\t")
+           escape(column_to_s(@seqname)),
+           escape(column_to_s(@source)),
+           escape(column_to_s(@feature)),
+           escape(column_to_s(@start)),
+           escape(column_to_s(@end)),
+           escape(column_to_s(@score)),
+           escape(column_to_s(@strand)),
+           escape(column_to_s(@frame)),
+           attributes_to_s(@attributes)
+          ].join("\t") + "\n"
         end
+
+        # private methods for escaping characters
+        module Escape
+          # unsafe characters to be escaped for normal columns
+          UNSAFE = /[^-_.!~*'()a-zA-Z\d\/?:@+$\[\] "\x80-\xfd><;=,]/n
+
+          # unsafe characters to be escaped for seqid columns
+          # and target_id of the "Target" attribute
+          UNSAFE_SEQID = /[^-a-zA-Z0-9.:^*$@!+_?|]/n
+
+          # unsafe characters to be escaped for attribute columns
+          UNSAFE_ATTRIBUTE = /[^-_.!~*'()a-zA-Z\d\/?:@+$\[\] "\x80-\xfd><]/n
+
+          private
+
+          # If str is empty, returns '.'. Otherwise, returns str.
+          def column_to_s(str)
+            str = str.to_s
+            str.empty? ? '.' : str
+          end
+
+          # Return the string corresponding to these characters unescaped
+          def unescape(string)
+            URI.unescape(string)
+          end
+
+          # Escape a column according to the specification at
+          # http://song.sourceforge.net/gff3.shtml.
+          def escape(string)
+            URI.escape(string, UNSAFE)
+          end
+
+          # Escape seqid column according to the specification at
+          # http://song.sourceforge.net/gff3.shtml.
+          def escape_seqid(string)
+            URI.escape(string, UNSAFE_SEQID)
+          end
         
-        def parse_attributes(attributes)
+          # Escape attribute according to the specification at
+          # http://song.sourceforge.net/gff3.shtml.
+          # In addition to the normal escape rule, the following characters
+          # are escaped: ",=;".
+          # Returns the string corresponding to these characters escaped.
+          def escape_attribute(string)
+            URI.escape(string, UNSAFE_ATTRIBUTE)
+          end
+        end #module Escape
+
+        include Escape
+
+        # Bio:GFF::GFF3::Record::Target is a class to store
+        # data of "Target" attribute.
+        class Target
+          include Record::Escape
+
+          # Creates a new object.
+          def initialize(target_id, start, endpos, strand = nil)
+            @target_id = target_id
+            @start = start ? start.to_i : nil
+            @end = endpos ? endpos.to_i : nil
+            @strand = strand
+          end
+
+          # target ID
+          attr_accessor :target_id
+
+          # start position
+          attr_accessor :start
+
+          # end position
+          attr_accessor :end
+
+          # strand (optional). Normally, "+" or "-", or nil.
+          attr_accessor :strand
+
+          # parses "target_id start end [strand]"-style string
+          # (for example, "ABC789 123 456 +")
+          # and creates a new Target object.
+          #
+          def self.parse(str)
+            target_id, start, endpos, strand =
+              str.split(/ +/, 4).collect { |x| URI.unescape(x) }
+            self.new(target_id, start, endpos, strand)
+          end
+
+          # returns a string
+          def to_s
+            tid = escape_seqid(column_to_s(@target_id))
+            st = escape_attribute(column_to_s(@start))
+            ed = escape_attribute(column_to_s(@end))
+            strnd = escape_attribute(@strand.to_s)
+            strnd = " " + strnd unless strnd.empty?
+            "#{tid} #{st} #{ed}#{strnd}"
+          end
+
+          # Returns true if self == other. Otherwise, returns false.
+          def ==(other)
+            if other.class == self.class and
+                other.target_id == self.target_id and
+                other.start == self.start and
+                other.end == self.end and
+                other.strand == self.strand then
+              true
+            else
+              false
+            end
+          end
+        end #class Target
+        
+        private
+        def parse_attributes(string)
           hash = Hash.new
-          attributes.split(';').each do |pair|
+          return hash if !string or string == '.'
+          string.split(';').each do |pair|
             key, value = pair.split('=', 2)
-            key2 = unescape(key)
-            value2 = unescape(value)
-            hash[key2] = value2
+            key = unescape(key)
+            values = value.to_s.split(',')
+            case key
+            when 'Target'
+              values.collect! { |v| Target.parse(v) }
+            else
+              values.collect! { |v| unescape(v) }
+            end
+            hash[key] = (values.size <= 1 ? values[0] : values)
           end
           return hash
         end # method parse_attributes
+
+        # Priority of attributes when output. 
+        # Smaller value, high priority.
+        # Default value for unlisted attribute is 999.
+        ATTRIBUTES_PRIORITY = {
+          'ID'            => 0,
+          'Name'          => 1,
+          'Alias'         => 2,
+          'Parent'        => 3,
+          'Target'        => 4,
+          'Gap'           => 5,
+          'Derives_from'  => 6,
+          'Note'          => 7,
+          'Dbxref'        => 8,
+          'Ontology_term' => 9
+        }
+        ATTRIBUTES_PRIORITY.default = 999
         
-        protected
-        # Return the attributes as a string as it appears at the end of a GFF3 line
-        def attributes_to_s
-          return '' if !@attributes
-          @attributes.collect{ |key,value|
-            "#{escape(key)}=#{escape(value)}"
-          }.join(';')
-        end
-        
-        # According to the specification at
-        # http://song.sourceforge.net/gff3.shtml
-        # URL escaping rules are used for tags or values containing the following characters: ",=;".
-        # Return the string corresponding to these characters unescaped
-        def unescape(string)
-          # translate each of the characters
-          ESCAPED_CHARACTERS.each do |char|
-            string.gsub! CGI.escape(char), char
+        # Return the attributes as a string as it appears at the end of
+        # a GFF3 line
+        def attributes_to_s(attr)
+          return '' unless attr
+          keys = attr.keys.sort! do |x, y|
+            z = ATTRIBUTES_PRIORITY[x] <=> ATTRIBUTES_PRIORITY[y]
+            z != 0 ? z : x <=> y
           end
-          return string
+          keys.collect do |key|
+            values = attr[key]
+            unless values.kind_of?(Array) then
+              values = [ values ]
+            end
+            val = values.collect do |v|
+              if v.kind_of?(Target) then
+                v.to_s
+              else
+                escape_attribute(v.to_s)
+              end
+            end.join(',')
+            "#{escape_attribute(key)}=#{val}"
+          end.join(';')
         end
-        
-        # According to the specification at
-        # http://song.sourceforge.net/gff3.shtml
-        # URL escaping rules are used for tags or values containing the following characters: ",=;".
-        # Return the string corresponding to these characters escaped.
-        def escape(string)
-          ESCAPED_CHARACTERS.each do |char|
-            string.gsub! char, CGI.escape(char)
-          end
-          return string
-        end
+
       end # class GFF3::Record
     end #class GFF3
     
