108635859c9747a181b68ac9c36000325f199e1a
Naohisa Goto <ngoto@gen-info.osaka-u.ac.jp>
2008-07-05 21:50:22 +0900

    test added and modified following changes in lib/bio/db/gff.rb
    Test added and modified To follow modification of lib/bio/db/gff.rb
    (commit id: a37910ba80ea0ebe3876970e7396285034c602f5).

--- test/unit/bio/db/test_gff.rb
+++ test/unit/bio/db/test_gff.rb
@@ -14,21 +14,11 @@ $:.unshift(libpath) unless $:.include?(libpath)
 require 'test/unit'
 require 'bio/db/gff'
 
-
-# subclass of the record class so that protected methods
-# can be tested singly
-class TestGFF3RecordSubclass<Bio::GFF::GFF3::Record
-  def test_unescape(string)
-    unescape(string)
-  end
-end
-
-
 module Bio
   class TestGFF < Test::Unit::TestCase
     
     def setup
-      data = <<END
+      data = <<END_OF_DATA
 I	sgd	CEN	151453	151591	.	+	.	CEN "CEN1" ; Note "CEN1\; Chromosome I Centromere"
 I	sgd	gene	147591	151163	.	-	.	Gene "TFC3" ; Note "transcription factor tau (TFIIIC) subunit 138"
 I	sgd	gene	147591	151163	.	-	.	Gene "FUN24" ; Note "transcription factor tau (TFIIIC) subunit 138"
@@ -37,7 +27,7 @@ I	sgd	ORF	147591	151163	.	-	.	ORF "YAL001C" ; Note "TFC3\; transcription factor 
 I	sgd	gene	143998	147528	.	+	.	Gene "VPS8" ; Note "Vps8p is a membrane-associated hydrophilic protein which contains a C-terminal cysteine-rich region that conforms to the H2 variant of the RING finger Zn2+ binding motif."
 I	sgd	gene	143998	147528	.	+	.	Gene "FUN15" ; Note "Vps8p is a membrane-associated hydrophilic protein which contains a C-terminal cysteine-rich region that conforms to the H2 variant of the RING finger Zn2+ binding motif."
 I	sgd	gene	143998	147528	.	+	.	Gene "VPT8" ; Note "Vps8p is a membrane-associated hydrophilic protein which contains a C-terminal cysteine-rich region that conforms to the H2 variant of the RING finger Zn2+ binding motif."
-END
+END_OF_DATA
       @obj = Bio::GFF.new(data)
     end
 
@@ -69,9 +59,9 @@ END
   class TestGFFRecord < Test::Unit::TestCase
     
     def setup
-      data =<<END
+      data =<<END_OF_DATA
 I	sgd	gene	151453	151591	.	+	.	Gene "CEN1" ; Note "Chromosome I Centromere"
-END
+END_OF_DATA
       @obj = Bio::GFF::Record.new(data)
     end
 
@@ -118,92 +108,6 @@ END
 
   end # class TestGFFRecord
   
-  
-  class TestGFF3Record < Test::Unit::TestCase
-    
-    def setup
-      data =<<END
-I	sgd	gene	151453	151591	.	+	.	Gene="CEN1";Note="Chromosome I Centromere"
-END
-      @obj = Bio::GFF::GFF3.new(data).records[0]
-    end
-    
-    def test_attributes
-      at = {"Note"=>'"Chromosome I Centromere"', "Gene"=>'"CEN1"'}
-      assert_equal(at, @obj.attributes)
-    end
-    
-    def test_none
-      # test blank with tab
-      data =<<END
-I	sgd	gene	151453	151591	.	+	.	
-END
-      obj = Bio::GFF::GFF3.new(data).records[0]
-      assert_nil obj.attributes
-      
-      # test blank with no tab at end
-      data =<<END
-I	sgd	gene	151453	151591	.	+	.
-END
-      obj = Bio::GFF::GFF3.new(data).records[0]
-      assert_nil obj.attributes
-    end
-    
-    def test_one
-      data =<<END
-I	sgd	gene	151453	151591	.	+	.	Gene="CEN1"
-END
-      obj = Bio::GFF::GFF3.new(data).records[0]
-      at = {"Gene"=>'"CEN1"'}
-      assert_equal  at, obj.attributes
-    end
-    
-    def test_with_escaped_sequence
-      data =<<END
-I	sgd	gene	151453	151591	.	+	.	Gene="CEN1%3Boh";Note="Chromosome I Centromere"
-END
-      obj = Bio::GFF::GFF3.new(data).records[0]
-      at = {"Note"=>'"Chromosome I Centromere"', "Gene"=>'"CEN1;oh"'}
-      assert_equal(at, obj.attributes)      
-    end
-    
-    def test_with_three
-      data =<<END
-I	sgd	gene	151453	151591	.	+	.	Hi=Bye;Gene="CEN1";Note="Chromosome I Centromere"
-END
-      obj = Bio::GFF::GFF3.new(data).records[0]
-      at = {"Note"=>'"Chromosome I Centromere"', "Gene"=>'"CEN1"', 'Hi'=>'Bye'}
-      assert_equal(at, obj.attributes)      
-    end
-    
-    
-    def test_unescape
-      record = TestGFF3RecordSubclass.new('I	sgd	gene	151453	151591	.	+	.	Hi=Bye;Gene="CEN1";Note="Chromosome I Centromere"')
-      assert_equal ';', record.test_unescape('%3B')
-    end
-    
-    
-    def test_record_to_s
-      line = 'I	sgd	gene	151453	151591	.	+	.	Hi=Bye;Note="Chromosome I Centromere";Gene="CEN1"'
-      record = TestGFF3RecordSubclass.new(line)
-      assert_equal line, record.to_s, '3 attributes'
-      
-      line = 'I	sgd	gene	151453	151591	.	+	.	'
-      record = TestGFF3RecordSubclass.new(line)
-      assert_equal line, record.to_s, 'no attributes'
-    end
-    
-    
-    def test_gff3_records_to_s
-      data =<<END
-I	sgd	gene	151453	151591	.	+	.	Note="Chromosome I Centromere;Gene="CEN1%3Boh""
-I	sgd	gene	151453	151591	.	+	.	
-END
-      gff3 = Bio::GFF::GFF3.new(data)
-      assert_equal data, gff3.to_s
-    end
-  end
-  
   class TestGFFRecordConstruct < Test::Unit::TestCase
 
     def setup
@@ -219,6 +123,204 @@ END
     end
 
   end # class TestGFFRecordConstruct
-end
+  
+  class TestGFF3Record < Test::Unit::TestCase
+    
+    def setup
+      data =<<END_OF_DATA
+chrI	SGD	centromere	151467	151584	.	+	.	ID=CEN1;Name=CEN1;gene=CEN1;Alias=CEN1,test%3B0001;Note=Chromosome%20I%20centromere;dbxref=SGD:S000006463;Target=test%2002 123 456 -,test%2C03 159 314;memo%3Dtest%3Battr=99.9%25%09match
+END_OF_DATA
+      @obj = Bio::GFF::GFF3::Record.new(data)
+    end
+
+    def test_seqname
+      assert_equal('chrI', @obj.seqname)
+    end
+
+    def test_source
+      assert_equal('SGD', @obj.source)
+    end
+
+    def test_feature
+      assert_equal('centromere', @obj.feature)
+    end
+
+    def test_start
+      assert_equal(151467, @obj.start)
+    end
+
+    def test_end
+      assert_equal(151584, @obj.end)
+    end
+
+    def test_score
+      assert_equal(nil, @obj.score)
+    end
+
+    def test_strand
+      assert_equal('+', @obj.strand)
+    end
+
+    def test_frame
+      assert_equal(nil, @obj.frame)
+    end
+
+    def test_attributes
+      attr = {
+        'ID'     => 'CEN1',
+        'Name'   => 'CEN1',
+        'gene'   => 'CEN1',
+        'Alias'  => [ 'CEN1', 'test;0001' ],
+        'Note'   => 'Chromosome I centromere',
+        'dbxref' => 'SGD:S000006463',
+        'Target' =>
+        [ Bio::GFF::GFF3::Record::Target.new('test 02', 123, 456, '-'),
+          Bio::GFF::GFF3::Record::Target.new('test,03', 159, 314)
+        ],
+        'memo=test;attr' => "99.9%\tmatch",
+      }
+      assert_equal(attr, @obj.attributes)
+    end
+
+    def test_to_s
+      str = <<END_OF_STR
+chrI	SGD	centromere	151467	151584	.	+	.	ID=CEN1;Name=CEN1;Alias=CEN1,test%3B0001;Target=test%2002 123 456 -,test%2C03 159 314;Note=Chromosome I centromere;dbxref=SGD:S000006463;gene=CEN1;memo%3Dtest%3Battr=99.9%25%09match
+END_OF_STR
+      assert_equal(str, @obj.to_s)
+    end
+  end #class TestGFF3Record
+
+  class TestGFF3RecordMisc < Test::Unit::TestCase
+    def test_attributes_none
+      # test blank with tab
+      data =<<END_OF_DATA
+I	sgd	gene	151453	151591	.	+	.	
+END_OF_DATA
+      obj = Bio::GFF::GFF3::Record.new(data)
+      assert_equal({}, obj.attributes)
+      
+      # test blank with no tab at end
+      data =<<END_OF_DATA
+I	sgd	gene	151453	151591	.	+	.
+END_OF_DATA
+      obj = Bio::GFF::GFF3::Record.new(data)
+      assert_equal({}, obj.attributes)
+    end
+    
+    def test_attributes_one
+      data =<<END_OF_DATA
+I	sgd	gene	151453	151591	.	+	.	gene=CEN1
+END_OF_DATA
+      obj = Bio::GFF::GFF3::Record.new(data)
+      at = {"gene"=>'CEN1'}
+      assert_equal(at, obj.attributes)
+    end
+    
+    def test_attributes_with_escaping
+      data =<<END_OF_DATA
+I	sgd	gene	151453	151591	.	+	.	gene="CEN1%3Boh";Note=Chromosome I Centromere
+END_OF_DATA
+      obj = Bio::GFF::GFF3::Record.new(data)
+      at = { "Note" => 'Chromosome I Centromere', "gene" => '"CEN1;oh"' }
+      assert_equal(at, obj.attributes)      
+    end
+    
+    def test_attributes_three
+      data =<<END_OF_DATA
+I	sgd	gene	151453	151591	.	+	.	hi=Bye;gene=CEN1;Note=Chromosome I Centromere
+END_OF_DATA
+      obj = Bio::GFF::GFF3::Record.new(data)
+      at = {"Note"=>'Chromosome I Centromere', "gene"=>'CEN1', 'hi'=>'Bye'}
+      assert_equal(at, obj.attributes)      
+    end
+  end #class TestGFF3RecordMisc
+
+  class TestGFF3RecordEscape < Test::Unit::TestCase
+    def setup
+      @obj = Object.new.extend(Bio::GFF::GFF3::Record::Escape)
+      @str = "A>B\tC=100%;d=e,f,g"
+    end
+
+    def test_escape
+      str = @str
+      assert_equal('A>B%09C=100%25;d=e,f,g',
+                   @obj.instance_eval { escape(str) })
+    end
+
+    def test_escape_attribute
+      str = @str
+      assert_equal('A>B%09C%3D100%25%3Bd%3De%2Cf%2Cg',
+                   @obj.instance_eval { escape_attribute(str) })
+    end
+
+    def test_escape_seqid
+      str = @str
+      assert_equal('A%3EB%09C%3D100%25%3Bd%3De%2Cf%2Cg',
+                   @obj.instance_eval { escape_seqid(str) })
+    end
+
+    def test_unescape
+      escaped_str = 'A%3EB%09C%3D100%25%3Bd%3De%2Cf%2Cg'
+      assert_equal(@str,
+                   @obj.instance_eval {
+                     unescape(escaped_str) })
+    end
+  end #class TestGFF3RecordEscape
+
+  class TestGFF3RecordTarget < Test::Unit::TestCase
+
+    def setup
+      @target =
+        [ Bio::GFF::GFF3::Record::Target.new('ABCD1234', 123, 456, '+'),
+          Bio::GFF::GFF3::Record::Target.new(">X Y=Z;P%,Q\tR", 78, 90),
+          Bio::GFF::GFF3::Record::Target.new(nil, nil, nil),
+        ]
+    end
+
+    def test_parse
+      strings = 
+        [ 'ABCD1234 123 456 +',
+          '%3EX%20Y%3DZ%3BP%25%2CQ%09R 78 90',
+          ''
+        ]
+      @target.each do |target|
+        str = strings.shift
+        assert_equal(target, Bio::GFF::GFF3::Record::Target.parse(str))
+      end
+    end
+
+    def test_target_id
+      assert_equal('ABCD1234', @target[0].target_id)
+      assert_equal(">X Y=Z;P%,Q\tR", @target[1].target_id)
+      assert_equal(nil, @target[2].target_id)
+    end
+
+    def test_start
+      assert_equal(123, @target[0].start)
+      assert_equal(78, @target[1].start)
+      assert_nil(@target[2].start)
+    end
+
+    def test_end
+      assert_equal(456, @target[0].end)
+      assert_equal(90, @target[1].end)
+      assert_nil(@target[2].end)
+    end
+
+    def test_strand
+      assert_equal('+', @target[0].strand)
+      assert_nil(@target[1].strand)
+      assert_nil(@target[2].strand)
+    end
+
+    def test_to_s
+      assert_equal('ABCD1234 123 456 +', @target[0].to_s)
+      assert_equal('%3EX%20Y%3DZ%3BP%25%2CQ%09R 78 90', @target[1].to_s)
+      assert_equal('. . .', @target[2].to_s)
+    end
+
+  end #class TestGFF3RecordTarget
+
+end #module Bio
 
 
