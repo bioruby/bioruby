e9c4705070fa818ec6be9079ed1129a645142ed0
Michael Barton <mail@michaelbarton.me.uk>
2008-07-28 15:20:01 +0100

    Added test for production of expected codeml options

--- test/unit/bio/appl/codeml/test_codeml.rb
+++ test/unit/bio/appl/codeml/test_codeml.rb
@@ -12,10 +12,11 @@ $:.unshift(libpath) unless $:.include?(libpath)
 require 'test/unit'
 require 'bio/appl/codeml'
 
+BIORUBY_ROOT  = Pathname.new(File.join(File.dirname(__FILE__), ['..'] * 5)).cleanpath.to_s
+TEST_DATA = Pathname.new(File.join(BIORUBY_ROOT, 'test', 'data', 'codeml')).cleanpath.to_s
+
 module Bio
   class TestCodemlData
-    BIORUBY_ROOT  = Pathname.new(File.join(File.dirname(__FILE__), ['..'] * 5)).cleanpath.to_s
-    TEST_DATA = Pathname.new(File.join(BIORUBY_ROOT, 'test', 'data', 'codeml')).cleanpath.to_s
 
     def self.generate_config_file
       test_config = Tempfile.new('codeml_config').path
@@ -37,6 +38,16 @@ module Bio
 
     def test_config_file_generated
       assert_not_nil(File.size?(EXAMPLE_CONFIG))
+    end
+
+    def test_expected_options_set_in_config_file
+      produced_config = File.open(EXAMPLE_CONFIG).inject(Hash.new) do |hash,line|
+        hash.store(*line.strip.split(' = '))
+        hash
+      end
+      assert_equal(produced_config['seqfile'], TEST_DATA + '/abglobin.aa')
+      assert_equal(produced_config['fix_kappa'], '1')
+      assert_equal(produced_config['model'], '1')
     end
 
   end
