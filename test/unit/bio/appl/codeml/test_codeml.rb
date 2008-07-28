a7c5f66b09b05dedf5a35cc5758f41ef9a2993ad
Michael Barton <mail@michaelbarton.me.uk>
2008-07-28 15:07:51 +0100

    Added test for generation of config file

--- test/unit/bio/appl/codeml/test_codeml.rb
+++ test/unit/bio/appl/codeml/test_codeml.rb
@@ -13,8 +13,31 @@ require 'test/unit'
 require 'bio/appl/codeml'
 
 module Bio
-  class TestCodeML
+  class TestCodemlData
     BIORUBY_ROOT  = Pathname.new(File.join(File.dirname(__FILE__), ['..'] * 5)).cleanpath.to_s
     TEST_DATA = Pathname.new(File.join(BIORUBY_ROOT, 'test', 'data', 'codeml')).cleanpath.to_s
+
+    def self.generate_config_file
+      test_config = Tempfile.new('codeml_config').path
+      Bio::CodeML.create_config_file({
+        :model       => 1,
+        :fix_kappa   => 1,
+        :aaRatefile  => TEST_DATA + '/wag.dat',
+        :seqfile     => TEST_DATA + '/abglobin.aa',
+        :treefile    => TEST_DATA + '/abglobin.trees',
+        :outfile     => Tempfile.new('codeml_test').path,
+      },test_config)
+      test_config
+    end
+  end
+
+  class TestCodemlConfigGeneration < Test::Unit::TestCase
+
+    EXAMPLE_CONFIG = TestCodemlData.generate_config_file
+
+    def test_config_file_generated
+      assert_not_nil(File.size?(EXAMPLE_CONFIG))
+    end
+
   end
 end
