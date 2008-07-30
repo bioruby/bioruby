878839f18aa1a65de5063d8a48e9b813cdddc88d
Michael Barton <mail@michaelbarton.me.uk>
2008-07-30 14:58:39 +0100

    Created codeml report parser

---
+++ lib/bio/appl/codeml/report.rb
@@ -0,0 +1,30 @@
+module Bio
+  class CodeML
+    class Report
+
+      attr_reader :tree_log_likelihood, :tree_length, :alpha
+
+      def initialize(codeml_report)
+        @tree_log_likelihood = pull_tree_log_likelihood(codeml_report)
+        @tree_length = pull_tree_length(codeml_report)
+        @alpha = pull_alpha(codeml_report)
+      end
+
+      private
+
+      def pull_tree_log_likelihood(text)
+        text[/lnL\(.+\):\s+(-?\d+(\.\d+)?)/,1].to_f
+      end
+
+      
+      def pull_tree_length(text)
+        text[/tree length\s+=\s+ (-?\d+(\.\d+)?)/,1].to_f
+      end
+
+      def pull_alpha(text)
+        text[/alpha .+ =\s+(-?\d+(\.\d+)?)/,1].to_f
+      end
+
+    end # End Report
+  end # End CodeML
+end # End Bio
