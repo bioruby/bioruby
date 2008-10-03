81eb2c246d01790db72f0b08929bec5d862c959e
Ra <raoul.bonnal@itb.cnr.it>
2008-10-03 11:37:37 +0200

    class cleaned

--- lib/bio/io/biosql/ontology.rb
+++ lib/bio/io/biosql/ontology.rb
@@ -2,8 +2,6 @@
  module Bio
     class SQL 
 		    class Ontology < DummyBase
-#delete		    	set_primary_key "ontology_id"
-		      set_sequence_name "ontology_pk_seq"
 		      has_many :terms, :class_name => "Term"
 		      has_many :term_paths, :class_name => "TermPath"
 		      has_many :term_relationships, :class_name => "TermRelationship"
