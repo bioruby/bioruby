eba61ba670c591f58866b37ababc4acac0cc7883
Ra <raoul.bonnal@itb.cnr.it>
2008-10-09 18:57:06 +0200

    Fix: compisite primary keys :bioentry_id, :reference_id, :rank

--- lib/bio/io/biosql/bioentry_reference.rb
+++ lib/bio/io/biosql/bioentry_reference.rb
@@ -1,7 +1,7 @@
 module Bio
     class SQL
 			class BioentryReference < DummyBase
-				set_primary_key :bioentry_reference_id
+				set_primary_keys :bioentry_id, :reference_id, :rank
 				belongs_to :bioentry, :class_name => "Bioentry"
 				belongs_to :reference , :class_name => "Reference"
 			end		
