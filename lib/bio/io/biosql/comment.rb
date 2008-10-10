54976693350ab0512cecf946999c2868b9e88007
Ra <raoul.bonnal@itb.cnr.it>
2008-10-10 14:41:52 +0200

    Cleaned

--- lib/bio/io/biosql/comment.rb
+++ lib/bio/io/biosql/comment.rb
@@ -1,8 +1,6 @@
  module Bio
     class SQL
 			class Comment < DummyBase
-#delete				set_primary_key "comment_id"
-			      set_sequence_name "comment_pk_seq"
 			      belongs_to :bioentry, :class_name => "Bioentry"
 			end
 		end #SQL
