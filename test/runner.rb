5df2a9dc0642d4f1e9a4398d6af908780d622a6e
Naohisa Goto <ng@bioruby.org>
2009-01-09 22:38:58 +0900

    Ruby 1.9.1 support: using alternatives if no Test::Unit::AutoRunner

--- test/runner.rb
+++ test/runner.rb
@@ -6,9 +6,18 @@ require 'pathname'
 bioruby_libpath = Pathname.new(File.join(File.dirname(__FILE__), ['..'], 'lib')).cleanpath.to_s
 $:.unshift(bioruby_libpath) unless $:.include?(bioruby_libpath)
 
-if RUBY_VERSION > "1.8.2"
+if !defined?(Test::Unit::AutoRunner) then
+  # Ruby 1.9.1 does not have Test::Unit::AutoRunner
+  Test::Unit.setup_argv do |files|
+    [ File.dirname($0) ]
+  end
+  # tests called when exiting the program
+
+elsif RUBY_VERSION > "1.8.2"
+  # current Test::Unit -- Ruby 1.8.3 or later
   exit Test::Unit::AutoRunner.run(true, File.dirname($0))
 else
+  # old Test::Unit -- Ruby 1.8.2 or older
   exit Test::Unit::AutoRunner.run(false, File.dirname($0))
 end
 
