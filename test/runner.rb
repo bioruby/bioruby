475ac6a6b38e8df30de3d9bf4c7e810759ab023d
Naohisa Goto <ng@bioruby.org>
2009-02-06 01:24:44 +0900

    Added workaroud for test-unit-2.0.x

--- test/runner.rb
+++ test/runner.rb
@@ -13,6 +13,13 @@ if !defined?(Test::Unit::AutoRunner) then
   end
   # tests called when exiting the program
 
+elsif defined?(Test::Unit::Color) then
+  # workaround for test-unit-2.0.x
+  r = Test::Unit::AutoRunner.new(true)
+  r.to_run.push File.dirname($0)
+  r.process_args(ARGV)
+  exit r.run
+
 elsif RUBY_VERSION > "1.8.2"
   # current Test::Unit -- Ruby 1.8.3 or later
   exit Test::Unit::AutoRunner.run(true, File.dirname($0))
