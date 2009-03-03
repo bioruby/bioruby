89f64af363d0b204e50ea71924909724d56bccc4
Ra <ilpuccio.febo@gmail.com>
2009-03-03 21:55:24 +0100

    class SeqfeatureQualifierValue: find function deleted, wrong here.

--- lib/bio/io/biosql/ar-biosql.rb
+++ lib/bio/io/biosql/ar-biosql.rb
@@ -153,15 +153,6 @@ module Bio
       set_sequence_name nil
       belongs_to :seqfeature
       belongs_to :term, :class_name => "Term"
-      
-      def self.find_cluster(query)
-        term_note= Term.find_by_name('note')
-        find(:all, :conditions =>["value like ? and term_id = ?", "cl:#{query}", term_note])
-      end
-      def self.find_cluster_info(query)
-        term_note= Term.find_by_name('note')
-        find(:all, :conditions =>["value like ? and term_id = ?", "cli:#{query}", term_note])
-      end
     end		
     class Seqfeature <DummyBase  
       set_sequence_name "seqfeature_pk_seq"
