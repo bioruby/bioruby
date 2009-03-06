c9a980877c9222e05aa0d9163ba51aa2c77a7146
Ra <ilpuccio.febo@gmail.com>
2009-03-06 13:52:06 +0100

    Fixed: date_modified, the code is moved to Bio::SQL::Sequence#bioentry_qualifier_anchor#method_reader. It's most an exercise of style than good programming. date_modifier reader should be a method apart.

--- lib/bio/db/biosql/biosql_to_biosequence.rb
+++ lib/bio/db/biosql/biosql_to_biosequence.rb
@@ -45,9 +45,10 @@ module Bio::Sequence::Adapter::BioSQL
 
   def_biosequence_adapter :date_created
 
-  def_biosequence_adapter :date_modified do |bs| 
-      Date.parse(bs.date_modified.to_s).strftime("%d-%b-%Y").upcase
-     end
+  def_biosequence_adapter :date_modified
+  #do |bs|
+   #   Date.parse(bs.date_modified.to_s).strftime("%d-%b-%Y").upcase
+    # end
 
   def_biosequence_adapter :division
 
