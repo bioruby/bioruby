38453c357ce09e93c550cb00fcafc7fe493ee720
Anthony Underwood <email2ants@gmail.com>
2009-08-07 12:07:27 +0100

    finalising chromatogram functionality

--- lib/bio/db/chromatogram/chromatogram_to_biosequence.rb
+++ lib/bio/db/chromatogram/chromatogram_to_biosequence.rb
@@ -1,7 +1,7 @@
 #
 # = bio/db/chromatogram/chromatogram_to_biosequence.rb - Bio::Chromatogram to Bio::Sequence adapter module
 #
-# Copyright::   Copyright (C) 2008
+# Copyright::   Copyright (C) 2009
 #               Anthony Underwood <email2ants@gmail.com>
 # License::     The Ruby License
 #
@@ -29,35 +29,7 @@ module Bio::Sequence::Adapter::Chromatogram
     orig.version
   end
   
-  # # secondary accessions
-  # def_biosequence_adapter :secondary_accessions do |orig|
-  #   orig.identifiers.accessions[1..-1]
-  # end
-  # 
-  # # entry_id
-  # def_biosequence_adapter :entry_id do |orig|
-  #   orig.identifiers.locus or orig.identifiers.accessions.first or
-  #     orig.identifiers.entry_id
-  # end
-  # 
-  # # NCBI GI is stored on other_seqids
-  # def_biosequence_adapter :other_seqids do |orig|
-  #   other = []
-  #   if orig.identifiers.gi then
-  #     other.push Bio::Sequence::DBLink.new('GI', orig.identifiers.gi)
-  #   end
-  #   other.empty? ? nil : other
-  # end
-  # 
-  # # definition
-  # def_biosequence_adapter :definition do |orig|
-  #   if orig.identifiers.accessions.empty? and
-  #       !(orig.identifiers.gi) then
-  #     orig.definition
-  #   else
-  #     orig.identifiers.description
-  #   end
-  # end
+
 
 end #module Bio::Sequence::Adapter::Chromatogram
 
