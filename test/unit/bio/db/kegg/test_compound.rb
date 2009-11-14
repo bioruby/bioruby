1199330eab95b8434303e92c5f792818e96db814
Kozo Nishida <kozo-ni@cg05.naist.jp>
2009-11-14 09:03:50 +0900

    Newly added unit tests for Bio::KEGG::COMPOUND and Bio::KEGG::REACTION
     * Newly added unit tests for Bio::KEGG::COMPOUND and
       Bio::KEGG::REACTION with test data. (Note that this is a
       combination of several commits made by Kozo Nishida,
       merged from git://github.com/kozo2/bioruby.git ).

---
+++ test/unit/bio/db/kegg/test_compound.rb
@@ -0,0 +1,49 @@
+#
+# test/unit/bio/db/kegg/test_compound.rb - Unit test for Bio::KEGG::COMPOUND
+#
+# Copyright::  Copyright (C) 2009 Kozo Nishida <kozo-ni@is.naist.jp>
+# License::    The Ruby License
+
+require 'pathname'
+libpath = Pathname.new(File.join(File.dirname(__FILE__), ['..'] * 5, 'lib')).cleanpath.to_s
+$:.unshift(libpath) unless $:.include?(libpath)
+
+require 'test/unit'
+require 'bio/db/kegg/compound'
+
+module Bio
+  class TestCompound < Test::Unit::TestCase
+
+    def setup
+      bioruby_root = Pathname.new(File.join(File.dirname(__FILE__), ['..'] * 5)).cleanpath.to_s
+      testdata_kegg = Pathname.new(File.join(bioruby_root, 'test', 'data', 'KEGG')).cleanpath.to_s
+      entry = File.read(File.join(testdata_kegg, "C00025.compound"))
+      @obj = Bio::KEGG::COMPOUND.new(entry)
+    end
+
+    def test_entry_id
+      assert_equal('C00025', @obj.entry_id)
+    end
+
+    def test_name
+      assert_equal("L-Glutamate", @obj.name)
+    end
+
+    def test_names
+      assert_equal(["L-Glutamate", "L-Glutamic acid", "L-Glutaminic acid", "Glutamate"], @obj.names)
+    end
+
+    def test_formula
+      assert_equal("C5H9NO4", @obj.formula)
+    end
+
+    def test_mass
+      assert_equal(147.0532, @obj.mass)
+    end
+
+    def test_dblinks
+      assert_equal([{"id"=>"56-86-0", "db"=>"CAS"}, {"id"=>"3327", "db"=>"PubChem"}, {"id"=>"16015", "db"=>"ChEBI"}, {"id"=>"C00001358", "db"=>"KNApSAcK"}, {"id"=>"GLU", "db"=>"PDB-CCD"}, {"id"=>"B00007", "db"=>"3DMET"}, {"id"=>"J9.171E", "db"=>"NIKKAJI"}], @obj.dblinks)
+    end
+
+  end
+end
---
+++ test/unit/bio/db/kegg/test_reaction.rb
@@ -0,0 +1,57 @@
+#
+# test/unit/bio/db/kegg/test_reaction.rb - Unit test for Bio::KEGG::REACTION
+#
+# Copyright::  Copyright (C) 2009 Kozo Nishida <kozo-ni@is.naist.jp>
+# License::    The Ruby License
+
+require 'pathname'
+libpath = Pathname.new(File.join(File.dirname(__FILE__), ['..'] * 5, 'lib')).cleanpath.to_s
+$:.unshift(libpath) unless $:.include?(libpath)
+
+require 'test/unit'
+require 'bio/db/kegg/reaction'
+
+module Bio
+  class TestReaction < Test::Unit::TestCase
+
+    def setup
+      bioruby_root = Pathname.new(File.join(File.dirname(__FILE__), ['..'] * 5)).cleanpath.to_s
+      testdata_kegg = Pathname.new(File.join(bioruby_root, 'test', 'data', 'KEGG')).cleanpath.to_s
+      entry = File.read(File.join(testdata_kegg, "R00006.reaction"))
+      @obj = Bio::KEGG::REACTION.new(entry)
+    end
+
+    def test_entry_id
+      assert_equal('R00006', @obj.entry_id)
+    end
+
+    def test_name
+      assert_equal('pyruvate:pyruvate acetaldehydetransferase (decarboxylating); 2-acetolactate pyruvate-lyase (carboxylating)', @obj.name)
+    end
+
+    def test_definition
+      assert_equal('2-Acetolactate + CO2 <=> 2 Pyruvate', @obj.definition)
+    end
+
+    def test_equation
+      assert_equal('C00900 + C00011 <=> 2 C00022', @obj.equation)
+    end
+
+    def test_rpairs
+      assert_equal([{"name"=>"C00022_C00900", "type"=>"main", "entry"=>"RP00440"}, {"name"=>"C00011_C00022", "type"=>"leave", "entry"=>"RP05698"}, {"name"=>"C00022_C00900", "type"=>"trans", "entry"=>"RP12733"}], @obj.rpairs)
+    end
+
+    def test_pathways
+      assert_equal([{"name"=>"Pantothenate and CoA biosynthesis", "entry"=>"rn00770"}], @obj.pathways)
+    end
+
+    def test_enzymes
+      assert_equal(["2.2.1.6"], @obj.enzymes)
+    end
+
+    def test_orthologies
+      assert_equal([{"entry"=>"K01652", "definition"=>"acetolactate synthase I/II/III large subunit [EC:2.2.1.6]"}, {"entry"=>"K01653", "definition"=>"acetolactate synthase I/III small subunit [EC:2.2.1.6]"}], @obj.orthologies)
+    end
+
+  end
+end
