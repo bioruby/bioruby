1d35e616ce411bf643ab6dcb7126a6e1aca1e186
Pjotr Prins <pjotr.public01@thebird.nl>
2010-01-02 17:04:56 +0100

    Codeml::Report Added new description and reference

--- lib/bio/appl/paml/codeml/report.rb
+++ lib/bio/appl/paml/codeml/report.rb
@@ -1,17 +1,11 @@
 #
 # = bio/appl/paml/codeml/report.rb - Codeml report parser
 #
-# Copyright::  Copyright (C) 2008 Michael D. Barton <mail@michaelbarton.me.uk>
+# Copyright::  Copyright (C) 2008-2010
+#              Michael D. Barton <mail@michaelbarton.me.uk>,
+#              Pjotr Prins <pjotr.prins@thebird.nl>
 #
 # License::    The Ruby License
-#
-# == Description
-#
-# This file contains a class that implement a simple interface to Codeml output file
-#
-# == References
-#
-# * http://abacus.gene.ucl.ac.uk/software/paml.html
 #
 
 require 'bio/appl/paml/codeml'
@@ -21,17 +15,102 @@ module Bio::PAML
 
     # == Description
     #
-    # A simple class for parsing codeml output.
+    # Run PAML codeml and get the results from the output file. The
+    # Codeml::Report object is returned by Bio::PAML::Codeml.query. For
+    # example
     #
-    # WARNING: This data is parsed using a regex from the output file, and
-    # so will take the first result found. If using multiple tree's, your
-    # milage may vary. See the source for the regular expressions.
+    #   codeml = Bio::PAML::Codeml.new('codeml', :runmode => 0, 
+    #       :RateAncestor => 1, :alpha => 0.5, :fix_alpha => 0)
+    #   result = codeml.query(alignment, tree)
     #
-    # require 'bio'
+    # where alignment and tree are Bioruby objects. This class assumes we have a
+    # buffer containing the output of codeml. 
     #
-    # report = Bio::PAML::Codeml::Report.new(File.open(codeml_output_file).read)
-    # report.gene_rate  # => Rate of gene evolution as defined be alpha
-    # report.tree_lengh # => Estimated phylogetic tree length
+    # == References
+    #
+    # Phylogenetic Analysis by Maximum Likelihood (PAML) is a package of
+    # programs for phylogenetic analyses of DNA or protein sequences using
+    # maximum likelihood. It is maintained and distributed for academic use
+    # free of charge by Ziheng Yang. Suggestion citation 
+    #
+    #   Yang, Z. 1997
+    #   PAML: a program package for phylogenetic analysis by maximum likelihood 
+    #   CABIOS 13:555-556 
+    #
+    # http://abacus.gene.ucl.ac.uk/software/paml.html
+    #
+    # == Examples
+    #
+    #   >> buf = File.read(File.join(TEST_DATA, 'output2.txt'))
+    #   >> c = Bio::PAML::Codeml::Report.new(buf)
+    #
+    #   >> c.model.size
+    #   => 2
+    #   >> c.model[0].name
+    #   => "M0"
+    # 
+    # Now fetch the results of the first model M0, and check its values
+    # 
+    #   >> m0 = c.model['M0']
+    #   >> m0.tree_length
+    #   => 9.00878
+    #   >> m0.lnL
+    #   => -28155.576740
+    #   >> m0.kappa
+    #   => 2.17796
+    # 
+    # Check the M3 and its specific values
+    #    
+    #   >> m3 = c.model['M3']
+    #   >> m3.lnL
+    #   => -30768.946749
+    #   >> m3.classes.size
+    #   => 3
+    #   >> m3.classes[0]
+    #   => ['p' => 0.69751, 'w' => 0.35313 ]
+    # 
+    # Next take the overall posterior analysis
+    # 
+    #   >> c.nb_sites.size
+    #   => 63
+    #   >> c.nb_sites[0].to_a
+    #   => [31,'L',1.0,2.895]
+    # 
+    # or by field
+    #
+    #   >> codon = c.nb_sites[0]
+    #   >> codon.position
+    #   => 31
+    #   >> codon.probability
+    #   => 1.0
+    #   >> codon.dN_dS
+    #   => 2.895
+    #
+    # with aliases
+    #
+    #   >> codon.p
+    #   => 1.0
+    #   >> codon.w
+    #   => 2.895
+    #
+    # The results of a single model (old style report parser)
+    #
+    #   >> buf = File.read(File.join(TEST_DATA, 'output.txt'))
+    #   >> c = Bio::PAML::Codeml::Report.new(buf)
+    #
+    #   >> c.tree_log_likelihood
+    #   => -1817.465211
+    #
+    #   >> c.tree_length
+    #   => 0.77902
+    #
+    #   >> c.alpha
+    #   => 0.58871
+    #
+    #   >> c.tree)
+    #   => "(((rabbit: 0.082889, rat: 0.187866): 0.038008, human: 0.055050): 0.033639, goat-cow: 0.096992, marsupial: 0.284574);"
+    #
+
     class Report < Bio::PAML::Common::Report
 
       attr_reader :tree_log_likelihood, :tree_length, :alpha, :tree
