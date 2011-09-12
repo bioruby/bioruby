414a6331f40fc99f554042e9a031689ea6d76da4
Naohisa Goto <ng@bioruby.org>
2011-09-12 20:54:06 +0900

    deleted rdoc.zsh which is obsolete and unused
     * Deleted rdoc.zsh which is obsolete and unused.
       To generate rdoc html, "rake rdoc" or "rake rerdoc".
       See "rake -T" for more information.

--- rdoc.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-#DIAGRAM='--diagram'
-WEBCVS='http://cvs.open-bio.org/cgi-bin/viewcvs/viewcvs.cgi'
-
-rdoc ${DIAGRAM} --op rdoc --inline-source \
-  --webcvs "${WEBCVS}/bioruby/\%s?cvsroot=bioruby" \
-  bin/*(.) lib/**/*.rb [A-Z]*(.) doc/*rd etc/bioinformatics/*(.)
