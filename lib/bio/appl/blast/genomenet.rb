c552aa3a6773139b14ae95e79e0fb43a2f91c6fb
Naohisa Goto <ng@bioruby.org>
2012-01-12 22:24:37 +0900

    Bug fix: GenomeNet BLAST server URI changed.
     * Bug fix: GenomeNet BLAST server URI changed.
       Reported by joaocardoso via GitHub.
       ( https://github.com/bioruby/bioruby/issues/44 )

--- lib/bio/appl/blast/genomenet.rb
+++ lib/bio/appl/blast/genomenet.rb
@@ -158,7 +158,8 @@ module Bio::Blast::Remote
       host = Host
       #host = "blast.genome.jp"
       #path = "/sit-bin/nph-blast"
-      path = "/sit-bin/blast" #2005.08.12
+      #path = "/sit-bin/blast" #2005.08.12
+      path = "/tools-bin/blast" #2012.01.12
 
       options = make_command_line_options
       opt = Bio::Blast::NCBIOptions.new(options)
