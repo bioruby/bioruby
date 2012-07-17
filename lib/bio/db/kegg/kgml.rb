e5478363ef6969ec14c4e09c2bd7c6d27c12cf5b
Naohisa Goto <ng@bioruby.org>
2012-07-17 22:23:28 +0900

    rdoc documentation for Bio::KEGG::KGML

--- lib/bio/db/kegg/kgml.rb
+++ lib/bio/db/kegg/kgml.rb
@@ -108,8 +108,18 @@ class KEGG
 #    end
 #  end
 #
+# === References
+#
+# * http://www.genome.jp/kegg/xml/docs/
+#
 class KGML
 
+  # Creates a new KGML object.
+  # 
+  # ---
+  # *Arguments*:
+  # * (required) _str_: String containing xml data
+  # *Returns*:: Bio::KEGG::KGML object
   def initialize(xml)
     dom = REXML::Document.new(xml)
     parse_root(dom)
@@ -117,30 +127,168 @@ class KGML
     parse_relation(dom)
     parse_reaction(dom)
   end
-  attr_reader :name, :org, :number, :title, :image, :link
-  attr_accessor :entries, :relations, :reactions
 
+  # KEGG-style ID string of this pathway map (String)
+  attr_reader :name
+
+  # "ko" (KEGG Orthology), "ec" (KEGG ENZYME),
+  # or the KEGG 3-letter organism code (String)
+  attr_reader :org
+
+  # map number (String)
+  attr_reader :number
+
+  # title (String)
+  attr_reader :title
+
+  # image URL of this pathway map (String)
+  attr_reader :image
+
+  # general information URL of this pathway map (String)
+  attr_reader :link
+
+  # contains KGML::Entry objects
+  attr_accessor :entries
+
+  # contains KGML::Relations objects
+  attr_accessor :relations
+
+  # contains KGML::Reactions objects
+  attr_accessor :reactions
+
+  # Bio::KEGG:Entry contains an entry element in the KGML.
   class Entry
-    attr_accessor :entry_id, :name, :category, :link, :reaction, :pathway
-    attr_accessor :label, :shape, :x, :y, :width, :height, :fgcolor, :bgcolor
+
+    # ID of this entry in this pathway map (Integer).
+    # ('id' attribute in 'entry' element)
+    attr_accessor :entry_id
+
+    # KEGG-style ID string of this entry (String)
+    attr_accessor :name
+
+    # type of this entry (String).
+    # Normally one of the following:
+    # * "ortholog"
+    # * "enzyme"
+    # * "reaction"
+    # * "gene"
+    # * "group"
+    # * "compound"
+    # * "map"
+    # See http://www.genome.jp/kegg/xml/docs/ for details.
+    # ('type' attribute in 'entry' element)
+    attr_accessor :category
+
+    # URL pointing information about this entry (String)
+    attr_accessor :link
+
+    # KEGG-style ID string of this reaction (String)
+    attr_accessor :reaction
+
+    # (Deprecated?) ('map' attribute in 'entry' element)
+    attr_accessor :pathway
+
+    # label of the 'graphics' element (String)
+    # ('name' attribute in 'graphics' element)
+    attr_accessor :label
+
+    # shape of the 'graphics' element (String)
+    # Normally one of the following:
+    # * "rectangle"
+    # * "circle"
+    # * "roundrectangle"
+    # * "line"
+    # If not specified, "rectangle" is the default value.
+    # ('type' attribute in 'graphics' element)
+    attr_accessor :shape
+
+    # X axis position (Integer) ('graphics' element)
+    attr_accessor :x
+
+    # Y axis position (Integer) ('graphics' element)
+    attr_accessor :y
+
+    # width (Integer) ('graphics' element)
+    attr_accessor :width
+
+    # height (Integer) ('graphics' element)
+    attr_accessor :height
+
+    # foreground color (String) ('graphics' element)
+    attr_accessor :fgcolor
+
+    # background color (String) ('graphics' element)
+    attr_accessor :bgcolor
+
+    # components included in this entry
+    # (Array containing Integer objects, or nil)
     attr_accessor :components
+
+    # (Deprecated?) names (Array)
     def names
       @name.split(/\s+/)
     end
   end
 
+  # Bio::KEGG::KGML::Relation contains a relation element in the KGML.
   class Relation
-    attr_accessor :node1, :node2, :rel
-    attr_accessor :name, :value
+
+    # the first entry of the relation (String)
+    # ('entry1' attribute in 'relation' element)
+    attr_accessor :node1
+
+    # the second entry of the relation (String)
+    # ('entry2' attribute in 'relation' element)
+    attr_accessor :node2
+
+    # type of this relation (String).
+    # Normally one of the following:
+    # * "ECrel"
+    # * "PPrel"
+    # * "GErel"
+    # * "PCrel"
+    # * "maplink"
+    # ('type' attribute in 'relation' element)
+    attr_accessor :rel
+
+    # interaction and/or relation type (String).
+    # See http://www.genome.jp/kegg/xml/docs/ for details.
+    # ('name' attribute in 'subtype' element)
+    attr_accessor :name
+
+    # interaction and/or relation information (String).
+    # See http://www.genome.jp/kegg/xml/docs/ for details.
+    # ('value' attribute in 'subtype' element)
+    attr_accessor :value
+
+    # (Deprecated?)
     def edge
       @value.to_i
     end
   end
 
+  # Bio::KEGG::KGML::Reaction contains a reaction element in the KGML.
   class Reaction
-    attr_accessor :entry_id, :direction
-    attr_accessor :substrates, :products	# Array
-    attr_accessor :alt				# Hash
+
+    # KEGG-stype ID string of this reaction (String)
+    # ('name' attribute in 'reaction' element)
+    attr_accessor :entry_id
+
+    # type of this reaction (String).
+    # Normally "reversible" or "irreversible".
+    # ('type' attribute in 'reaction' element)
+    attr_accessor :direction
+
+    # Substrates. Each substrate name is the KEGG-style ID string.
+    # (Array containing String objects)
+    attr_accessor :substrates
+
+    # Products. Each product name is the KEGG-style ID string.
+    # (Array containing String objects)
+    attr_accessor :products
+
+    # (Deprecated?) (Hash)
+    attr_accessor :alt
   end
 
   private
