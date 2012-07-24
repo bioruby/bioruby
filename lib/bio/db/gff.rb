88f12fa10f05804cb410e54f65001746a31ff299
Andrew Grimm <andrew.j.grimm@gmail.com>
2012-07-24 21:55:47 +1000

    Squash warning in gff.rb .

--- lib/bio/db/gff.rb
+++ lib/bio/db/gff.rb
@@ -1332,15 +1332,15 @@ module Bio
 
             # rest of data_ref
             len = 0
-            data_ref.each do |ref|
-              len += ref.length if ref.code == :M
+            data_ref.each do |ref2|
+              len += ref.length if ref2.code == :M
             end
             data.push Code.new(:D, len) if len > 0
 
             # rest of data_tgt
             len = 0
-            data_tgt.each do |tgt|
-              len += tgt.length if tgt.code == :M
+            data_tgt.each do |tgt2|
+              len += tgt2.length if tgt2.code == :M
             end
             data.push Code.new(:I, len) if len > 0
 
