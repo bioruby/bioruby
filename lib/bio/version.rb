e8fa160e158970259d915abc6113cb425537b8d6
Naohisa Goto <ng@bioruby.org>
2013-05-25 03:38:17 +0900

    Bio::BIORUBY_EXTRA_VERSION set to ".0001" (Release version with patches)

--- lib/bio/version.rb
+++ lib/bio/version.rb
@@ -23,7 +23,7 @@ module Bio
   # "-preN"   (N=0..99) :: Pre-release test version.
   # "-rcN"    (N=0..99) :: Release candidate version.
   #
-  BIORUBY_EXTRA_VERSION = nil
+  BIORUBY_EXTRA_VERSION = ".0001"
 
   # Version identifier, including extra version string (String)
   # Unlike BIORUBY_VERSION, it is not comparable.
