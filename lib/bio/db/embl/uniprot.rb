b998ad13849ff7f1d69ed0c640a2e1bafe3fc957
Naohisa Goto <ng@bioruby.org>
2013-06-28 14:27:36 +0900

    Bio::UniProt is changed to be an alias of Bio::UniProtKB.

--- lib/bio/db/embl/uniprot.rb
+++ lib/bio/db/embl/uniprot.rb
@@ -1,42 +1,25 @@
 #
 # = bio/db/embl/uniprot.rb - UniProt database class
 # 
-# Copyright::  Copyright (C) 2005 Toshiaki Katayama <k@bioruby.org>
+# Copyright::  Copyright (C) 2013 BioRuby Project
 # License::    The Ruby License
 #
-#  $Id: uniprot.rb,v 1.5 2007/04/05 23:35:40 trevor Exp $
 #
 
-require 'bio/db/embl/sptr'
+warn "Bio::UniProt is an alias of Bio::UniProtKB. Please use Bio::UniProtKB. Bio::UniProt may be deprecated in the future." if $VERBOSE
 
 module Bio
 
-# == Description
-# 
-# Parser class for SwissProt database entry.# See also Bio::SPTR class.
-# This class holds name space for UniProtKB/SwissProt specific methods.
-#
-# UniProtKB/SwissProt specific methods are defined in this class. 
-# Shared methods for UniProtKB/SwissProt and TrEMBL classes are 
-# defined in Bio::SPTR class.
-#
-# == Examples
-#
-#   str = File.read("p53_human.swiss")
-#   obj = Bio::UniProt.new(str)
-#   obj.entry_id #=> "P53_HUMAN"
-#
-# == Referencees
-#
-# * UniProt
-#   http://uniprot.org/
-#
-# * The UniProtKB/SwissProt/TrEMBL User Manual
-#   http://www.expasy.org/sprot/userman.html
-#
-class UniProt < SPTR
-  # Nothing to do (UniProt format is abstracted in SPTR)
-end
+require 'bio/db/embl/uniprotkb' unless const_defined?(:UniProtKB)
+
+  # Bio::UniProt is changed to an alias of Bio::UniProtKB.
+  # Please use Bio::UniProtKB.
+  # Bio::UniProt may be deprecated in the future.
+  #
+  # Note that Bio::SPTR have been renamed to Bio::UniProtKB and
+  # is also an alias of Bio::UniProtKB.
+  #
+  UniProt = UniProtKB
 
 end
 
