a40157205282e148bf3a2e43aed1e08d713fb598
Naohisa Goto <ng@bioruby.org>
2015-03-24 00:46:23 +0900

    suppress warnings "instance variable @circular not initialized"

--- lib/bio/util/restriction_enzyme/range/sequence_range.rb
+++ lib/bio/util/restriction_enzyme/range/sequence_range.rb
@@ -196,7 +196,7 @@ class SequenceRange
     c_cut = cc.vc_complement_as_original_class
     h_cut = cc.hc_between_strands_as_original_class
     
-    if @circular
+    if (defined? @circular) && @circular
       # NOTE
       # if it's circular we should start at the beginning of a cut for orientation
       # scan for it, hack off the first set of hcuts and move them to the back
@@ -238,7 +238,7 @@ class SequenceRange
   
     # Bin "-1" is an easy way to indicate the start of a strand just in case
     # there is a horizontal cut at position 0
-    bins.delete(-1) unless @circular
+    bins.delete(-1) unless ((defined? @circular) && @circular)
     bins
   end
   
