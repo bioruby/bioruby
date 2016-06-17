57bf535da34715beafccb902404cf1bb35b18af4
Naohisa Goto <ng@bioruby.org>
2016-06-17 14:48:46 +0900

    Removed Bio::SQL that have been moved to separate repository
     * Bio::SQL is moved to https://github.com/bioruby/bioruby-biosql
       and removed from this repository.
    
     * List of deleted files:
       * deleted:    lib/bio/db/biosql/biosql_to_biosequence.rb
       * deleted:    lib/bio/db/biosql/sequence.rb
       * deleted:    lib/bio/io/biosql/ar-biosql.rb
       * deleted:    lib/bio/io/biosql/biosql.rb
       * deleted:    lib/bio/io/biosql/config/database.yml
       * deleted:    lib/bio/io/sql.rb
       * deleted:    test/unit/bio/db/biosql/tc_biosql.rb
       * deleted:    test/unit/bio/db/biosql/ts_suite_biosql.rb

--- test/unit/bio/db/biosql/ts_suite_biosql.rb
+++
@@ -1,8 +0,0 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-$:.unshift File.dirname(__FILE__)
-
-require 'test/unit' 
-
-# Add your testcases here
-require 'tc_biosql'
