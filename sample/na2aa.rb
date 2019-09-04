7af9e81988939007eb36dab6b102a7422e8196d8
Naohisa Goto <ng@bioruby.org>
2019-09-04 14:34:05 +0900

    sample/na2aa.rb: Completely rewritten
     * sample/na2aa.rb: Completely rewritten. License is changed because
       old code is completely wiped out. Note that the old code always
       raises error due to a bug in the code.
     * The old code was trying to replace 'X' (any) to '-' (gap) but
       the new code does not modify translated sequences anymore.

--- sample/na2aa.rb
+++ sample/na2aa.rb
@@ -1,34 +1,20 @@
 #!/usr/bin/env ruby
 #
-# translate.rb - translate any NA input into AA FASTA format
+# na2aa.rb - translate any NA input into AA FASTA format
 #
-#   Copyright (C) 2008 KATAYAMA Toshiaki <k@bioruby.org> & Pjotr Prins
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  $Id: na2aa.rb,v 1.1 2008/02/06 16:25:53 pjotr Exp $
+# Copyright::   Copyright (C) 2019 BioRuby Project
+# License::     The Ruby License
 #
 
 require 'bio'
-require 'pp'
 
-include Bio
-
-ARGV.each do | fn |
-  Bio::FlatFile.auto(fn).each do | item |
-    seq = Sequence::NA.new(item.data)
-    aa = seq.translate
-    aa.gsub!(/X/,'-')
-    rec = Bio::FastaFormat.new('> '+item.definition+"\n"+aa)
-    print rec
+ARGV.each do |fn|
+  Bio::FlatFile.open(fn) do |ff|
+    ff.each do |entry|
+      next if /\A\s*\z/ =~ ff.entry_raw.to_s
+      na = entry.naseq
+      aa = na.translate
+      print aa.to_fasta(entry.definition, 70)
+    end
   end
 end
-
